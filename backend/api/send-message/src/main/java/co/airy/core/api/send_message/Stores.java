166a831d7d38665733532ec41567c3dddfa3875a
Paulo Diniz <paulo.rc.diniz@gmail.com>
2020-09-30 11:43:57 +0200

    contact

--- backend/api/send-message/src/main/java/co/airy/core/api/send_message/Stores.java
+++ backend/api/send-message/src/main/java/co/airy/core/api/send_message/Stores.java
@@ -1,14 +1,18 @@
 package co.airy.core.api.send_message;
 
 import co.airy.avro.communication.Channel;
+import co.airy.avro.communication.Contact;
 import co.airy.avro.communication.Conversation;
 import co.airy.kafka.schema.application.ApplicationCommunicationChannels;
+import co.airy.kafka.schema.application.ApplicationCommunicationContacts;
 import co.airy.kafka.schema.application.ApplicationCommunicationConversations;
 import co.airy.kafka.streams.KafkaStreamsWrapper;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Named;
+import org.javatuples.Pair;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
@@ -26,11 +30,18 @@ public class Stores implements ApplicationListener<ApplicationStartedEvent>, Dis
 
     private final String applicationCommunicationChannels = new ApplicationCommunicationChannels().name();
 
+    private String CONTACT_CHANNEL_STORE = "contact-channel-store";
+
     private void startStream() {
         final StreamsBuilder builder = new StreamsBuilder();
 
         final KTable<String, Channel> channelKTable = builder.table(new ApplicationCommunicationChannels().name(), Consumed.as("channelsTable"), Materialized.as("channelsTableStore"));
         final KTable<String, Conversation> conversationKTable = builder.table(new ApplicationCommunicationConversations().name(), Consumed.as("conversationsTable"), Materialized.as("conversationsTableStore"));
+
+        builder.<String, Contact>table(new ApplicationCommunicationContacts().name(), Consumed.as("contactsTable"), Materialized.as("contactsTableStore"))
+                .join(conversationKTable, Pair::with, Named.as("contactsAndConversationsJoined"))
+                .join(channelKTable, pair -> pair.getValue1().getChannelId(), Pair::setAt1, Named.as("contactsAndConversationsAndChannelsJoined"),
+                        Materialized.as(CONTACT_CHANNEL_STORE));
 
         streams.start(builder.build(), appId);
     }

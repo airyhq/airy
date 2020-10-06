00b5a8a30927fc94aea10d79d36e8d23f47d317b
Aitor Algorta <aitor@airy.co>
2020-10-06 14:19:07 +0200

    addLib

---
+++ frontend/components/example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'components'
+import 'components/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App

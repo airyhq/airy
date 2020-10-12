1e3bde54bbcc7e928f58a0d8e4f59544977906b5
Pascal Holy <pascal@airy.co>
2020-10-12 15:33:49 +0200

    Clean topics script

--- infrastructure/bootstrap/airy-bootstrapping.sh
+++ infrastructure/bootstrap/airy-bootstrapping.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 vagrant box add airy-core airy.box
 vagrant init airy-core
 vagrant up
--- infrastructure/bootstrap/create-topics.sh
+++ infrastructure/bootstrap/create-topics.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 # It creates Kafka topics which don't exist yet in a cluster, but are in the topics.sh
-# Example usage: (from the bastion host) cd ~/bin; ./create-topics.sh
-# Note: default values, which can be overwritten with env variables before
-# running, REPLICAS=3, PARTITIONS=64. If the topics are there - they will not be
-# re-created
-
 
 RELEASE_NAME=airy
 ZOOKEEPER=${RELEASE_NAME}-cp-zookeeper:2181
@@ -28,150 +23,11 @@ then
 fi
 done
 
-
-for topic in ${sourceTopicsCompacted[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic --config cleanup.policy=compact --config segment.bytes=10485760 --config min.compaction.lag.ms=86400000
-fi
-done
-
-
-for topic in ${failedTopics[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic
-fi
-
-done
-
-for topic in ${internalTopics[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic --config retention.ms=3600000 --config retention.bytes=1000000000 --config cleanup.policy=compact,delete
-fi
-done
-
-for topic in ${identityTopics[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic --config cleanup.policy=compact
-fi
-done
-
-for topic in ${identityTopicsCompacted[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic --config cleanup.policy=compact --config segment.bytes=10485760 --config min.compaction.lag.ms=86400000
-fi
-done
-
 for topic in ${communicationTopics[@]}; do
 if ! grep -q "^$topic$" $TOPICS_FILE
 then
   echo "creating $topic"
   kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic
-fi
-done
-
-for topic in ${migrationTopics[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic
-fi
-done
-
-for topic in ${communicationTopicsCompacted[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic --config cleanup.policy=compact --config segment.bytes=10485760 --config min.compaction.lag.ms=86400000
-fi
-done
-
-for topic in ${organizationTopics[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic
-fi
-done
-
-for topic in ${automationTopics[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic
-fi
-done
-
-for topic in ${automationTopicsCompacted[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic --config cleanup.policy=compact --config segment.bytes=10485760 --config min.compaction.lag.ms=86400000
-fi
-done
-
-for topic in ${organizationTopicsCompacted[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic --config cleanup.policy=compact --config segment.bytes=10485760 --config min.compaction.lag.ms=86400000
-fi
-done
-
-for topic in ${activityTopics[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic --config retention.ms=1800000
-fi
-done
-
-for topic in ${sinkTopics[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic
-fi
-done
-
-for topic in ${contactTopicsCompacted[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic --config cleanup.policy=compact --config segment.bytes=10485760 --config min.compaction.lag.ms=86400000
-fi
-done
-
-for topic in ${contactTopics[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic
-fi
-done
-
-for topic in ${etlTopicsCompacted[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic --config cleanup.policy=compact --config segment.bytes=10485760 --config min.compaction.lag.ms=86400000
-fi
-done
-
-for topic in ${opsTopicsWithRetention[@]}; do
-if ! grep -q "^$topic$" $TOPICS_FILE
-then
-  echo "creating $topic"
-  kafka-topics --create --zookeeper $ZOOKEEPER --replication-factor $REPLICAS --partitions $PARTITIONS --topic $topic --config retention.ms=2592000000
 fi
 done
 

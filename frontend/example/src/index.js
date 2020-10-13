fccfb266230630ec3de22359c8fea470fc1014d3
Aitor Algorta <aitor@airy.co>
2020-10-13 11:33:06 +0200

    WIP

---
+++ frontend/example/src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+require("./index.css");
+var react_1 = __importDefault(require("react"));
+var react_dom_1 = __importDefault(require("react-dom"));
+var App_1 = __importDefault(require("./App"));
+react_dom_1["default"].render(react_1["default"].createElement(App_1["default"], null), document.getElementById('root'));
+//# sourceMappingURL=index.js.map
---
+++ frontend/example/src/model/index.js
@@ -0,0 +1,19 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+exports.__esModule = true;
+__export(require("./AiryRequestConfig"));
+__export(require("./Channel"));
+__export(require("./Conversation"));
+__export(require("./Member"));
+__export(require("./Membership"));
+__export(require("./Message"));
+__export(require("./Organization"));
+__export(require("./Settings"));
+__export(require("./Tag"));
+__export(require("./Team"));
+__export(require("./Template"));
+__export(require("./User"));
+__export(require("./Contact"));
+//# sourceMappingURL=index.js.map

389f52ff31971722a18878056dabd93c850b3fa3
Aitor Algorta <aitor@airy.co>
2020-10-13 14:42:29 +0200

    remove autogenerated files

--- frontend/components/src/components/loaders/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-  for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-exports.__esModule = true;
-__export(require("./SimpleLoader"));
-__export(require("./AnalyticsLoader"));
-__export(require("./AiryLoader"));
-//# sourceMappingURL=index.js.map

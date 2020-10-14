2763f057095a8b62710f7bee8e14f1105456f47b
Pascal Holy <pascal@airy.co>
2020-10-14 17:45:24 +0200

    Fix db script

--- infrastructure/bootstrap/create-database.sh
+++ infrastructure/bootstrap/create-database.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 IFS=$'\n\t'
 
 apt update
-apt-get install postgresql-client
+apt-get install postgresql-client -y --force-yes
 PGPASSWORD=4fRteQH2MTKmZzdv psql -h postgres postgres -U postgresadmin -c 'CREATE DATABASE airy_core;'

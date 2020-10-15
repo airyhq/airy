2a692e88c7a47ccaec467c9b2ce1f62e1806a16f
Christoph Pröschel <chris@airy.co>
2020-10-15 10:07:33 +0200

    Add webhook consumer

---
+++ backend/webhook/consumer/go.mod
@@ -0,0 +1,8 @@
+module redis-worker
+
+go 1.13
+
+require (
+	github.com/alicebob/miniredis/v2 v2.13.3
+	github.com/go-redis/redis/v8 v8.2.2
+)
---
+++ backend/webhook/consumer/pkg/scheduler/scheduler.go
@@ -0,0 +1,84 @@
+package scheduler
+
+import (
+	"fmt"
+	"redis-worker/pkg/scheduler/queue"
+	"regexp"
+
+	"log"
+	"redis-worker/pkg/scheduler/consumer"
+	"time"
+)
+
+type Task struct {
+	ticker    *time.Ticker
+	queueName string
+	queue     queue.Queue
+	consumers map[string]consumer.Task
+}
+
+func Start(hostname, port string) *Task {
+	t := &Task{
+		ticker:    time.NewTicker(time.Minute),
+		queue:     queue.NewQueue(queue.GetClient(hostname, port)),
+		consumers: make(map[string]consumer.Task),
+	}
+
+	go func() {
+		t.updateConsumers(t.queue)
+		for {
+			select {
+			case <-t.ticker.C:
+				t.updateConsumers(t.queue)
+			}
+		}
+	}()
+
+	log.Println("queue consumer scheduler started")
+	return t
+}
+
+type ConsumerStatus struct {
+	Errors []consumer.ConsumerError
+	Queue  string
+}
+
+func (t *Task) GetStatuses() []ConsumerStatus {
+	errors := make([]ConsumerStatus, len(t.consumers))
+
+	for k, v := range t.consumers {
+		if err := v.GetErrors(); err != nil {
+			errors = append(errors, ConsumerStatus{
+				Errors: err,
+				Queue:  k,
+			})
+		}
+	}
+
+	return errors
+}
+
+func (t *Task) updateConsumers(q queue.Queue) {
+	queues, err := q.GetAllQueues()
+
+	if err != nil {
+		log.Println("failed to fetch queues", err)
+	}
+
+	for _, it := range queues {
+		fmt.Println(it)
+		if _, ok := t.consumers[it]; !ok && !isProcessingQueue(it) {
+			t.consumers[it] = consumer.StartConsumer(t.queue.Rdb, it)
+		}
+	}
+}
+
+func isProcessingQueue(queue string) bool {
+	matched, err := regexp.MatchString(`.*_processing`, queue)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return matched
+}

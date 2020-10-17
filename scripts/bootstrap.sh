e4977411a848b06e19cf0b7adcf7bb2a33c08664
Pascal Holy <pascal@airy.co>
2020-10-17 22:25:02 +0200

    Add sed with var

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,9 +2,23 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-echo Hello, who am I talking to?
-read varname
-echo It\'s nice to meet you $varname
+echo Please enter your Facebook app id
+read fb_app_id
+
+sed -i '.bak' "s/<fb_app_id>/$fb_app_id/" "infrastructure/deployments/sources-facebook-events-router.yaml"
+sed -i '.bak' "s/<fb_app_id>/$fb_app_id/" "infrastructure/deployments/api-admin.yaml"
+
+
+echo Please enter your Facebook app secret
+read fb_app_secret
+
+sed -i  '.bak' "s/<fb_app_secret>/$fb_app_secret/" infrastructure/deployments/api-admin.yaml
+
+
+echo Please enter your Facebook webhook secret
+read fb_webhook_secret
+
+sed -i '.bak' "s/<fb_webhook_secret>/$fb_webhook_secret/" infrastructure/deployments/sources-facebook-webhook.yaml
 
 
 cd infrastructure

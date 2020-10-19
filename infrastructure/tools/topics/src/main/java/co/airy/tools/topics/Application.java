77f2d6f2c30a7918dd3ac02980f3cb7ccffb1f02
Pascal Holy <pascal@airy.co>
2020-10-19 14:10:36 +0200

    Rm tf output

--- infrastructure/tools/topics/src/main/java/co/airy/tools/topics/Application.java
+++ infrastructure/tools/topics/src/main/java/co/airy/tools/topics/Application.java
@@ -22,7 +22,7 @@ public class Application {
     public Application() {
         TopicsFinder finder = new TopicsFinder();
 
-        String template = getTemplate("sh");
+        String template = getTemplate();
 
 
         Set<String> results = finder.findTopics();
@@ -75,49 +75,12 @@ public class Application {
         }
     }
 
-    private void createTerraformOutput(ArrayList<String> resultsSortedNames, String template) throws IOException {
-        Method name;
-        Method config;
-
-        try {
-
-            BufferedWriter out = new BufferedWriter(new FileWriter(new Date().toString() + "topics.tf", true));
-
-            for (String result : resultsSortedNames) {
-                Class topicClass = Class.forName(result);
-                if (Modifier.isAbstract(topicClass.getModifiers())) {
-                    continue;
-                }
-                name = topicClass.getMethod("name");
-                config = topicClass.getMethod("config");
-
-                String topicName = ((String) name.invoke(topicClass.getDeclaredConstructor().newInstance(), null)).replace(".", "_");
-
-                Map<String, String> topicConfig = (Map<String, String>) config.invoke(topicClass.getDeclaredConstructor().newInstance(), null);
-
-                List<String> keys = new ArrayList<>(topicConfig.keySet());
-
-                Collections.sort(keys);
-
-                String topicConfigFormatted = keys.stream().map(k -> String.format("\t\"%s\" = \"%s\"", k, topicConfig.get(k))).collect(Collectors.joining("\n"));
-
-
-                String replaced = String.format(template, topicName, topicName, topicConfigFormatted);
-
-                out.write(replaced + "\n");
-            }
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private String getTemplate(String format) {
+    private String getTemplate() {
         String line = "";
         StringBuilder buffer = new StringBuilder();
 
         try {
-            try (InputStream is = (getClass().getResourceAsStream(String.format("/topic.%s.tpl", format)))) {
+            try (InputStream is = (getClass().getResourceAsStream(String.format("/topic.sh.tpl")))) {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(is));
                 while ((line = reader.readLine()) != null) {
                     buffer.append(line).append("\n");
--- infrastructure/tools/topics/src/main/resources/topic.tf.tpl
+++
@@ -1,9 +0,0 @@
-resource "kafka_topic" "%s" {
-   name               = "%s"
-   replication_factor = var.replicas
-   partitions         = var.partitions
-
-   config = {
-%s
-   }
- }

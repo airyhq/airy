1bab85f0681bbd543661d202757c9b1ba41688ec
Christoph Pröschel <chris@airy.co>
2020-10-21 17:31:03 +0200

    Remove date range, make showcase use npm library

--- frontend/showcase/src/index.tsx
+++ frontend/showcase/src/index.tsx
@@ -4,4 +4,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import "@airyhq/components/dist/main.css";
+
 ReactDOM.render(<App />, document.getElementById("root"));

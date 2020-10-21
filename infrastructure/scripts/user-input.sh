8c3bbd8c6adde5163b5a550000cab870d18baa9e
Pascal Holy <pascal@airy.co>
2020-10-21 16:13:04 +0200

    Add user input from config file

--- infrastructure/scripts/user-input.sh
+++ infrastructure/scripts/user-input.sh
@@ -2,22 +2,27 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-echo Please enter your Facebook app id
-read fb_app_id
+config=( # set default values in config array
+    [fb_app_id]="default"
+    [fb_app_secret]="default"
+    [fb_webhook_secret]="default"
+)
 
-sed -i '.bak' "s/<fb_app_id>/$fb_app_id/" ../deployments/sources-facebook-events-router.yaml
-sed -i '.bak' "s/<fb_app_id>/$fb_app_id/" ../deployments/api-admin.yaml
+while read line
+do
+    if echo $line | grep -F = &>/dev/null
+    then
+        varname=$(echo "$line" | cut -d '=' -f 1)
+        config[$varname]=$(echo "$line" | cut -d '=' -f 2-)
+    fi
+done < ../airy.conf
 
-echo Please enter your Facebook app secret
-read fb_app_secret
+sed -i '.bak' "s/<fb_app_id>/${config[fb_app_id]}/" ../deployments/sources-facebook-events-router.yaml
+sed -i '.bak' "s/<fb_app_id>/${config[fb_app_id]}/" ../deployments/api-admin.yaml
 
+sed -i  '.bak' "s/<fb_app_secret>/${config[fb_app_se]}cret/" ../deployments/api-admin.yaml
 
-sed -i  '.bak' "s/<fb_app_secret>/$fb_app_secret/" ../deployments/api-admin.yaml
-
-echo Please enter your Facebook webhook secret
-read fb_webhook_secret
-
-sed -i '.bak' "s/<fb_webhook_secret>/$fb_webhook_secret/" ../deployments/sources-facebook-webhook.yaml
+sed -i '.bak' "s/<fb_webhook_secret>/${config[fb_webhoo]}k_secret/" ../deployments/sources-facebook-webhook.yaml
 
 kubectl apply -f ../deployments/sources-facebook-events-router.yaml
 kubectl apply -f ../deployments/api-admin.yaml

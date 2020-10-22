04544760120bc24fa01f1a22a264502212b84091
ljupcovangelski <ljupco@airy.co>
2020-10-22 17:50:36 +0200

    Fix airy-core s3 image

---
+++ infrastructure/images/tasks/pull-images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+docker pull confluentinc/cp-enterprise-kafka:5.5.0
+docker pull confluentinc/cp-kafka:5.5.0
+docker pull confluentinc/cp-zookeeper:5.5.0
+docker pull postgres:12.4
+docker pull redis:5.0.1-alpine

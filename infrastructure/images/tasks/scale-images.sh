ca6d9ebb4c0a8556f0d1c1507892d70b1dbe475a
ljupcovangelski <ljupco@airy.co>
2020-10-27 15:12:32 +0100

    Pruning all unused images

--- infrastructure/images/tasks/scale-images.sh
+++ infrastructure/images/tasks/scale-images.sh
@@ -3,4 +3,12 @@ set -euo pipefail
 IFS=$'\n\t'
 
 kubectl scale --replicas=0 deployment -l boxImage="no"
-docker rmi -f $(docker images | grep airyhq | awk '{ print $3; }')
+
+kubectl get pods --field-selector=status.phase=Terminating
+while [ `kubectl get pods --field-selector=status.phase=Terminating 2>/dev/null | wc -l` -gt 0 ]
+do 
+    echo "Waiting for pods to terminate..."
+    sleep 1
+done
+
+docker image prune -f -a

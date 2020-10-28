e2b99144b0ba98b91c794843c47e4bd76e8cc4cc
Pascal Holy <pascal@airy.co>
2020-10-29 00:10:41 +0100

    Fix shit

--- infrastructure/scripts/user-input.sh
+++ infrastructure/scripts/user-input.sh
@@ -5,7 +5,11 @@ IFS=$'\n\t'
 cd /vagrant/scripts
 
 mkdir -p ~/airy-core/
+cp ../deployments/sources-facebook-events-router.yaml ~/airy-core/	
+cp ../deployments/api-admin.yaml ~/airy-core/	
+cp ../deployments/api-auth.yaml ~/airy-core/
 cp ../deployments/sources-facebook-webhook.yaml ~/airy-core/
+cp ../deployments/api-communication.yaml ~/airy-core/
 
 kubectl create configmap secrets-config --from-literal=JWT_SECRET=`cat /dev/urandom | env LC_CTYPE=C tr -dc a-z0-9 | head -c 128; echo` --dry-run=client -o yaml | kubectl apply -f -
 kubectl create configmap user-config --from-env-file=../airy.conf --dry-run=client -o yaml | kubectl apply -f -
@@ -18,3 +22,4 @@ kubectl apply -f ~/airy-core/sources-facebook-events-router.yaml
 kubectl apply -f ~/airy-core/api-admin.yaml
 kubectl apply -f ~/airy-core/api-auth.yaml
 kubectl apply -f ~/airy-core/sources-facebook-webhook.yaml
+kubectl apply -f ~/airy-core/api-communication.yaml

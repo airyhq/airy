e3627e0009e44220b9e38bf8909032e646c15c8b
Pascal Holy <pascal@airy.co>
2020-10-29 12:07:33 +0100

    Randomize pg password on user-input

--- infrastructure/scripts/user-input.sh
+++ infrastructure/scripts/user-input.sh
@@ -11,6 +11,13 @@ cp ../deployments/api-auth.yaml ~/airy-core/
 cp ../deployments/sources-facebook-webhook.yaml ~/airy-core/
 cp ../deployments/api-communication.yaml ~/airy-core/
 
+cp -R /vagrant/helm-chart ~/airy-core/
+RANDOM_POSTGRES_PASSWORD=`cat /dev/urandom | env LC_CTYPE=C tr -dc a-z0-9 | head -c 32; echo`
+sed "s/<pg_password>/$RANDOM_POSTGRES_PASSWORD/" /vagrant/helm-chart/charts/postgres/values.yaml > ~/airy-core/helm-chart/charts/postgres/values.yaml	
+/usr/local/bin/helm upgrade -f ~/airy-core/helm-chart/values.yaml airy ~/airy-core/helm-chart/ --version 0.5.0 --timeout 1000s	
+kubectl delete pods -l app=postgres	
+kubectl create configmap postgres-config --from-literal=POSTGRES_PASSWORD=$RANDOM_POSTGRES_PASSWORD --from-literal=POSTGRES_USER=postgresadmin --dry-run=client -o yaml | kubectl apply -f -
+
 kubectl create configmap secrets-config --from-literal=JWT_SECRET=`cat /dev/urandom | env LC_CTYPE=C tr -dc a-z0-9 | head -c 128; echo` --dry-run=client -o yaml | kubectl apply -f -
 kubectl create configmap user-config --from-env-file=../airy.conf --dry-run=client -o yaml | kubectl apply -f -
 

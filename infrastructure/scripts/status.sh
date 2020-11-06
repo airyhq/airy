ba8b76f9ed18a41d057be7fc83cf3703b3f16bee
Pascal Holy <pascal@airy.co>
2020-11-06 17:36:07 +0100

    Update docs

--- infrastructure/scripts/status.sh
+++ infrastructure/scripts/status.sh
@@ -10,7 +10,6 @@ done
 
 kubectl exec kafka-client -- /root/wait-for-service.sh api-auth 80 10 Airy-auth
 
-INGRESS_IP=`ip addr show eth1 | grep "inet " | awk '{ print $2; }' | cut -d "/" -f1`
 
 FB_WEBHOOK_PUBLIC_URL=`kubectl get configmap public-urls -o jsonpath='{.data.FB_WEBHOOK_PUBLIC_URL}'`
 
@@ -22,4 +21,4 @@ echo "You can access the API of the Airy Core Platform at:"
 echo "http://${INGRESS_IP}/"
 echo
 echo "Example:"
-echo "curl -X POST -H 'Content-Type: application/json' -d '{\"first_name\": \"Grace\",\"last_name\": \"Hopper\",\"password\": \"the_answer_is_42\",\"email\": \"grace@example.com\"}' http://${INGRESS_IP}/users.signup"
+echo "curl -X POST -H 'Content-Type: application/json' -d '{\"first_name\": \"Grace\",\"last_name\": \"Hopper\",\"password\": \"the_answer_is_42\",\"email\": \"grace@example.com\"}' http://api.airy.local/users.signup"

eccfcb460a47814d45c1fc3a54b34763258b0582
Pascal Holy <pascal@airy.co>
2020-11-06 17:36:07 +0100

    Update docs

--- infrastructure/scripts/status.sh
+++ infrastructure/scripts/status.sh
@@ -10,19 +10,11 @@ done
 
 kubectl exec kafka-client -- /root/wait-for-service.sh api-auth 80 10 Airy-auth
 
-INGRESS_IP=`ip addr show eth1 | grep "inet " | awk '{ print $2; }' | cut -d "/" -f1`
 
 FB_WEBHOOK_PUBLIC_URL=`kubectl get configmap public-urls -o jsonpath='{.data.FB_WEBHOOK_PUBLIC_URL}'`
 
 echo
 echo "Your public url for the Facebook Webhook is:"
-echo ${FB_WEBHOOK_PUBLIC_URL}/facebook
-echo
-echo "You can access the web frontend of the Airy Core Platform at:"
-echo "http://${INGRESS_IP}/demo"
-echo
-echo "You can access the API of the Airy Core Platform at:"
-echo "http://${INGRESS_IP}/"
-echo
+echo http://api.airy.local/facebook
 echo "Example:"
-echo "curl -X POST -H 'Content-Type: application/json' -d '{\"first_name\": \"Grace\",\"last_name\": \"Hopper\",\"password\": \"the_answer_is_42\",\"email\": \"grace@example.com\"}' http://${INGRESS_IP}/users.signup"
+echo "curl -X POST -H 'Content-Type: application/json' -d '{\"first_name\": \"Grace\",\"last_name\": \"Hopper\",\"password\": \"the_answer_is_42\",\"email\": \"grace@example.com\"}' http://api.airy.local/users.signup"

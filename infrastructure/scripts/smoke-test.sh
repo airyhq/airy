b5f3f8cd3581618e12bebea9a254e0bfd318b8cb
Pascal Holy <pascal@airy.co>
2020-11-09 17:44:54 +0100

    Add chatplugin connection to smoke test

--- infrastructure/scripts/smoke-test.sh
+++ infrastructure/scripts/smoke-test.sh
@@ -17,6 +17,8 @@ function apiCall {
       response=$(curl -H ${content_type} -s -w "%{stderr}%{http_code}\n" ${url} -d ${request_payload} 2>&1)
     else
       response=$(curl -H ${content_type} -H "Authorization: $token" -s -w "%{stderr}%{http_code}\n" ${url} -d ${request_payload} 2>&1)
+      >&2 echo curl -H ${content_type} -H "Authorization: $token" -s -w "%{stderr}%{http_code}\n" ${url} -d ${request_payload} 2>&1
+
   fi
   response_http_code=$(head -1 <<< "$response")
   response_payload=$(tail -1 <<< "$response")
@@ -32,16 +34,22 @@ function apiCall {
 login_payload=$(apiCall "users.login" '{"email":"grace@example.com","password":"the_answer_is_42"}' 200)
 
 token=$(echo $login_payload | jq -r '.token')
+
+channels_connect=$(apiCall "channels.connect" '{"source": "chat_plugin", "source_channel_id": "my-chat-channel-1", "token": "wat", "name": "chat plugin source", "image_url": ""}' 200 ${token})
+channels_connect_payload=$(tail -1 <<< "$channels_connect")
+channel_id=$(echo $channels_connect_payload | jq -r '.id')
+chatplugin_authenticate_payload=$(apiCall "chatplugin.authenticate" "{"channel_id": "$channel_id"}" 200 ${token})
+exit
+
+
 conversation_list_payload=$(apiCall "conversations.list" '{}' 200 ${token})
 conversations_info_payload=$(apiCall "conversations.info" '{}' 200 ${token})
 conversations_read_payload=$(apiCall "conversations.read" '{}' 200 ${token})
 
-chatplugin_authenticate_payload=$(apiCall "chatplugin.authenticate" '{}' 200 ${token})
 
-channels_list_payload=$(apiCall "channels.list" '{}' 200 ${token})
-channels_disconnect_payload=$(apiCall "channels.disconnect" '{}' 200 ${token})
-channels_explore_payload=$(apiCall "channels.explore" '{}' 200 ${token})
-channels_connect_payload=$(apiCall "channels.connect" '{}' 200 ${token})
+channels_list=$(apiCall "channels.list" '{}' 200 ${token})
+channels_disconnect=$(apiCall "channels.disconnect" '{}' 200 ${token})
+channels_explore=$(apiCall "channels.explore" '{}' 200 ${token})
 
 webhooks_subscribe_payload=$(apiCall "webhooks.subscribe" '{}' 200 ${token})
 webhooks_unsubscribe_payload=$(apiCall "webhooks.unsubscribe" '{}' 200 ${token})

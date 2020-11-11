620cd8416dfcc62133ddaff73a0ed7ad462a3660
Pascal Holy <pascal@airy.co>
2020-11-11 13:30:52 +0100

    Rename extractFromPayload function

--- infrastructure/scripts/smoke-test.sh
+++ infrastructure/scripts/smoke-test.sh
@@ -29,7 +29,7 @@ function apiCall {
   echo ${response_payload}
 }
 
-function getValueFrom {
+function extractFromPayload {
   local payload=$(tail -1 <<< "$1")
   echo $payload | jq -r ".$2"
 }
@@ -39,18 +39,20 @@ login_response=$(apiCall "users.login" '{"email":"grace@example.com","password":
 token=$(echo $login_response | jq -r '.token')
 
 channels_connect_response=$(apiCall "channels.connect" '{"source": "chat_plugin", "source_channel_id": "my-chat-channel-1", "token": "wat", "name": "chat plugin source", "image_url": ""}' 200 ${token})
+sleep 1
 channels_list=$(apiCall "channels.list" '{}' 200 ${token})
 channels_explore=$(apiCall "channels.explore" '{}' 200 ${token})
-channel_id=$(getValueFrom $channels_connect_response "id")
+channel_id=$(extractFromPayload $channels_connect_response "id")
 
 
 chatplugin_authenticate_response=$(apiCall "chatplugin.authenticate" "{\"channel_id\": \"$channel_id\"}" 200 ${token})
-chatplugin_token=$(getValueFrom $chatplugin_authenticate_response "token")
+chatplugin_token=$(extractFromPayload $chatplugin_authenticate_response "token")
 
 chatplugin_send_response=$(apiCall "chatplugin.send" '{"message": {"text": "Message from chatplugin"}}' 200 ${chatplugin_token})
-
+chatplugin_message_id=$(extractFromPayload $chatplugin_send_response "id")
+sleep 1
 conversation_list_response=$(apiCall "conversations.list" '{}' 200 ${token})
-conversation_id=$(getValueFrom $conversation_list_response "data[0].id")
+conversation_id=$(extractFromPayload $conversation_list_response "data[0].id")
 conversations_info_response=$(apiCall "conversations.info" "{\"conversation_id\": \"$conversation_id\"}" 200 ${token})
 conversations_read_response=$(apiCall "conversations.read" "{\"conversation_id\": \"$conversation_id\"}" 202 ${token})
 
@@ -59,7 +61,7 @@ messages_send_response=$(apiCall "messages.send" "{\"conversation_id\": \"$conve
 messages_list_response=$(apiCall "messages.list" "{\"conversation_id\": \"$conversation_id\"}" 200 ${token})
 
 tags_create_response=$(apiCall "tags.create" '{"name": "tag-1", "color":"tag-red"}' 201 ${token})
-tag_id=$(getValueFrom $tags_create_response "id")
+tag_id=$(extractFromPayload $tags_create_response "id")
 tags_update_response=$(apiCall "tags.update" "{\"id\":\"$tag_id\", \"name\": \"tag-1\", \"color\": \"tag-blue\"}" 200 ${token})
 tags_list_response=$(apiCall "tags.list" '{}' 200 ${token})
 tags_delete_response=$(apiCall "tags.delete" "{\"id\": \"$tag_id\"}" 200 ${token})

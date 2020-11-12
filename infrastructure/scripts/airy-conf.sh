266450aba8c4d8cdeb68c00e2ce3872ea4a06e77
Pascal Holy <pascal@airy.co>
2020-11-12 14:25:17 +0100

    Move config to config maps

--- infrastructure/scripts/airy-conf.sh
+++ infrastructure/scripts/airy-conf.sh
@@ -2,64 +2,15 @@
 set -eo pipefail
 IFS=$'\n\t'
 
-declare -A config
-
-config=(
-    ["FB_APP_ID"]="default"
-    ["FB_APP_SECRET"]="default"
-    ["FB_WEBHOOK_SECRET"]="default"
-    ["MAIL_URL"]="default"
-    ["MAIL_PORT"]="587"
-    ["MAIL_USERNAME"]="default"
-    ["MAIL_PASSWORD"]="default"
-    ["MAIL_FROM"]="default@localhost"
-)
-
 cd /vagrant/scripts
-CONFIG_FILE="../airy.conf"
-
-
 mkdir -p ~/airy-core
 
 
 kubectl create configmap secrets-config --from-literal=JWT_SECRET=`cat /dev/urandom | env LC_CTYPE=C tr -dc a-z0-9 | head -c 128; echo` --dry-run=client -o yaml | kubectl apply -f -
 kubectl create configmap user-config --from-env-file=../airy.conf --dry-run=client -o yaml | kubectl apply -f -
+kubectl create configmap public-urls --from-literal=RANDOM_INGRESS_ID=`cat /dev/urandom | env LC_CTYPE=C tr -dc a-z0-9 | head -c 16; echo` --dry-run=client -o yaml | kubectl apply -f -
 
-
-
-RANDOM_JWT_SECRET=`cat /dev/urandom | env LC_CTYPE=C tr -dc a-z0-9 | head -c 128; echo`
-sed -i "s/<jwt_secret>/${RANDOM_JWT_SECRET}/" ~/airy-core/api-auth.yaml
-sed -i "s/<jwt_secret>/${RANDOM_JWT_SECRET}/" ~/airy-core/api-admin.yaml
-sed -i "s/<jwt_secret>/${RANDOM_JWT_SECRET}/" ~/airy-core/api-communication.yaml
-sed -i "s/<jwt_secret>/${RANDOM_JWT_SECRET}/" ~/airy-core/sources-chatplugin.yaml
-
-sed -i "s/<fb_app_id>/${config[FB_APP_ID]}/" ~/airy-core/sources-facebook-events-router.yaml
-sed -i "s/<fb_app_id>/${config[FB_APP_ID]}/" ~/airy-core/api-admin.yaml
-
-sed -i "s/<fb_app_secret>/${config[FB_APP_SECRET]}/" ~/airy-core/api-admin.yaml
-
-sed -i "s/<fb_webhook_secret>/${config[FB_WEBHOOK_SECRET]}/" ~/airy-core/sources-facebook-webhook.yaml
-
-sed -i "s/<mail_url>/${config[MAIL_URL]}/" ~/airy-core/api-auth.yaml
-sed -i "s/<mail_port>/${config[MAIL_PORT]}/" ~/airy-core/api-auth.yaml
-sed -i "s/<mail_username>/${config[MAIL_USERNAME]}/" ~/airy-core/api-auth.yaml
-sed -i "s/<mail_password>/${config[MAIL_PASSWORD]}/" ~/airy-core/api-auth.yaml
-sed -i "s/<mail_from>/${config[MAIL_FROM]}/" ~/airy-core/api-auth.yaml
-
-# Generate random string for the ngrok webhook
-RANDOM_INGRESS_ID=`cat /dev/urandom | env LC_CTYPE=C tr -dc a-z0-9 | head -c 16; echo`
-sed -i "s/<ngrok_client_string>/fb-${RANDOM_INGRESS_ID}/" ~/airy-core/sources-facebook-webhook.yaml
-
-kubectl apply -f ~/airy-core/api-admin.yaml
-kubectl apply -f ~/airy-core/api-auth.yaml
-kubectl apply -f ~/airy-core/frontend-demo.yaml
-kubectl apply -f ~/airy-core/api-communication.yaml
-kubectl apply -f ~/airy-core/sources-facebook-events-router.yaml
-kubectl apply -f ~/airy-core/sources-facebook-sender.yaml
-kubectl apply -f ~/airy-core/sources-facebook-webhook.yaml
-kubectl apply -f ~/airy-core/webhook-consumer.yaml
-kubectl apply -f ~/airy-core/webhook-publisher.yaml
-kubectl apply -f ~/airy-core/sources-chatplugin.yaml
+helm upgrade -f ~/airy-core/helm-chart/values.yaml airy ~/airy-core/helm-chart/ --set global.appImageTag=beta --version 0.5.0 --timeout 1000s 2>/dev/null
 
 kubectl scale deployment airy-schema-registry --replicas=1
 kubectl exec kafka-client -- /root/wait-for-service.sh airy-schema-registry 8081 15 Schema-registry

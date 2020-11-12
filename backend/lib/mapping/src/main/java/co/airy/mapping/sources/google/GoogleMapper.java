1deea9c0f6db6db35065501a32b3b282b218482b
Paulo Diniz <paulo.rc.diniz@gmail.com>
2020-11-12 15:05:29 +0100

    [#270] Google content mapper

---
+++ backend/lib/mapping/src/main/java/co/airy/mapping/sources/google/GoogleMapper.java
@@ -0,0 +1,29 @@
+package co.airy.mapping.sources.google;
+
+import co.airy.mapping.SourceMapper;
+import co.airy.mapping.model.Content;
+import co.airy.mapping.model.Text;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GoogleMapper implements SourceMapper {
+    private final ObjectMapper objectMapper;
+
+    public GoogleMapper() {
+        this.objectMapper = new ObjectMapper();
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "google";
+    }
+
+    @Override
+    public Content render(String payload) throws Exception {
+        final JsonNode jsonNode = objectMapper.readTree(payload);
+        final JsonNode messageNode = jsonNode.get("message");
+        return new Text(messageNode.get("text").textValue());
+    }
+}

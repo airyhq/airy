654205fd4670476408eefb4b932b2a2add07b655
lucapette <ciao@lucapette.me>
2020-11-12 15:59:18 +0100

    [#221] Further improvements to the script (#283)
    Fixes #218

--- infrastructure/scripts/smoke-test.sh
+++ infrastructure/scripts/smoke-test.sh
@@ -40,28 +40,46 @@ token=$(echo $login_response | jq -r '.token')
 
 channels_connect_response=$(apiCall "channels.connect" '{"source": "chat_plugin", "source_channel_id": "my-chat-channel-1", "token": "wat", "name": "chat plugin source", "image_url": ""}' 200 ${token})
 sleep 1
-channels_list=$(apiCall "channels.list" '{}' 200 ${token})
-channels_explore=$(apiCall "channels.explore" '{}' 200 ${token})
 channel_id=$(extractFromPayload $channels_connect_response "id")
 
+echo "created channed ${channel_id}"
+
+channels_list=$(apiCall "channels.list" '{}' 200 ${token})
+channels_explore=$(apiCall "channels.explore" '{"source": "chat_plugin"}' 200 ${token})
 
 chatplugin_authenticate_response=$(apiCall "chatplugin.authenticate" "{\"channel_id\": \"$channel_id\"}" 200 ${token})
 chatplugin_token=$(extractFromPayload $chatplugin_authenticate_response "token")
 
+echo "authenticated via chatplugin ${chatplugin_token}"
+
 chatplugin_send_response=$(apiCall "chatplugin.send" '{"message": {"text": "Message from chatplugin"}}' 200 ${chatplugin_token})
 chatplugin_message_id=$(extractFromPayload $chatplugin_send_response "id")
 sleep 1
+
 conversation_list_response=$(apiCall "conversations.list" '{}' 200 ${token})
 conversation_id=$(extractFromPayload $conversation_list_response "data[0].id")
-conversations_info_response=$(apiCall "conversations.info" "{\"conversation_id\": \"$conversation_id\"}" 200 ${token})
-conversations_read_response=$(apiCall "conversations.read" "{\"conversation_id\": \"$conversation_id\"}" 202 ${token})
 
+echo "message sent and created conversation ${conversation_id}"
+
+conversation_info_response=$(apiCall "conversations.info" "{\"conversation_id\": \"$conversation_id\"}" 200 ${token})
+
+echo "conversation info response ${conversation_info_response}"
+
+conversations_read_response=$(apiCall "conversations.read" "{\"conversation_id\": \"$conversation_id\"}" 202 ${token})
 
 messages_send_response=$(apiCall "messages.send" "{\"conversation_id\": \"$conversation_id\", \"message\":{\"text\": \"Response from Airy\"}}" 200 ${token})
 messages_list_response=$(apiCall "messages.list" "{\"conversation_id\": \"$conversation_id\"}" 200 ${token})
 
 tags_create_response=$(apiCall "tags.create" '{"name": "tag-1", "color":"tag-red"}' 201 ${token})
 tag_id=$(extractFromPayload $tags_create_response "id")
+
+echo "tag created ${tag_id}"
+
+conversation_tag_response=$(apiCall "conversations.tag" "{\"conversation_id\": \"$conversation_id\"}" 202 ${token})
+conversation_info_response=$(apiCall "conversations.info" "{\"conversation_id\": \"$conversation_id\"}" 200 ${token})
+
+echo "conversation info response ${conversation_info_response}"
+
 tags_update_response=$(apiCall "tags.update" "{\"id\":\"$tag_id\", \"name\": \"tag-1\", \"color\": \"tag-blue\"}" 200 ${token})
 tags_list_response=$(apiCall "tags.list" '{}' 200 ${token})
 tags_delete_response=$(apiCall "tags.delete" "{\"id\": \"$tag_id\"}" 200 ${token})

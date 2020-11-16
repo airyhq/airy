8dbed712226629e2f9fddd90c6823d3f9f39af96
Pascal Holy <pascal@airy.co>
2020-11-16 16:24:42 +0100

    Fix images

--- infrastructure/scripts/airy-conf.sh
+++ infrastructure/scripts/airy-conf.sh
@@ -15,7 +15,7 @@ kubectl apply -f ~/airy-core/public-urls-config.yaml
 kubectl create configmap secrets-config --from-literal=JWT_SECRET=`cat /dev/urandom | env LC_CTYPE=C tr -dc a-z0-9 | head -c 128; echo` --dry-run=client -o yaml | kubectl apply -f -
 kubectl create configmap user-config --from-env-file=../airy.conf --dry-run=client -o yaml | kubectl apply -f -
 
-helm upgrade -f ~/airy-core/helm-chart/values.yaml airy ~/airy-core/helm-chart/ --set global.appImageTag=beta --version 0.5.0 --timeout 1000s 2>/dev/null
+helm upgrade -f ~/airy-core/helm-chart/values.yaml airy ~/airy-core/helm-chart/ --set global.appImageTag=$APP_IMAGE_TAG --version 0.5.0 --timeout 1000s 2>/dev/null
 
 kubectl scale deployment schema-registry-1 --replicas=1
 kubectl exec kafka-client -- /root/wait-for-service.sh schema-registry-1 8081 15 Schema-registry

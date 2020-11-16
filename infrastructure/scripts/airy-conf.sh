5cd58dd7d0744991076c8b4737fe1f21b42c4daf
Pascal Holy <pascal@airy.co>
2020-11-16 15:24:56 +0100

    Fix images

--- infrastructure/scripts/airy-conf.sh
+++ infrastructure/scripts/airy-conf.sh
@@ -5,10 +5,15 @@ IFS=$'\n\t'
 cd /vagrant/scripts
 mkdir -p ~/airy-core
 
+cp ../deployments/* ~/airy-core/
+
+# Generate random string for the ngrok webhook
+RANDOM_INGRESS_ID=`cat /dev/urandom | env LC_CTYPE=C tr -dc a-z0-9 | head -c 16; echo`
+sed -i "s/<ngrok_client_id>/${RANDOM_INGRESS_ID}/" ~/airy-core/public-urls-config.yaml
+kubectl apply -f ~/airy-core/public-urls-config.yaml
 
 kubectl create configmap secrets-config --from-literal=JWT_SECRET=`cat /dev/urandom | env LC_CTYPE=C tr -dc a-z0-9 | head -c 128; echo` --dry-run=client -o yaml | kubectl apply -f -
 kubectl create configmap user-config --from-env-file=../airy.conf --dry-run=client -o yaml | kubectl apply -f -
-kubectl create configmap public-urls --from-literal=RANDOM_INGRESS_ID=`cat /dev/urandom | env LC_CTYPE=C tr -dc a-z0-9 | head -c 16; echo` --dry-run=client -o yaml | kubectl apply -f -
 
 helm upgrade -f ~/airy-core/helm-chart/values.yaml airy ~/airy-core/helm-chart/ --set global.appImageTag=beta --version 0.5.0 --timeout 1000s 2>/dev/null
 

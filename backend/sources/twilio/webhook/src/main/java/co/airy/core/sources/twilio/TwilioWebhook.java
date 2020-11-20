d77d14f9cbe8bc384aa535776246e923ab89bb74
Christoph Proeschel <chris@airy.co>
2020-11-20 16:27:17 +0100

    [#353] Add Twilio webhook (#362)

---
+++ backend/sources/twilio/webhook/src/main/java/co/airy/core/sources/twilio/TwilioWebhook.java
@@ -0,0 +1,147 @@
+package co.airy.core.twilio;
+
+import co.airy.kafka.schema.source.SourceTwilioEvents;
+import co.airy.spring.kafka.healthcheck.ProducerHealthCheck;
+import com.twilio.security.RequestValidator;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+
+import static java.util.stream.Collectors.toMap;
+
+@RestController
+public class TwilioWebhook implements HealthIndicator, DisposableBean {
+
+    private final String sourceTwilioEvents = new SourceTwilioEvents().name();
+
+    private final Producer<String, String> producer;
+    private final String authToken;
+    private final ProducerHealthCheck producerHealthCheck;
+
+    public TwilioWebhook(ProducerHealthCheck producerHealthCheck,
+                         @Value("${twilio.token}") String authToken,
+                         @Value("${kafka.brokers}") String brokers) {
+        this.producerHealthCheck = producerHealthCheck;
+        this.authToken = authToken;
+
+        final Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        props.put(ProducerConfig.RETRIES_CONFIG, String.valueOf(Integer.MAX_VALUE));
+        props.put(ProducerConfig.ACKS_CONFIG, "1");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        producer = new KafkaProducer<>(props);
+    }
+
+
+    public Health health() {
+        try {
+            producerHealthCheck.sendHealthCheck();
+            return Health.up().build();
+        } catch (Exception e) {
+            return Health.down(e).build();
+        }
+    }
+
+    @PostMapping(
+            path = "/twilio",
+            consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    ResponseEntity<Void> accept(
+            HttpServletRequest request
+    ) throws Exception {
+
+        final String content = getRequestBody(request);
+
+        final Map<String, String> params = parseUrlEncoded(content);
+        final String requestUrl = getFullRequestUrl(request);
+
+        if (!isRequestAuthenticated(requestUrl, params, request.getHeader("X-Twilio-Signature"))) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
+        final String requestId = UUID.randomUUID().toString();
+
+        try {
+            ProducerRecord<String, String> record = new ProducerRecord<>(sourceTwilioEvents, requestId, content);
+            producer.send(record).get();
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+        }
+    }
+
+    private String getRequestBody(final HttpServletRequest request) throws IOException {
+        final StringBuilder builder = new StringBuilder();
+        final BufferedReader reader = request.getReader();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            builder.append(line);
+        }
+        return builder.toString();
+    }
+
+
+    static Map<String, String> parseUrlEncoded(String payload) {
+        List<String> kvPairs = Arrays.asList(payload.split("\\&"));
+
+        return kvPairs.stream()
+                .map((kvPair) -> {
+                    String[] fields = kvPair.split("=");
+                    String name = URLDecoder.decode(fields[0], StandardCharsets.UTF_8);
+
+                    // For the validation we need empty headers to be present
+                    String value = "";
+                    if (fields.length > 1) {
+                        value = URLDecoder.decode(fields[1], StandardCharsets.UTF_8);
+                    }
+
+                    return List.of(name, value);
+                })
+                .collect(toMap(
+                        (tuple) -> tuple.get(0),
+                        (tuple) -> tuple.get(1)
+                ));
+    }
+
+    private String getFullRequestUrl(HttpServletRequest request) {
+        return request.getRequestURL().toString() + (request.getQueryString() == null ? "" : "?" + request.getQueryString());
+    }
+
+    private boolean isRequestAuthenticated(String requestUrl, Map<String, String> params, String headerSignature) {
+        final RequestValidator validator = new RequestValidator(authToken);
+        return validator.validate(requestUrl, params, headerSignature);
+    }
+
+    @Override
+    public void destroy() {
+        if (producer != null) {
+            producer.close(Duration.ofSeconds(10));
+        }
+    }
+}
+
---
+++ backend/sources/twilio/webhook/src/test/java/co/airy/core/sources/twilio/TwilioControllerIntegrationTest.java
@@ -0,0 +1,132 @@
+package co.airy.core.sources.twilio;
+
+import co.airy.kafka.schema.source.SourceTwilioEvents;
+import co.airy.kafka.test.KafkaTestHelper;
+import co.airy.kafka.test.junit.SharedKafkaTestResource;
+import co.airy.spring.core.AirySpringBootApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = AirySpringBootApplication.class,
+        properties = {
+                "twilio.token=whatever",
+        })
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+class TwilioControllerIntegrationTest {
+    @RegisterExtension
+    public static final SharedKafkaTestResource sharedKafkaTestResource = new SharedKafkaTestResource();
+    private static KafkaTestHelper testHelper;
+    private static final SourceTwilioEvents sourceTwilioEvents = new SourceTwilioEvents();
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Value("${twilio.token}")
+    private String twilioAuthToken;
+
+    @BeforeAll
+    static void beforeAll() throws Exception {
+        testHelper = new KafkaTestHelper(sharedKafkaTestResource, sourceTwilioEvents);
+        testHelper.beforeAll();
+    }
+
+    @Test
+    void failsForUnauthenticated() throws Exception {
+        final Map<String, String> parameters = Map.of(
+                "ApiVersion", "2010-04-01",
+                "SmsSid", "MM3753d789407e9d3f4c0a8b919ec5473f"
+        );
+
+        final String validationSignature = getValidationSignature("http://localhost/twilio", parameters, "DROP TABLE");
+
+        mvc.perform(post("/twilio")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .header("X-Twilio-Signature", validationSignature)
+                .content("ApiVersion=2010-04-01&SmsSid=MM3753d789407e9d3f4c0a8b919ec5473f")
+        )
+                .andExpect(status().isForbidden());
+    }
+
+
+    @Test
+    void canAcceptRequests() throws Exception {
+        final Map<String, String> parameters = Map.of(
+                "ApiVersion", "2010-04-01",
+                "SmsSid", "MM3753d789407e9d3f4c0a8b919ec5473f"
+        );
+
+        final String validationSignature = getValidationSignature("http://localhost/twilio", parameters, twilioAuthToken);
+
+        mvc.perform(post("/twilio")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .content("ApiVersion=2010-04-01&SmsSid=MM3753d789407e9d3f4c0a8b919ec5473f")
+                .header("X-Twilio-Signature", validationSignature)
+        )
+                .andExpect(status().isOk());
+
+        List<String> records = testHelper.consumeValues(1, sourceTwilioEvents.name());
+
+        assertThat(records, hasSize(1));
+    }
+
+    private static final String HMAC = "HmacSHA1";
+
+    // Adapted from com.twilio.security.RequestValidator only for testing
+    private String getValidationSignature(String url, Map<String, String> params, String authToken) {
+        try {
+            final SecretKeySpec signingKey = new SecretKeySpec(authToken.getBytes(), HMAC);
+
+            StringBuilder builder = new StringBuilder(url);
+            if (params != null) {
+                List<String> sortedKeys = new ArrayList<>(params.keySet());
+                Collections.sort(sortedKeys);
+
+                for (String key : sortedKeys) {
+                    builder.append(key);
+
+                    String value = params.get(key);
+                    builder.append(value == null ? "" : value);
+                }
+            }
+
+            Mac mac = Mac.getInstance(HMAC);
+            mac.init(signingKey);
+
+            byte[] rawHmac = mac.doFinal(builder.toString().getBytes(StandardCharsets.UTF_8));
+            return DatatypeConverter.printBase64Binary(rawHmac);
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @AfterAll
+    static void afterAll() throws Exception {
+        testHelper.afterAll();
+    }
+}

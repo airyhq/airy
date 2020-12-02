799a75ef26a7ff173ed1db89a0fbac4cd62d92e5
Bodo Tasche <bodo@wannawork.de>
2020-12-02 12:17:17 +0100

    prettify generated code

--- frontend/types/content.ts
+++ frontend/types/content.ts
@@ -3,12 +3,12 @@
 // Generated using typescript-generator version 2.26.723 on 2020-12-02 10:41:15.
 
 export interface Content {
-    type: "text";
+  type: 'text';
 }
 
 export interface Text extends Content {
-    type: "text";
-    text: string;
+  type: 'text';
+  text: string;
 }
 
 export type ContentUnion = Text;

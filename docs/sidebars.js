73903057f0b8a3b22616aa716862f80611646e12
lucapette <ciao@lucapette.me>
2020-12-10 11:03:02 +0100

    Restructure all the things

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -28,7 +28,6 @@ module.exports = {
           Deployment: ['guides/airy-core-in-test-env', 'guides/airy-core-in-production'],
         },
         'guides/airy-core-and-rasa',
-        'guides/airy-core-and-facebook',
       ],
     },
 

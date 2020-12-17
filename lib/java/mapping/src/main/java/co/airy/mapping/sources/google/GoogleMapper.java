aa226b28566c535ebd02b1a6b690242b1eeb27c2
Paulo Diniz <paulo.rc.diniz@gmail.com>
2020-12-17 13:49:39 +0100

    Added Image content model for gogle (#531)
    * Added Image content model for gogle
    
    * Checking host

--- lib/java/mapping/src/main/java/co/airy/mapping/sources/google/GoogleMapper.java
+++ lib/java/mapping/src/main/java/co/airy/mapping/sources/google/GoogleMapper.java
@@ -2,12 +2,19 @@ package co.airy.mapping.sources.google;
 
 import co.airy.mapping.SourceMapper;
 import co.airy.mapping.model.Content;
+import co.airy.mapping.model.Image;
 import co.airy.mapping.model.Text;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toMap;
 
 @Component
 public class GoogleMapper implements SourceMapper {
@@ -26,6 +33,43 @@ public class GoogleMapper implements SourceMapper {
     public List<Content> render(String payload) throws Exception {
         final JsonNode jsonNode = objectMapper.readTree(payload);
         final JsonNode messageNode = jsonNode.get("message");
-        return List.of(new Text(messageNode.get("text").textValue()));
+
+        final String messageNodeValue = messageNode.get("text").textValue();
+        if(isGoogleStorageUrl(messageNodeValue)) {
+            return List.of(new Image(messageNodeValue));
+        } else {
+            return List.of(new Text(messageNodeValue));
+        }
     }
+
+    private boolean isGoogleStorageUrl(final String url) {
+        URI uri;
+        try {
+            uri = new URI(url);
+        } catch (URISyntaxException e) {
+            return false;
+        }
+
+        if(!uri.getHost().startsWith("storage.googleapis.com")) {
+            return false;
+        }
+
+        final Map<String, String> params = List.of(uri.getQuery().split("&"))
+                .stream()
+                .map(param -> param.split("="))
+                .map(l -> Map.of(l[0], l[1]))
+                .flatMap(map -> map.entrySet().stream())
+                .collect(toMap(
+                        m -> m.getKey().toLowerCase(),
+                        Map.Entry::getValue,
+                        (s1, s2) -> s2));
+
+        return !params.get("x-goog-algorithm").isEmpty()
+                && !params.get("x-goog-credential").isEmpty()
+                && !params.get("x-goog-date").isEmpty()
+                && !params.get("x-goog-expires").isEmpty()
+                && !params.get("x-goog-signedheaders").isEmpty()
+                && !params.get("x-goog-signature").isEmpty();
+    }
+
 }

91b318f62f4f5b4c42f5d0d1dd2e87a7f508eb34
Paulo Diniz <paulo.rc.diniz@gmail.com>
2020-12-18 15:48:55 +0100

    [#496] Added Image content model for Facebook (#539)
    * [#496] Added Image content model for Facebook

--- lib/java/mapping/src/main/java/co/airy/mapping/sources/facebook/FacebookMapper.java
+++ lib/java/mapping/src/main/java/co/airy/mapping/sources/facebook/FacebookMapper.java
@@ -2,11 +2,13 @@ package co.airy.mapping.sources.facebook;
 
 import co.airy.mapping.SourceMapper;
 import co.airy.mapping.model.Content;
+import co.airy.mapping.model.Image;
 import co.airy.mapping.model.Text;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -27,6 +29,23 @@ public class FacebookMapper implements SourceMapper {
     public List<Content> render(String payload) throws Exception {
         final JsonNode jsonNode = objectMapper.readTree(payload);
         final JsonNode messageNode = jsonNode.get("message");
-        return List.of(new Text(messageNode.get("text").textValue()));
+
+        List<Content> contents = new ArrayList<>();
+
+        if (messageNode.get("text") != null) {
+            contents.add(new Text(messageNode.get("text").textValue()));
+        }
+
+        if (messageNode.get("attachments") != null) {
+            messageNode.get("attachments")
+                    .elements()
+                    .forEachRemaining(attachmentNode -> {
+                        if ("image".equalsIgnoreCase(attachmentNode.get("type").textValue())) {
+                            contents.add(new Image(attachmentNode.get("payload").get("url").textValue()));
+                        }
+                    });
+        }
+
+        return contents;
     }
 }
--- lib/java/mapping/src/test/java/co/airy/mapping/FacebookTest.java
+++ lib/java/mapping/src/test/java/co/airy/mapping/FacebookTest.java
@@ -1,11 +1,14 @@
 package co.airy.mapping;
 
+import co.airy.mapping.model.Content;
+import co.airy.mapping.model.Image;
 import co.airy.mapping.model.Text;
 import co.airy.mapping.sources.facebook.FacebookMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.StreamUtils;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -22,5 +25,15 @@ public class FacebookTest {
         final Text message = (Text) mapper.render(sourceContent).get(0);
 
         assertThat(message.getText(), equalTo(text));
+    }
+
+    @Test
+    void canRenderImages() throws Exception {
+        final String imageUrl = "https://url-from-facebook.com/123-id";
+        final String sourceContent = String.format(StreamUtils.copyToString(getClass().getClassLoader().getResourceAsStream("facebook/image.json"), StandardCharsets.UTF_8), imageUrl);
+
+        final List<Content> contents = mapper.render(sourceContent);
+        final Image image = (Image) contents.get(0);
+        assertThat(image.getUrl(), equalTo(imageUrl));
     }
 }

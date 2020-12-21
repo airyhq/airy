7837114639a3ca1f74df60fc747fcf485ce835c7
Paulo Diniz <paulo.rc.diniz@gmail.com>
2020-12-21 11:14:58 +0100

    [#223] Future of `/channels.explore`

---
+++ backend/sources/facebook/connector/src/main/java/co/airy/core/sources/facebook/WebhookController.java
@@ -0,0 +1,76 @@
+package co.airy.core.sources.facebook;
+
+import co.airy.kafka.schema.source.SourceFacebookEvents;
+import co.airy.spring.kafka.healthcheck.ProducerHealthCheck;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+import java.util.Properties;
+import java.util.UUID;
+
+@RestController
+public class WebhookController implements DisposableBean {
+    private final String sourceFacebookEvents = new SourceFacebookEvents().name();
+    private final String webhookSecret;
+
+    private final Producer<String, String> producer;
+    private final ProducerHealthCheck producerHealthCheck;
+
+    WebhookController(ProducerHealthCheck producerHealthCheck,
+                      @Value("${kafka.brokers}") String brokers,
+                      @Value("${facebook.webhook-secret}") String webhookSecret) {
+        this.producerHealthCheck = producerHealthCheck;
+        this.webhookSecret = webhookSecret;
+        final Properties props = new Properties();
+
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        props.put(ProducerConfig.RETRIES_CONFIG, String.valueOf(Integer.MAX_VALUE));
+        props.put(ProducerConfig.ACKS_CONFIG, "1");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        producer = new KafkaProducer<>(props);
+    }
+
+    // https://developers.facebook.com/docs/graph-api/webhooks/getting-started
+    @GetMapping("/facebook")
+    int verify(@RequestParam(value = "hub.challenge") int challenge, @RequestParam(value = "hub.verify_token") String verifyToken) {
+        if (!verifyToken.equals(this.webhookSecret)) {
+            return 0;
+        }
+
+        return challenge;
+    }
+
+    @PostMapping("/facebook")
+    ResponseEntity<Void> accept(@RequestBody String event) {
+        String requestId = UUID.randomUUID().toString();
+        try {
+            ProducerRecord<String, String> record = new ProducerRecord<>(sourceFacebookEvents, requestId, event);
+            producer.send(record).get();
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+        }
+    }
+
+    @Override
+    public void destroy() {
+        if (producer != null) {
+            producer.close(Duration.ofSeconds(10));
+        }
+    }
+}
---
+++ backend/sources/facebook/connector/src/test/java/co/airy/core/sources/facebook/WebhookControllerTest.java
@@ -0,0 +1,68 @@
+package co.airy.core.sources.facebook;
+
+import co.airy.kafka.schema.Topic;
+import co.airy.kafka.schema.source.SourceFacebookEvents;
+import co.airy.kafka.test.KafkaTestHelper;
+import co.airy.kafka.test.junit.SharedKafkaTestResource;
+import co.airy.spring.core.AirySpringBootApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(
+        properties = {
+                "facebook.webhook-secret=theansweris42"
+        },
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = AirySpringBootApplication.class)
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+class WebhookControllerTest {
+    @RegisterExtension
+    public static final SharedKafkaTestResource sharedKafkaTestResource = new SharedKafkaTestResource();
+
+    private static KafkaTestHelper kafkaTestHelper;
+
+    private static final Topic sourceFacebookEvents = new SourceFacebookEvents();
+
+    @Autowired
+    private MockMvc mvc;
+
+    @BeforeAll
+    static void beforeAll() throws Exception {
+        kafkaTestHelper = new KafkaTestHelper(sharedKafkaTestResource, sourceFacebookEvents);
+
+        kafkaTestHelper.beforeAll();
+    }
+
+    @Test
+    void returns200() throws Exception {
+        mvc.perform(post("/facebook")
+                .content("whatever"))
+                .andExpect(status().isOk());
+
+        List<String> records = kafkaTestHelper.consumeValues(1, sourceFacebookEvents.name());
+
+        assertThat(records, hasSize(1));
+        assertEquals("whatever", records.get(0));
+    }
+
+    @AfterAll
+    static void afterAll() throws Exception {
+        kafkaTestHelper.afterAll();
+    }
+}

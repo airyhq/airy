973d741ff74631091512ea1b0cb9d97de6e23cb2
lucapette <ciao@lucapette.me>
2020-12-21 15:36:12 +0100

    Introduce twilio connector

---
+++ backend/sources/twilio/connector/src/main/java/co/airy/core/sources/twilio/Stores.java
@@ -0,0 +1,82 @@
+package co.airy.core.sources.twilio;
+
+import co.airy.avro.communication.Channel;
+import co.airy.avro.communication.ChannelConnectionState;
+import co.airy.avro.communication.DeliveryState;
+import co.airy.avro.communication.Message;
+import co.airy.avro.communication.SenderType;
+import co.airy.core.sources.twilio.dto.SendMessageRequest;
+import co.airy.kafka.schema.application.ApplicationCommunicationChannels;
+import co.airy.kafka.schema.application.ApplicationCommunicationMessages;
+import co.airy.kafka.streams.KafkaStreamsWrapper;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Stores implements DisposableBean, ApplicationListener<ApplicationReadyEvent> {
+    private static final String appId = "sources.twilio.ConnectorStores";
+
+    private final KafkaStreamsWrapper streams;
+    private final Connector connector;
+
+    Stores(KafkaStreamsWrapper streams, Connector connector) {
+        this.streams = streams;
+        this.connector = connector;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        // Channels table
+        KTable<String, Channel> channelsTable = builder.<String, Channel>table(new ApplicationCommunicationChannels().name())
+                .filter((sourceChannelId, channel) -> channel.getSource().startsWith("twilio")
+                        && channel.getConnectionState().equals(ChannelConnectionState.CONNECTED));
+
+        final KStream<String, Message> messageStream = builder.<String, Message>stream(new ApplicationCommunicationMessages().name())
+                .filter((messageId, message) -> message.getSource().startsWith("twilio"))
+                .selectKey((messageId, message) -> message.getConversationId());
+
+        final KTable<String, SendMessageRequest> contextTable = messageStream
+                .groupByKey()
+                .aggregate(SendMessageRequest::new,
+                        (conversationId, message, aggregate) -> {
+                            if (SenderType.SOURCE_CONTACT.equals(message.getSenderType())) {
+                                aggregate.setSourceConversationId(message.getSenderId());
+                            }
+
+                            aggregate.setChannelId(message.getChannelId());
+
+                            return aggregate;
+                        })
+                .join(channelsTable, SendMessageRequest::getChannelId, (aggregate, channel) -> {
+                    aggregate.setChannel(channel);
+                    return aggregate;
+                });
+
+        messageStream.filter((messageId, message) -> DeliveryState.PENDING.equals(message.getDeliveryState()))
+                .join(contextTable, (message, sendMessageRequest) -> sendMessageRequest.toBuilder().message(message).build())
+                .mapValues(connector::sendMessage)
+                .to(new ApplicationCommunicationMessages().name());
+
+        streams.start(builder.build(), appId);
+    }
+
+    @Override
+    public void destroy() {
+        if (streams != null) {
+            streams.close();
+        }
+    }
+
+    // visible for testing
+    KafkaStreams.State getStreamState() {
+        return streams.state();
+    }
+}

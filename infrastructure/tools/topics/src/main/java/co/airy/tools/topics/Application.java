b8927814c17e8d928b8cfc12de057d9606ce22f2
lucapette <ciao@lucapette.me>
2020-12-29 12:12:31 +0100

    [#526] Introduce namespacing for topics (#566)

--- infrastructure/tools/topics/src/main/java/co/airy/tools/topics/Application.java
+++ infrastructure/tools/topics/src/main/java/co/airy/tools/topics/Application.java
@@ -1,9 +1,5 @@
 package co.airy.tools.topics;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
@@ -16,15 +12,38 @@ import static java.util.stream.Collectors.joining;
 public class Application {
 
     public static void main(String[] args) {
+        String createTopicTemplate = "kafka-topics --create --if-not-exists --zookeeper ${ZOOKEEPER} --replication-factor ${REPLICAS} --partitions ${PARTITIONS} --topic \"${AIRY_CORE_NAMESPACE}%s\" %s 1>/dev/null";
+        String headerTemplate = "#!/bin/bash\n" +
+                "\n" +
+                "##########################################################################\n" +
+                "# THIS FILE WAS GENERATED. DO NOT EDIT. See /infrastructure/tools/topics #\n" +
+                "##########################################################################\n" +
+                "\n" +
+                "set -euo pipefail\n" +
+                "IFS=$'\\n\\t'\n" +
+                "\n" +
+                "ZOOKEEPER=airy-cp-zookeeper:2181\n" +
+                "PARTITIONS=${PARTITIONS:-10}\n" +
+                "REPLICAS=${REPLICAS:-1}\n" +
+                "AIRY_CORE_NAMESPACE=${AIRY_CORE_NAMESPACE:-}\n" +
+                "\n" +
+                "echo \"Creating Kafka topics\"\n" +
+                "\n" +
+                "if [ -n \"${AIRY_CORE_NAMESPACE}\" ]\n" +
+                "then\n" +
+                "  AIRY_CORE_NAMESPACE=\"${AIRY_CORE_NAMESPACE}.\"\n" +
+                "  echo \"Using ${AIRY_CORE_NAMESPACE} to namespace topics\"\n" +
+                "fi";
+
         TopicsFinder finder = new TopicsFinder();
 
-        String template = getTemplate();
 
         List<String> topics = finder.findTopics();
 
         Method name;
         Method config;
 
+        System.out.println(headerTemplate + "\n\n");
         try {
             for (String result : topics) {
                 Class<?> topicClass = Class.forName(result);
@@ -51,31 +70,10 @@ public class Application {
                     topicConfigFormatted = "--config " + topicConfigFormatted;
                 }
 
-                System.out.println(String.format(template, topicName, topicConfigFormatted));
+                System.out.println(String.format(createTopicTemplate, topicName, topicConfigFormatted) + "\n");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    private static String getTemplate() {
-        String line = "";
-        StringBuilder buffer = new StringBuilder();
-
-        try {
-            try (InputStream is = (Application.class.getResourceAsStream("/topic.sh.tpl"))) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line).append("\n");
-                }
-
-                reader.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return buffer.toString();
-    }
-
 }

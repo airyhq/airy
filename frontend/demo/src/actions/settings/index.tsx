95fd7482a7e4bb4b883aecab3224f9d6b1604d55
Audrey Kadjar <audreykadjar@gmail.com>
2020-12-29 17:37:28 +0100

    made small edits

--- frontend/demo/src/actions/settings/index.tsx
+++ frontend/demo/src/actions/settings/index.tsx
@@ -1,16 +1,5 @@
 import _, {Dispatch} from 'redux';
-
-const fakeData = () => {
-  return {
-    colors: {
-      'tag-green': {default: '0E764F', background: 'F5FFFB', font: '0E764F', position: 3, border: '0E764F'},
-      'tag-blue': {default: '1578D4', background: 'F1FAFF', font: '1578D4', position: 1, border: '1578D4'},
-      'tag-red': {default: 'E0243A', background: 'FFF7F9', font: 'E0243A', position: 2, border: 'E0243A'},
-      'tag-purple': {default: '730A80', background: 'FEF7FF', font: '730A80', position: 4, border: '730A80'},
-      enabled: true,
-    },
-  };
-};
+import {fakeData} from '../../pages/Tags/FAKESETTINGS';
 
 export const ADD_SETTINGS_TO_STORE = 'ADD_SETTINGS_TO_STORE';
 

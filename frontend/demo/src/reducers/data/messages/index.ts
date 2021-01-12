04af1d9d9c0e5540d32893c7363cb5e5ce8b801d
Thorsten <thorsten@airy.co>
2021-01-12 15:27:14 +0100

    [#400] Load messages of conversations (#567)
    * added structure
    
    * messages are displayed
    
    * better structure for messages in redux
    
    * messages being displayed properly
    
    * eslint and some small fixes
    
    * better loading of messages
    
    * fix some suggestions
    
    * linting
    
    * remove unused code
    
    * use HttpClient for messages
    
    * restore right version
    
    * revision of code
    
    Co-authored-by: Aitor Algorta <aitor@airy.co>

---
+++ frontend/demo/src/reducers/data/messages/index.ts
@@ -0,0 +1,43 @@
+import {ActionType, getType} from 'typesafe-actions';
+
+import * as actions from '../../../actions/messages';
+import {Message} from 'httpclient';
+import {DataState} from '..';
+import _ from 'lodash-es';
+
+type Action = ActionType<typeof actions>;
+
+export type MessagesState = {
+  data: DataState;
+};
+
+export type MessageById = {
+  [messageId: string]: Message;
+};
+
+export type Messages = {
+  all: {[conversationId: string]: MessageById};
+};
+
+const initialState = {
+  all: {},
+};
+
+function organiseMessages(messages: Message[]): MessageById {
+  return _.keyBy(messages, 'id');
+}
+
+export default function messagesReducer(state = initialState, action: Action): any {
+  switch (action.type) {
+    case getType(actions.loadingMessagesAction):
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [action.payload.conversationId]: organiseMessages(action.payload.messages),
+        },
+      };
+    default:
+      return state;
+  }
+}

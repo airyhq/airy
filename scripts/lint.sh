4bde5d3bbb7a19099a00656378064c116343571a
Christoph Pröschel <chris@airy.co>
2021-01-14 10:20:43 +0100

    Profile linting

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,10 +6,14 @@ echo "Check create-topics.sh is in sync"
 cmp <(bazel run //infrastructure/tools/topics:app) infrastructure/scripts/provision/create-topics.sh
 echo
 echo "Running Bazel lint"
-bazel run @com_github_airyhq_bazel_tools//code-format:check_buildifier
+bazel run  --profile buildifier.log @com_github_airyhq_bazel_tools//code-format:check_buildifier
+echo "Buildifier profile"
+bazel analyze-profile buildifier.log
 echo
 echo "Running Prettier and Java tests"
-bazel test --test_tag_filters=lint //...
+bazel test --profile lint_tests.log --test_tag_filters=lint //...
+echo "Lint tests profile"
+bazel analyze-profile lint_tests.log
 echo
 echo "Running eslint"
 yarn eslint

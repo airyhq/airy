e9a5a8f96605acc43e80ac16a7cc867e77575e3c
lucapette <ciao@lucapette.me>
2021-01-20 15:58:39 +0100

    Prepare the code for more scalable integration tests

---
+++ infrastructure/cli/integration/version_test.go
@@ -0,0 +1,40 @@
+package integration
+
+import (
+	"os/exec"
+	"testing"
+
+	"reflect"
+)
+
+func TestVersion(t *testing.T) {
+	tests := []struct {
+		name    string
+		args    []string
+		golden  string
+		wantErr bool
+	}{
+		{"version", []string{"version", "--config", "golden/cli.yaml"}, "cli.version", false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(testing *testing.T) {
+			cmd := exec.Command(binaryName, tt.args...)
+			output, err := cmd.CombinedOutput()
+			actual := string(output)
+			if (err != nil) != tt.wantErr {
+				if tt.wantErr {
+					t.Fatalf("Test %s expected to fail but did not. Error message: %v Output: %s\n", tt.name, err, actual)
+				} else {
+					t.Fatalf("Test %s expected to pass but did not. Error message: %v Output: %s\n", tt.name, err, actual)
+				}
+			}
+			golden := NewGoldenFile(t, tt.golden)
+			expected := golden.Load()
+
+			if !reflect.DeepEqual(actual, expected) {
+				t.Fatalf("diff: %v", Diff(actual, expected))
+			}
+		})
+	}
+}

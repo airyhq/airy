c43aadb446c1451c5aa3d54bf362f87c19cd065d
Kazeem Adetunji <kazeem@airy.co>
2021-01-27 10:31:07 +0100

    [#654] Replace scss files with scss modules (#753)
    * Initial commit
    
    * Replaced scss styles with scss modules
    
    * Import name changed

--- frontend/chat-plugin/src/airyRenderProps/AiryMessage/index.tsx
+++ frontend/chat-plugin/src/airyRenderProps/AiryMessage/index.tsx
@@ -1,7 +1,7 @@
 import {h} from 'preact';
 import linkifyString from 'linkifyjs/string';
 
-import style from './index.module.scss';
+import styles from './index.module.scss';
 
 type AiryMessageProps = {
   message: {
@@ -20,12 +20,12 @@ type AiryMessageProps = {
 const AiryMessage = ({message}: AiryMessageProps) => {
   const isInbound = message.sender_type === 'source_contact';
   const messageDisplay = linkifyString(message.content[0].text, {
-    className: `${isInbound ? style.messageLinkRight : style.messageLinkLeft}`,
+    className: `${isInbound ? styles.messageLinkRight : styles.messageLinkLeft}`,
   });
 
   return (
-    <div className={`${isInbound ? style.containerRight : style.containerLeft}`}>
-      <div className={`${isInbound ? style.bubbleRight : style.bubbleLeft}`}>
+    <div className={`${isInbound ? styles.containerRight : styles.containerLeft}`}>
+      <div className={`${isInbound ? styles.bubbleRight : styles.bubbleLeft}`}>
         <div dangerouslySetInnerHTML={{__html: messageDisplay}} />
       </div>
     </div>

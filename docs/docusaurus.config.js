7ad7bd59e498b55e97e795e3bf69ff0a7071ef78
lucapette <ciao@lucapette.me>
2021-01-29 09:58:49 +0100

    [#491] Do not show the edit button for docs
    Until we sort out how we want to handle versioned docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -39,7 +39,6 @@ module.exports = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/airyhq/airy/edit/main/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),

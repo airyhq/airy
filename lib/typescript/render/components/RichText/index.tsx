e7ccb09a7fd8f651b64d86de4e23ce8efbcc6588
thorstenairy <stayprod.io@gmail.com>
2021-02-05 16:53:16 +0100

    run prettier

--- lib/typescript/render/components/RichText/index.tsx
+++ lib/typescript/render/components/RichText/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import styles from './index.module.scss';
 import {Avatar} from '../Avatar';
-import {Message, SenderType} from 'httpclient';
-import {isFromContact} from 'httpclient';
+import {Message} from 'httpclient';
 import {DefaultMessageRenderingProps} from '..';
 
 type RichTextRenderProps = DefaultMessageRenderingProps & {
@@ -10,24 +9,23 @@ type RichTextRenderProps = DefaultMessageRenderingProps & {
 };
 
 export const RichText = (props: RichTextRenderProps) => {
-  const {message, conversation, sentAt, showAvatar} = props;
+  const {message, fromContact, sentAt, contact} = props;
   const messageJSON = JSON.parse(message.content);
   const messageText = messageJSON.text;
   const messageFallback = messageJSON.fallback;
   const messageIsRichText = messageJSON.containsRichText;
-  const isAppUser = message.senderType == SenderType.appUser;
 
   return (
     <div className={styles.messageListItemContainer}>
       <div className={styles.messageListItem} id={`message-item-${message.id}`}>
-        {isAppUser ? (
+        {!fromContact ? (
           <div className={styles.messageListItemMember}>
             <div className={styles.messageListItemMemberText}>{messageIsRichText ? messageText : messageFallback}</div>
             {sentAt && <div className={styles.messageTime}>{sentAt}</div>}
           </div>
         ) : (
           <div className={styles.messageListUserContainer}>
-            <div className={styles.messageAvatar}>{showAvatar && <Avatar contact={conversation.contact} />} </div>
+            <div className={styles.messageAvatar}>{contact && <Avatar contact={contact} />}</div>
             <div className={styles.messageListItemUser}>
               <div className={styles.messageListItemUserText}>{messageIsRichText ? messageText : messageFallback}</div>
               {sentAt && <div className={styles.messageTime}>{sentAt}</div>}

d11eab4fa022dab8f6216a84da0342eb9c8ab93b
Christoph Pröschel <chris@airy.co>
2021-02-05 13:57:42 +0100

    Map json payload to update records

--- backend/model/metadata/src/main/java/co/airy/model/metadata/MetadataObjectMapper.java
+++ backend/model/metadata/src/main/java/co/airy/model/metadata/MetadataObjectMapper.java
@@ -3,16 +3,25 @@ package co.airy.model.metadata;
 import co.airy.avro.communication.Metadata;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Comparator.comparing;
 
 public class MetadataObjectMapper {
     public static JsonNode getMetadataPayload(List<Metadata> metadataList) {
-        metadataList.sort(comparing(metadata -> ((Metadata) metadata).getTimestamp()));
+        metadataList.sort(comparing(Metadata::getTimestamp));
 
         final ObjectNode root = JsonNodeFactory.instance.objectNode();
         for (Metadata metadata : metadataList) {
@@ -34,15 +43,60 @@ public class MetadataObjectMapper {
 
             ObjectNode nextRootNode;
             // Check if there is an existing object node we can use
+            // If the existing node is not an object we overwrite it
             final JsonNode existingNode = root.get(nodeName);
             if (existingNode != null && existingNode.isObject()) {
                 nextRootNode = (ObjectNode) existingNode;
             } else {
                 nextRootNode = JsonNodeFactory.instance.objectNode();
+                root.set(nodeName, nextRootNode);
             }
-
             applyMetadata(nextRootNode, remainingNodes, value);
-            root.set(nodeName, nextRootNode);
+
         }
+    }
+
+    public static List<Metadata> getMetadataFromJson(Subject subject, JsonNode payload) throws Exception {
+        final long creationTime = Instant.now().toEpochMilli();
+
+        return getKeyValuePairs(payload, "")
+                .stream()
+                .map((keyValuePair ->
+                        Metadata.newBuilder()
+                                .setSubject(subject.toString())
+                                .setKey(keyValuePair.getKey())
+                                .setValue(keyValuePair.getValue())
+                                .setTimestamp(creationTime)
+                                .build()
+                )).collect(Collectors.toList());
+    }
+
+    private static List<KeyValuePair> getKeyValuePairs(JsonNode payload, String prefixPath) throws Exception {
+        final Iterator<Map.Entry<String, JsonNode>> nodes = payload.fields();
+
+        List<KeyValuePair> results = new ArrayList<>();
+        while (nodes.hasNext()) {
+            Map.Entry<String, JsonNode> entry = nodes.next();
+            final JsonNode node = entry.getValue();
+
+            // i.e. prefixPath = contacts. node key = displayName
+            final String currentPath = prefixPath + entry.getKey();
+            if (node.isTextual()) {
+                results.add(new KeyValuePair(currentPath, node.textValue()));
+            } else if (node.isObject()) {
+                results.addAll(getKeyValuePairs(node, currentPath + "."));
+            } else {
+                throw new Exception(String.format("node \"%s\" needs to be of type String or Object (is: \"%s\")", entry.getKey(), node.getNodeType()));
+            }
+        }
+
+        return results;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class KeyValuePair {
+        private String key;
+        private String value;
     }
 }
--- backend/model/metadata/src/test/java/co/airy/model/metadata/ObjectMapperTest.java
+++ backend/model/metadata/src/test/java/co/airy/model/metadata/ObjectMapperTest.java
@@ -1,17 +1,24 @@
 package co.airy.model.metadata;
 
 import co.airy.avro.communication.Metadata;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static co.airy.model.metadata.MetadataObjectMapper.getMetadataFromJson;
 import static co.airy.model.metadata.MetadataObjectMapper.getMetadataPayload;
 import static co.airy.model.metadata.MetadataRepository.newConversationMetadata;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ObjectMapperTest {
 
@@ -29,7 +36,7 @@ public class ObjectMapperTest {
         assertThat(payload.get("tags").get("1234").textValue(), equalTo(""));
     }
 
-    //@Test
+    @Test
     void canResolveConflictsByTime() {
         final Metadata oldMetadata = newConversationMetadata("id", "contact.address.line_1", "Mission Street");
         final Metadata newMetadata = newConversationMetadata("id", "contact.address", "Anklamer Str.");
@@ -44,5 +51,34 @@ public class ObjectMapperTest {
         final JsonNode payload = getMetadataPayload(metadata);
         assertThat(payload.get("contact").get("address").isTextual(), equalTo(true));
         assertThat(payload.get("contact").get("address").textValue(), equalTo("Anklamer Str."));
+    }
+
+    @Test
+    void canMapObjectToMetadata() throws Exception {
+        final String updateJson = "{" +
+                "  \"contact\": {" +
+                "    \"displayName\": \"Grace\"," +
+                "    \"address\": {" +
+                "      \"line_1\": \"Mission Street\"" +
+                "    }" +
+                "  }," +
+                "  \"tags\": {" +
+                "    \"1234\": \"\"" +
+                "  }" +
+                "}";
+        final JsonNode node = new ObjectMapper().readTree(updateJson);
+
+        final List<Metadata> metadataList = getMetadataFromJson(new Subject("conversation", "id"), node);
+
+        assertThat(metadataList, hasSize(3));
+        assertTrue(metadataList.stream().anyMatch((metadata ->
+                metadata.getKey().equals("contact.displayName") && metadata.getValue().equals("Grace")
+        )));
+        assertTrue(metadataList.stream().anyMatch((metadata ->
+                metadata.getKey().equals("contact.address.line_1") && metadata.getValue().equals("Mission Street")
+        )));
+        assertTrue(metadataList.stream().anyMatch((metadata ->
+                metadata.getKey().equals("tags.1234") && metadata.getValue().equals("")
+        )));
     }
 }

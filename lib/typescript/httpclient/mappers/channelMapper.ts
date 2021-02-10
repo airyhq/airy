6df0567becb23f4fb7ba5ec1cbe09c803f6f5f9e
Christoph Pröschel <chris@airy.co>
2021-02-10 10:33:18 +0100

    More fixes to the mapping

--- lib/typescript/httpclient/mappers/channelMapper.ts
+++ lib/typescript/httpclient/mappers/channelMapper.ts
@@ -1,12 +1,16 @@
 import {Channel} from '../model';
 import {ChannelApiPayload} from '../payload/ChannelApiPayload';
 
-export const channelMapper = (payload: ChannelApiPayload): Channel => ({
-  ...payload,
-  sourceChannelId: payload.source_channel_id,
-  metadata: {
-    ...payload.metadata,
-    imageUrl: payload.metadata.image_url,
-  },
-  connected: true,
-});
+export const channelMapper = (payload: ChannelApiPayload): Channel => {
+  let channel = {
+    ...payload,
+    sourceChannelId: payload.source_channel_id,
+    metadata: {
+      ...payload.metadata,
+      imageUrl: payload.metadata.image_url,
+    },
+    connected: true,
+  };
+  delete channel.metadata.image_url;
+  return channel;
+};
--- lib/typescript/httpclient/model/Metadata.ts
+++ lib/typescript/httpclient/model/Metadata.ts
@@ -1,3 +1,4 @@
 export interface Metadata {
+  userData?: Metadata;
   [key: string]: any;
 }

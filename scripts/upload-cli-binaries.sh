d94bdd85944ba1ad24841fb91c4139c5d98d7ffe
Pascal Holy <pascal@airy.co>
2021-02-11 16:39:30 +0100

    Change sha filename and windows filename

--- scripts/upload-cli-binaries.sh
+++ scripts/upload-cli-binaries.sh
@@ -4,9 +4,14 @@ IFS=$'\n\t'
 
 for os in "linux" "darwin" "windows"
 do
-    sha256sum bazel-bin/infrastructure/cli/airy_"$os"_bin > bazel-bin/infrastructure/cli/airy_"$os"_bin.sha
-    aws s3 cp bazel-bin/infrastructure/cli/airy_"$os"_bin s3://airy-core-binaries/"$(cat ./VERSION)"/"$os"/amd64/airy
-    aws s3 cp bazel-bin/infrastructure/cli/airy_"$os"_bin.sha s3://airy-core-binaries/"$(cat ./VERSION)"/"$os"/amd64/airy.sha
+    sha256sum bazel-bin/infrastructure/cli/airy_"$os"_bin > bazel-bin/infrastructure/cli/airy_"$os"_sha256sum.txt
+    if [[ ${os} = "windows" ]]
+    then
+        aws s3 cp bazel-bin/infrastructure/cli/airy_"$os"_bin s3://airy-core-binaries/"$(cat ./VERSION)"/"$os"/amd64/airy.exe
+    else
+        aws s3 cp bazel-bin/infrastructure/cli/airy_"$os"_bin s3://airy-core-binaries/"$(cat ./VERSION)"/"$os"/amd64/airy
+    fi
+    aws s3 cp bazel-bin/infrastructure/cli/airy_"$os"_sha256sum.txt s3://airy-core-binaries/"$(cat ./VERSION)"/"$os"/amd64/
 done
 
 cat ./VERSION > stable.txt

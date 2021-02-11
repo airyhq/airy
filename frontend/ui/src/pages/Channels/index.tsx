c85382d4eb5be5d9947b1101a239704fc81b1aa5
Kazeem Adetunji <ogbeche77@yahoo.com>
2021-02-11 11:31:03 +0100

    resolves conflict in component rendering

--- frontend/ui/src/pages/Channels/index.tsx
+++ frontend/ui/src/pages/Channels/index.tsx
@@ -93,7 +93,7 @@ const Channels = (props: ChannelsConnectProps) => {
       </div>
     </div>
   );
-
+  console.log(props.channels);
   return (
     <div className={styles.channelsWrapper}>
       <div className={styles.channelsHeadline}>
@@ -106,7 +106,6 @@ const Channels = (props: ChannelsConnectProps) => {
               <FilterIcon className={styles.searchIcon} title="Filter" />
             </button>
           </div>
-
           {renderSearchChannelInput}
         </div>
       </div>
@@ -115,19 +114,43 @@ const Channels = (props: ChannelsConnectProps) => {
         <p>Choose a channel you want to connect</p>
       </div>
 
-      <div>
-        <div className={styles.wrapper}>
-          <div className={styles.flexWrap}>
-            <div className={styles.airyChannel}>
-              <div className={styles.airyLogo}>
-                <AiryLogo />
-              </div>
-              <div className={styles.airyTitleAndText}>
-                <p className={styles.airyTitle}>Airy Live Chat</p>
-                <p className={styles.airyText}>Best of class browser messenger</p>
-              </div>
+      <div className={styles.wrapper}>
+        <div className={styles.flexWrap}>
+          <div className={styles.airyChannel}>
+            <div className={styles.airyLogo}>
+              <AiryLogo />
             </div>
-            {Channels && props.channels.length === 0 && (
+            <div className={styles.airyTitleAndText}>
+              <p className={styles.airyTitle}>Airy Live Chat</p>
+              <p className={styles.airyText}>Best of class browser messenger</p>
+            </div>
+          </div>
+          {Channels && props.channels.length === 0 && (
+            <div className={styles.channelButton}>
+              <button type="button" onClick={handleAiryChannel} className={styles.addChannelButton}>
+                <div className={styles.channelButtonIcon}>
+                  <AddChannel />
+                </div>
+              </button>
+            </div>
+          )}
+
+          {Channels && props.channels.length > 0 && (
+            <>
+              <div className={styles.airyConnectedContainer}>
+                <div className={styles.airyConnectedPara}>
+                  <p>{totalChannel} CONNECTED</p>
+                </div>
+
+                <div className={styles.airyConnectedChannel}>
+                  {props.channels.map((channel: Channel) => (
+                    <li key={channel.sourceChannelId} className={styles.channelListEntry}>
+                      <img src={channel.imageUrl} className={styles.channelImage} />
+                      <div className={styles.channelName}>{channel.name}</div>
+                    </li>
+                  ))}
+                </div>
+              </div>
               <div className={styles.channelButton}>
                 <button type="button" onClick={handleAiryChannel} className={styles.addChannelButton}>
                   <div className={styles.channelButtonIcon}>
@@ -135,114 +158,114 @@ const Channels = (props: ChannelsConnectProps) => {
                   </div>
                 </button>
               </div>
-            )}
+            </>
+          )}
+        </div>
 
-            {Channels && props.channels.length > 0 && (
-              <>
-                <div className={styles.airyConnectedContainer}>
-                  <div className={styles.airyConnectedPara}>
-                    <p>{totalChannel} Connected</p>
-                  </div>
-
-                  <div className={styles.airyConnectedChannel}>
-                    {props.channels.map((channel: Channel) => (
-                      <li key={channel.sourceChannelId} className={styles.channelListEntry}>
-                        <img src={channel.imageUrl} className={styles.channelImage} />
-                        <div className={styles.channelName}>{channel.name}</div>
-                        <div className={styles.channelAction}></div>
-                      </li>
-                    ))}
-                  </div>
+        <div className={styles.flexWrap}>
+          <div className={styles.facebookChannel}>
+            <div className={styles.facebookLogo}>
+              <FacebookLogo />
+            </div>
+            <div className={styles.facebookTitleAndText}>
+              <p className={styles.facebookTitle}>Messenger</p>
+              <p className={styles.facebookText}>Connect multiple Facebook pages</p>
+            </div>
+          </div>
+          {Channels && props.channels.length === 0 && (
+            <div className={styles.channelButton}>
+              <button type="button" onClick={handleFacebookChannel} className={styles.addChannelButton}>
+                <div className={styles.channelButtonIcon}>
+                  <AddChannel />
                 </div>
-                <div className={styles.channelButton}>
-                  <button type="button" onClick={handleAiryChannel} className={styles.addChannelButton}>
-                    <div className={styles.channelButtonIcon}>
-                      <AddChannel />
-                    </div>
-                  </button>
-                </div>
-              </>
-            )}
-          </div>
+              </button>
+            </div>
+          )}
 
-          <div className={styles.flexWrap}>
-            <div className={styles.facebookChannel}>
-              <div className={styles.facebookLogo}>
-                <FacebookLogo />
+          {Channels && props.channels.length > 0 && (
+            <>
+              <div className={styles.airyConnectedContainer}>
+                <div className={styles.airyConnectedPara}>
+                  <p>{totalChannel} CONNECTED</p>
+                </div>
+
+                <div className={styles.airyConnectedChannel}>
+                  {props.channels.map((channel: Channel) => (
+                    <li key={channel.sourceChannelId} className={styles.channelListEntry}>
+                      <img src={channel.imageUrl} className={styles.channelImage} />
+                      <div className={styles.channelName}>{channel.name}</div>
+                      <div className={styles.channelAction}></div>
+                    </li>
+                  ))}
+                </div>
               </div>
-              <div className={styles.facebookTitleAndText}>
-                <p className={styles.facebookTitle}>Messenger</p>
-                <p className={styles.facebookText}>Connect multiple Facebook pages</p>
-              </div>
-            </div>
-            <div>
               <div className={styles.channelButton}>
-                <button type="button" onClick={handleFacebookChannel} className={styles.addChannelButton}>
+                <button type="button" onClick={handleAiryChannel} className={styles.addChannelButton}>
                   <div className={styles.channelButtonIcon}>
                     <AddChannel />
                   </div>
                 </button>
               </div>
+            </>
+          )}
+        </div>
+        <div className={styles.flexWrap}>
+          <div className={styles.smsChannel}>
+            <div className={styles.smsLogo}>
+              <SMSLogo />
+            </div>
+            <div className={styles.smsTitleAndText}>
+              <p className={styles.smsTitle}>SMS</p>
+              <p className={styles.smsText}>Deliver SMS with ease</p>
             </div>
           </div>
-          <div className={styles.flexWrap}>
-            <div className={styles.smsChannel}>
-              <div className={styles.smsLogo}>
-                <SMSLogo />
-              </div>
-              <div className={styles.smsTitleAndText}>
-                <p className={styles.smsTitle}>SMS</p>
-                <p className={styles.smsText}>Deliver SMS with ease</p>
-              </div>
-            </div>
-            <div>
-              <div className={styles.channelButton}>
-                <button type="button" onClick={handleSmsChannel} className={styles.addChannelButton}>
-                  <div className={styles.channelButtonIcon}>
-                    <AddChannel />
-                  </div>
-                </button>
-              </div>
+          <div>
+            <div className={styles.channelButton}>
+              <button type="button" onClick={handleSmsChannel} className={styles.addChannelButton}>
+                <div className={styles.channelButtonIcon}>
+                  <AddChannel />
+                </div>
+              </button>
             </div>
           </div>
-          <div className={styles.flexWrap}>
-            <div className={styles.whatsappChannel}>
-              <div className={styles.whatsappLogo}>
-                <WhatsappLogo />
-              </div>
-              <div className={styles.whatsappTitleAndText}>
-                <p className={styles.whatsappTitle}>Whatsapp</p>
-                <p className={styles.whatsappText}>World #1 chat app</p>
-              </div>
+        </div>
+        <div className={styles.flexWrap}>
+          <div className={styles.whatsappChannel}>
+            <div className={styles.whatsappLogo}>
+              <WhatsappLogo />
             </div>
-            <div>
-              <div className={styles.channelButton}>
-                <button type="button" onClick={handleWhatsappChannel} className={styles.addChannelButton}>
-                  <div className={styles.channelButtonIcon}>
-                    <AddChannel />
-                  </div>
-                </button>
-              </div>
+            <div className={styles.whatsappTitleAndText}>
+              <p className={styles.whatsappTitle}>Whatsapp</p>
+              <p className={styles.whatsappText}>World #1 chat app</p>
             </div>
           </div>
-          <div className={styles.flexWrap}>
-            <div className={styles.googleChannel}>
-              <div className={styles.googleLogo}>
-                <GoogleLogo />
-              </div>
-              <div className={styles.googleTitleAndText}>
-                <p className={styles.googleTitle}>Google Business Messages</p>
-                <p className={styles.googleText}>Be there when people search</p>
-              </div>
+          <div>
+            <div className={styles.channelButton}>
+              <button type="button" onClick={handleWhatsappChannel} className={styles.addChannelButton}>
+                <div className={styles.channelButtonIcon}>
+                  <AddChannel />
+                </div>
+              </button>
             </div>
-            <div>
-              <div className={styles.channelButton}>
-                <button type="button" onClick={handleGoogleChannel} className={styles.addChannelButton}>
-                  <div className={styles.channelButtonIcon}>
-                    <AddChannel />
-                  </div>
-                </button>
-              </div>
+          </div>
+        </div>
+        <div className={styles.flexWrap}>
+          <div className={styles.googleChannel}>
+            <div className={styles.googleLogo}>
+              <GoogleLogo />
+            </div>
+            <div className={styles.googleTitleAndText}>
+              <p className={styles.googleTitle}>Google Business Messages</p>
+              <p className={styles.googleText}>Be there when people search</p>
+            </div>
+          </div>
+          <div>
+            <div className={styles.channelButton}>
+              <button type="button" onClick={handleGoogleChannel} className={styles.addChannelButton}>
+                <div className={styles.channelButtonIcon}>
+                  <AddChannel />
+                </div>
+              </button>
             </div>
           </div>
         </div>

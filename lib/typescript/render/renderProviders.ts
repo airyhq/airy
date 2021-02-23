4a915fe207d2b75594778f71495b1940e447ccb3
thorstenairy <stayprod.io@gmail.com>
2021-02-23 09:17:04 +0100

    added whatsapp to render library

--- lib/typescript/render/renderProviders.ts
+++ lib/typescript/render/renderProviders.ts
@@ -1,6 +1,7 @@
 import {FacebookRender} from './providers/facebook/FacebookRender';
 import {ChatPluginRender} from './providers/chatplugin/ChatPluginRender';
 import {TwilioSMSRender} from './providers/twilio/twilioSMS/TwilioSMSRender';
+import {TwilioWhatsappRender} from './providers/twilio/twilioWhatsapp/TwilioWhatsappRender';
 import {MessageRenderProps} from './shared';
 
 type Provider = (messageRenderProps: MessageRenderProps) => JSX.Element;
@@ -9,4 +10,5 @@ export const renderProviders: {[key: string]: Provider} = {
   facebook: FacebookRender,
   chat_plugin: ChatPluginRender,
   'twilio.sms': TwilioSMSRender,
+  'twilio.whatsapp': TwilioWhatsappRender,
 };

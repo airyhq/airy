a3e0e3d95526cf1d54f45830a8ed0df89a6f27ec
ljupcovangelski <ljupco@airy.co>
2021-03-02 19:01:20 +0100

    [#1022] Change ingress hostnames

--- frontend/ui/src/routes/routes.ts
+++ frontend/ui/src/routes/routes.ts
@@ -1,10 +1,9 @@
-export const ROOT_ROUTE = '/';
-export const LOGIN_ROUTE = '/login';
-export const LOGOUT_ROUTE = '/logout';
-export const CHANNELS_ROUTE = '/channels';
-export const CHANNELS_CHAT_PLUGIN_ROUTE = '/channels/chat_plugin';
-export const INBOX_ROUTE = '/inbox';
-export const INBOX_CONVERSATIONS_ROUTE = '/inbox/conversations';
-export const TAGS_ROUTE = '/tags';
+export const ROOT_ROUTE = '/ui';
+export const LOGIN_ROUTE = '/ui/login';
+export const LOGOUT_ROUTE = '/ui/logout';
+export const CHANNELS_ROUTE = '/ui/channels';
+export const INBOX_ROUTE = '/ui/inbox';
+export const INBOX_CONVERSATIONS_ROUTE = '/ui/inbox/conversations';
+export const TAGS_ROUTE = '/ui/tags';
 
 export const ROUTES_WITHOUT_NAVBAR = [LOGIN_ROUTE];

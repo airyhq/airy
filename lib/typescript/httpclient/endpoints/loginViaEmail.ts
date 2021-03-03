790db2e93924a3528e6fa0f67b64347d39cacbd0
Bodo Tasche <bitboxer@users.noreply.github.com>
2021-03-03 17:13:53 +0100

    [#983] The login call should not send the auth… (#1127)
    …token
    
    closes #983

--- lib/typescript/httpclient/endpoints/loginViaEmail.ts
+++ lib/typescript/httpclient/endpoints/loginViaEmail.ts
@@ -6,7 +6,7 @@ import {HttpClient} from '../client';
 export default HttpClient.prototype.loginViaEmail = async function loginViaEmail(
   requestPayload: LoginViaEmailRequestPayload
 ) {
-  const response: UserPayload = await this.doFetchFromBackend('users.login', requestPayload);
+  const response: UserPayload = await this.doFetchFromBackend('users.login', requestPayload, {ignoreAuthToken: true});
 
   return {...camelcaseKeys(response), displayName: `${response.first_name} ${response.last_name}`};
 };

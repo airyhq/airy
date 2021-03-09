31cafc152e0f76ba328113e18c93d15f990b0673
Christoph Pröschel <chris@airy.co>
2021-03-09 09:27:35 +0100

    provider must be installed

--- infrastructure/cli/pkg/providers/minikube/minikube.go
+++ infrastructure/cli/pkg/providers/minikube/minikube.go
@@ -4,17 +4,26 @@ import (
 	"cli/pkg/kube"
 	"fmt"
 	"os"
+	"os/exec"
 )
+
+const minikube = "minikube"
 
 type Minikube struct {
 }
 
 func (m *Minikube) Provision() (kube.KubeCtx, error) {
-	// Use this to
-	//clientcmd.NewNonInteractiveClientConfig()
+	mustBeInstalled()
 	fmt.Println("minikube provider not yet implemented")
 	os.Exit(1)
 	return kube.KubeCtx{}, nil
 }
 
+func mustBeInstalled() {
+	_, err := exec.LookPath(minikube)
+	if err != nil {
+		panic("could not find minikube installation. Please install Minikube from here https://github.com/kubernetes/minikube/releases")
+	}
+}
 
+func startCluster

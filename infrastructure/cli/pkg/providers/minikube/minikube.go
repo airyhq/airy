3bd58c9d022cf3f8b535bc665aec85bdaac72331
Christoph Pröschel <chris@airy.co>
2021-03-09 17:50:47 +0100

    create cluster

--- infrastructure/cli/pkg/providers/minikube/minikube.go
+++ infrastructure/cli/pkg/providers/minikube/minikube.go
@@ -3,27 +3,50 @@ package minikube
 import (
 	"cli/pkg/kube"
 	"fmt"
-	"os"
+	"k8s.io/client-go/util/homedir"
 	"os/exec"
+	"path/filepath"
 )
 
-const minikube = "minikube"
+const (
+	minikube = "minikube"
+	profile  = "airy-core"
+)
 
 type Minikube struct {
 }
 
 func (m *Minikube) Provision() (kube.KubeCtx, error) {
-	mustBeInstalled()
-	fmt.Println("minikube provider not yet implemented")
-	os.Exit(1)
-	return kube.KubeCtx{}, nil
-}
-
-func mustBeInstalled() {
-	_, err := exec.LookPath(minikube)
-	if err != nil {
-		panic("could not find minikube installation. Please install Minikube from here https://github.com/kubernetes/minikube/releases")
+	if err := checkInstallation(); err != nil {
+		return kube.KubeCtx{}, err
 	}
+
+	if err := startCluster(); err != nil {
+		return kube.KubeCtx{}, err
+	}
+
+	homeDir := homedir.HomeDir()
+	if homeDir == "" {
+		return kube.KubeCtx{}, fmt.Errorf("could not find the kubeconfig")
+	}
+
+	return kube.New(filepath.Join(homeDir, ".kube", "config"), profile), nil
 }
 
-func startCluster
+func checkInstallation() error {
+	_, err := exec.LookPath(minikube)
+	return err
+}
+
+func startCluster() error {
+	return runMinikube("start", "--cpus=4", "--memory=7168")
+}
+
+func runMinikube(args ...string) error {
+	defaultArgs := []string{"--profile="+profile}
+	cmd := exec.Command("minikube", append(defaultArgs, args...)...)
+	if out, err := cmd.CombinedOutput(); err != nil {
+		return fmt.Errorf("running Minikube failed with err: %v\n%v", err, string(out))
+	}
+	return nil
+}

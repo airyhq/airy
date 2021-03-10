airyhq/airy

commit 11ae58cd918cbabbbb515490d199dddeb6e20a24

[#1174] Build the right cli version on develop (#1177)

tools/build/bazel_status.sh
@@ -6,6 +6,12 @@ set -o pipefail
 
 echo STABLE_GIT_COMMIT $(git rev-parse HEAD)
 
-echo STABLE_VERSION $(cat ./VERSION)
+BRANCH=$(git branch --show-current)
+
+if [[ $BRANCH == "develop" ]]; then
+  echo STABLE_VERSION "develop"
+else
+  echo STABLE_VERSION $(cat ./VERSION)
+fi
 
 echo PROJECT_DIR $(pwd)

.github/workflows/main.yml
@@ -49,7 +49,7 @@ jobs:
         if: startsWith(github.ref, 'refs/heads/release') || startsWith(github.ref, 'refs/heads/main') || startsWith(github.ref, 'refs/heads/develop')
         run: |
           echo ${{ secrets.PAT }} | docker login ghcr.io -u airydevci --password-stdin
-          ./scripts/push-images.sh ${{ github.ref }}
+          ./scripts/push-images.sh
       - name: Install aws cli
         uses: chrislennon/action-aws-cli@v1.1
         env:

scripts/push-images.sh
@@ -3,21 +3,7 @@
 set -eo pipefail
 IFS=$'\n\t'
 
-BRANCH_TARGET=$(echo "$1" | cut -d'/' -f3)
-
-echo "Branch target: ${BRANCH_TARGET}"
-
-case ${BRANCH_TARGET} in
-  develop)
-    tag="develop"
-    ;;
-
-  main|release)
-    tag="release"
-    ;;
-esac
-
-release_targets=$(bazel query "filter("${tag}$", //...)" --output label)
+release_targets=$(bazel query "filter("release$", //...)" --output label)
 
 for target in $release_targets; do
   echo "Deploying $target"

tools/build/container_release.bzl
@@ -2,15 +2,6 @@ load("@io_bazel_rules_docker//container:container.bzl", "container_push")
 
 def container_release(registry, repository):
     container_push(
-        name = "develop",
-        format = "Docker",
-        image = ":image",
-        registry = registry,
-        repository = repository,
-        tag = "develop",
-    )
-
-    container_push(
         name = "local",
         format = "Docker",
         image = ":image",

commit ed906aa554aed7a0e2f7e87a8ca74107c61eade6

[#1174] Follow up fix to bug introduced by PR #1177

tools/build/bazel_status.sh
@@ -6,7 +6,11 @@ set -o pipefail
 
 echo STABLE_GIT_COMMIT $(git rev-parse HEAD)
 
-BRANCH=$(git branch --show-current)
+if [[ $CI = true ]]; then
+  BRANCH=$(echo "$GITHUB_REF" | sed -e "s/^refs\/heads\///")
+else
+  BRANCH=$(git branch --show-current)
+fi
 
 if [[ $BRANCH == "develop" ]]; then
   echo STABLE_VERSION "develop"
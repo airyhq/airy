33edcbdfb8ad4eb604da9aa32ba91666ff6a81f0
lucapette <ciao@lucapette.me>
2021-03-10 12:41:11 +0100

    Fix

--- tools/build/bazel_status.sh
+++ tools/build/bazel_status.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 set -o errexit
+set -o nounset
 set -o pipefail
 
 echo STABLE_GIT_COMMIT $(git rev-parse HEAD)
 
-if [[ $CI = true ]]; then
+if [[ ${CI+x} ]]; then
   BRANCH=$(echo "$GITHUB_REF" | sed -e "s/^refs\/heads\///")
 else
   BRANCH=$(git branch --show-current)

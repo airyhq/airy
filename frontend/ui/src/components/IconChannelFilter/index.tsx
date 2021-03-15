f0487b28ac094586de04c96b870d3aa1bd02aa41
Aitor Algorta <aitor@airy.co>
2021-03-15 16:33:27 +0100

    better naming for svg

--- frontend/ui/src/components/IconChannelFilter/index.tsx
+++ frontend/ui/src/components/IconChannelFilter/index.tsx
@@ -6,14 +6,14 @@ import {ReactComponent as GoogleIcon} from 'assets/images/icons/google_avatar.sv
 import {ReactComponent as WhatsappIcon} from 'assets/images/icons/whatsapp_avatar.svg';
 import {ReactComponent as SmsIcon} from 'assets/images/icons/sms_avatar.svg';
 import {ReactComponent as FacebookIcon} from 'assets/images/icons/messenger_avatar.svg';
-import {ReactComponent as ChatPluginSource} from 'assets/images/icons/airy_avatar.svg';
+import {ReactComponent as AiryAvatar} from 'assets/images/icons/airy_avatar.svg';
 
 const sourceIconsMap = {
   'google': GoogleIcon,
   'facebook': FacebookIcon,
   'twilio.sms': SmsIcon,
   'twilio.whatsapp': WhatsappIcon,
-  'chatplugin': ChatPluginSource,
+  'chatplugin': AiryAvatar,
 };
 
 export const IconChannelFilter = ({channel}: {channel: Channel}) => {

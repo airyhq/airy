e520d47d332740eb38f1861ff130ab976bd57fb0
thorstenairy <stayprod.io@gmail.com>
2021-03-16 09:37:13 +0100

    renamed variable as suggested

--- frontend/ui/src/services/image/index.ts
+++ frontend/ui/src/services/image/index.ts
@@ -1,6 +1,6 @@
 import _, {SyntheticEvent} from 'react';
 
-export const fallbackImage = (event: SyntheticEvent<HTMLImageElement, Event>, channelSource: string) => {
-  event.currentTarget.src = `https://s3.amazonaws.com/assets.airy.co/${channelSource}_avatar.svg`;
+export const fallbackImage = (event: SyntheticEvent<HTMLImageElement, Event>, source: string) => {
+  event.currentTarget.src = `https://s3.amazonaws.com/assets.airy.co/${source}_avatar.svg`;
   event.currentTarget.alt = 'fallback image';
 };

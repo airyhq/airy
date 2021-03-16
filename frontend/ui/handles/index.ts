54bd20c361e0d4bfbc2fd72a2a629c93dd60dfcd
Pascal Holy <pascal@airy.co>
2021-03-16 12:00:24 +0100

    Add test for filtering and creating a tag

--- frontend/ui/handles/index.ts
+++ frontend/ui/handles/index.ts
@@ -2,3 +2,19 @@ export const cyMessageList = 'messageList';
 export const cyInputBar = 'inputBar';
 export const cyMessageTextArea = 'messageTextArea';
 export const cyMessageSendButton = 'messageSendButton';
+
+export const cySearchButton = 'searchButton';
+export const cySearchField = 'searchField';
+export const cySearchFieldBackButton = 'searchFieldBackButton';
+
+export const cyConversationList = 'conversationList';
+
+export const cyShowTagsDialog = 'showTagsDialog';
+export const cyTagsDialogInput = 'tagsDialogInput';
+export const cyTagsDialogButton = 'tagsDialogButton';
+export const cyTagsDialogColorSelectorBlue = 'tagsDialogColorSelectorBlue';
+export const cyTagsDialogColorSelectorRed = 'tagsDialogColorSelectorRed';
+export const cyTagsDialogColorSelectorGreen = 'tagsDialogColorSelectorGreen';
+export const cyTagsDialogColorSelectorPurple = 'tagsDialogColorSelectorPurple';
+export const cyTagsSearchField = 'tagsSearchField';
+export const cyTagsTable = 'tagsTable';

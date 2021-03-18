b4d42a3c2259008049bc0192c5af99225f550782
Thorsten <thorsten@airy.co>
2021-03-18 17:09:34 +0100

    [#1281] Added RichCard and RichCardCarousel to google (#1288)
    closes #1281

--- lib/typescript/render/providers/google/googleModel.ts
+++ lib/typescript/render/providers/google/googleModel.ts
@@ -1,5 +1,12 @@
+import {Suggestion} from '../../components/RichCard';
+
+export enum MediaHeight {
+  short = 'SHORT',
+  medium = 'MEDIUM',
+  tall = 'TALL',
+}
 export interface Content {
-  type: 'text' | 'image' | 'suggestions';
+  type: 'text' | 'image' | 'suggestions' | 'richCard' | 'richCardCarousel';
 }
 
 export interface TextContent extends Content {
@@ -11,6 +18,26 @@ export interface ImageContent extends Content {
   type: 'image';
   imageUrl: string;
   altText?: string;
+}
+export interface RichCardContent extends Content {
+  type: 'richCard';
+  title?: string;
+  description?: string;
+  media: {
+    height: MediaHeight;
+    contentInfo: {
+      altText?: string;
+      fileUrl: string;
+      forceRefresh: boolean;
+    };
+  };
+  suggestions: Suggestion[];
+}
+
+export interface RichCardCarouselContent extends Content {
+  type: 'richCardCarousel';
+  cardWidth: string;
+  cardContents: [RichCardContent];
 }
 
 interface SuggestedReplies {
@@ -64,4 +91,4 @@ export interface SuggestionsContent extends Content {
   suggestions: SuggestionsUnion[];
 }
 
-export type ContentUnion = TextContent | ImageContent | SuggestionsContent;
+export type ContentUnion = TextContent | ImageContent | SuggestionsContent | RichCardContent | RichCardCarouselContent;

5ed66e880fd7451f74ea313ccc35264b3bb5400f
Pascal Holy <pascal@airy.co>
2021-03-19 14:06:23 +0100

    Add support folder

---
+++ integration/cypress/support/commands.ts
@@ -0,0 +1,35 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+Cypress.Commands.add("login", (email, password) => {
+    cy.visit('/login');
+    cy.get('form')
+        .within(() => {
+            cy.get('input[type=email]').type(email);
+            cy.get('input[type=password]').type(password);
+        })
+        .submit();
+
+    cy.wait(500)
+})
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })

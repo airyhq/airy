airyhq/airy

commit e76eea1e895212be508e9a7ce3537be321da32a1

[#1097] Allow users to fetch a chat plugin resume token (#1350)

backend/sources/chat-plugin/src/main/java/co/airy/core/chat_plugin/config/AuthConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -35,11 +36,15 @@ public class AuthConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .addFilter(new JwtAuthenticationFilter(authenticationManager(), jwt))
                 .authorizeRequests()
-                .antMatchers("/actuator/**", "/ws.chatplugin").permitAll()
-                .mvcMatchers("/chatplugin.authenticate").permitAll()
                 .anyRequest().authenticated();
     }
 
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers("/actuator/**", "/ws.chatplugin")
+                .mvcMatchers("/chatplugin.authenticate", "/chatplugin.resumeToken");
+    }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource(final Environment environment) {

backend/sources/chat-plugin/src/main/java/co/airy/core/chat_plugin/config/JwtAuthenticationFilter.java
@@ -14,6 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+import static co.airy.spring.web.Headers.getAuthToken;
+
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     private final Jwt jwt;
 
@@ -26,10 +28,7 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     protected void doFilterInternal(HttpServletRequest req,
                                     HttpServletResponse res,
                                     FilterChain chain) throws IOException, ServletException {
-        String authToken = req.getHeader(HttpHeaders.AUTHORIZATION);
-        if (authToken != null && authToken.startsWith("Bearer")) {
-            authToken = authToken.substring(7);
-        }
+        String authToken = getAuthToken(req);
 
         if (authToken == null) {
             chain.doFilter(req, res);

lib/java/spring/auth/src/main/java/co/airy/spring/auth/AuthConfig.java
@@ -28,7 +28,7 @@ public class AuthConfig extends WebSecurityConfigurerAdapter {
     private final String[] ignoreAuthPatterns;
     private final String apiToken;
 
-    public AuthConfig(Jwt jwt, @Value("${token:#{null}}") String apiToken, List<IgnoreAuthPattern> ignorePatternBeans) {
+    public AuthConfig(Jwt jwt, @Value("${system_token:#{null}}") String apiToken, List<IgnoreAuthPattern> ignorePatternBeans) {
         this.jwt = jwt;
         this.apiToken = apiToken;
         this.ignoreAuthPatterns = ignorePatternBeans.stream()

backend/sources/chat-plugin/src/main/java/co/airy/core/chat_plugin/ChatController.java
@@ -7,6 +7,7 @@ import co.airy.avro.communication.SenderType;
 import co.airy.core.chat_plugin.config.Jwt;
 import co.airy.core.chat_plugin.payload.AuthenticationRequestPayload;
 import co.airy.core.chat_plugin.payload.AuthenticationResponsePayload;
+import co.airy.core.chat_plugin.payload.ResumeTokenRequestPayload;
 import co.airy.core.chat_plugin.payload.ResumeTokenResponsePayload;
 import co.airy.core.chat_plugin.payload.SendMessageRequestPayload;
 import co.airy.model.message.dto.MessageResponsePayload;
@@ -14,11 +15,14 @@ import co.airy.spring.web.payload.EmptyResponsePayload;
 import co.airy.spring.web.payload.RequestErrorResponsePayload;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 
@@ -30,6 +34,7 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import static co.airy.spring.web.Headers.getAuthToken;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
@@ -37,11 +42,13 @@ public class ChatController {
     private final ObjectMapper objectMapper;
     private final Stores stores;
     private final Jwt jwt;
+    private final String apiToken;
 
-    public ChatController(Stores stores, Jwt jwt, ObjectMapper objectMapper) {
+    public ChatController(Stores stores, Jwt jwt, ObjectMapper objectMapper, @Value("${system_token:#{null}}") String apiToken) {
         this.stores = stores;
         this.jwt = jwt;
         this.objectMapper = objectMapper;
+        this.apiToken = apiToken;
     }
 
     @PostMapping("/chatplugin.authenticate")
@@ -60,7 +67,7 @@ public class ChatController {
             return ResponseEntity.badRequest().body(new EmptyResponsePayload());
         }
 
-        final String authToken = jwt.getAuthToken(principal.getSessionId(), principal.getChannelId());
+        final String authToken = jwt.getAuthToken(principal.getConversationId(), principal.getChannelId());
 
         return ResponseEntity.ok(new AuthenticationResponsePayload(authToken,
                 messages.stream().map(MessageResponsePayload::fromMessage).collect(Collectors.toList())));
@@ -77,22 +84,35 @@ public class ChatController {
             throw new HttpClientErrorException(NOT_FOUND, "Not Found", null, null, Charset.defaultCharset());
         }
 
-        final String sessionId = UUID.randomUUID().toString();
-        return new Principal(channelId, sessionId);
+        final String conversationId = UUID.randomUUID().toString();
+        return new Principal(channelId, conversationId);
     }
 
     @PostMapping("/chatplugin.resumeToken")
-    ResponseEntity<ResumeTokenResponsePayload> getResumeToken(Authentication authentication) {
-        final Principal principal = (Principal) authentication.getPrincipal();
-        final String resumeToken = jwt.getResumeToken(principal.getSessionId(), principal.getChannelId());
+    ResponseEntity<ResumeTokenResponsePayload> getResumeToken(
+            @Valid @RequestBody(required = false) ResumeTokenRequestPayload requestPayload,
+            @RequestHeader(value = "Authorization") String authHeader) {
+        final Principal principal = getUserOrSystemPrincipal(requestPayload, authHeader);
+        final String resumeToken = jwt.getResumeToken(principal.getConversationId(), principal.getChannelId());
         return ResponseEntity.ok(new ResumeTokenResponsePayload(resumeToken));
+    }
+
+    private Principal getUserOrSystemPrincipal(ResumeTokenRequestPayload requestPayload, String authHeader) {
+        final String requestToken = getAuthToken(authHeader);
+        if (apiToken != null && apiToken.equals(requestToken)) {
+            if (requestPayload == null) {
+                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST);
+            }
+            return new Principal(requestPayload.getChannelId(), requestPayload.getConversationId());
+        }
+
+        return jwt.authenticate(requestToken);
     }
 
     @PostMapping("/chatplugin.send")
     ResponseEntity<?> sendMessage(@RequestBody @Valid SendMessageRequestPayload requestPayload, Authentication authentication) {
         final Principal principal = (Principal) authentication.getPrincipal();
         final String channelId = principal.getChannelId();
-        final String sessionId = principal.getSessionId();
         final Channel channel = stores.getChannel(channelId);
 
         if (channel == null) {
@@ -108,7 +128,7 @@ public class ChatController {
                     .setHeaders(Map.of())
                     .setDeliveryState(DeliveryState.DELIVERED)
                     .setSource(channel.getSource())
-                    .setSenderId(sessionId)
+                    .setSenderId(principal.getConversationId())
                     .setSenderType(SenderType.SOURCE_CONTACT)
                     .setSentAt(Instant.now().toEpochMilli())
                     .build();

backend/sources/chat-plugin/src/main/java/co/airy/core/chat_plugin/Principal.java
@@ -1,19 +1,13 @@
 package co.airy.core.chat_plugin;
 
-import co.airy.uuid.UUIDv5;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Principal implements java.security.Principal {
     private String channelId;
-    private String sessionId;
     private String conversationId;
-
-    public Principal(String channelId, String sessionId) {
-        this.channelId = channelId;
-        this.sessionId = sessionId;
-        this.conversationId = UUIDv5.fromNamespaceAndName(channelId, sessionId).toString();
-    }
 
     @Override
     public String getName() {

backend/sources/chat-plugin/src/main/java/co/airy/core/chat_plugin/Stores.java
@@ -112,7 +112,6 @@ public class Stores implements HealthIndicator, ApplicationListener<ApplicationS
 
     public List<Message> getMessages(String conversationId) {
         final ReadOnlyKeyValueStore<String, MessagesTreeSet> messagesStore = getMessagesStore();
-
         final MessagesTreeSet messagesTreeSet = messagesStore.get(conversationId);
 
         return messagesTreeSet == null ? List.of() : new ArrayList<>(messagesTreeSet);

backend/sources/chat-plugin/src/main/java/co/airy/core/chat_plugin/config/Jwt.java
@@ -26,9 +26,9 @@ import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 @Component
 public class Jwt {
     private static final Logger log = AiryLoggerFactory.getLogger(Jwt.class);
-    public static final String SESSION_ID_CLAIM = "session_id";
+    public static final String CONVERSATION_ID_CLAIM = "conversation_id";
     public static final String CHANNEL_ID_CLAIM = "channel_id";
-    public static final String RESUME_SESSION_ID_CLAIM = "resume_session_id";
+    public static final String RESUME_CONVERSATION_ID_CLAIM = "resume_conversation_id";
     public static final String RESUME_CHANNEL_ID_CLAIM = "resume_channel_id";
     private final Key signingKey;
 
@@ -36,16 +36,15 @@ public class Jwt {
         this.signingKey = parseSigningKey(tokenKey);
     }
 
-    public String getAuthToken(String sessionId, String channelId) {
+    public String getAuthToken(String conversationId, String channelId) {
         Date now = Date.from(Instant.now());
 
         Map<String, Object> claims = new HashMap<>();
-        claims.put(SESSION_ID_CLAIM, sessionId);
+        claims.put(CONVERSATION_ID_CLAIM, conversationId);
         claims.put(CHANNEL_ID_CLAIM, channelId);
 
         JwtBuilder builder = Jwts.builder()
-                .setId(sessionId)
-                .setSubject(sessionId)
+                .setSubject(conversationId)
                 .setIssuedAt(now)
                 .addClaims(claims)
                 .signWith(signingKey, SignatureAlgorithm.HS256);
@@ -56,16 +55,15 @@ public class Jwt {
         return builder.compact();
     }
 
-    public String getResumeToken(String sessionId, String channelId) {
+    public String getResumeToken(String conversationId, String channelId) {
         Date now = Date.from(Instant.now());
 
         Map<String, Object> claims = new HashMap<>();
-        claims.put(RESUME_SESSION_ID_CLAIM, sessionId);
+        claims.put(RESUME_CONVERSATION_ID_CLAIM, conversationId);
         claims.put(RESUME_CHANNEL_ID_CLAIM, channelId);
 
         JwtBuilder builder = Jwts.builder()
-                .setId(sessionId)
-                .setSubject(sessionId)
+                .setSubject(conversationId)
                 .setIssuedAt(now)
                 .addClaims(claims)
                 .signWith(signingKey, SignatureAlgorithm.HS256);
@@ -89,9 +87,9 @@ public class Jwt {
         }
 
         try {
-            final String sessionId = (String) claims.get(SESSION_ID_CLAIM);
+            final String conversationId = (String) claims.get(CONVERSATION_ID_CLAIM);
             final String channelId = (String) claims.get(CHANNEL_ID_CLAIM);
-            return new Principal(channelId, sessionId);
+            return new Principal(channelId, conversationId);
         } catch (Exception e) {
             log.error(e.getMessage());
             throw new HttpClientErrorException(UNAUTHORIZED, "Unauthorized", null, null, Charset.defaultCharset());
@@ -111,9 +109,9 @@ public class Jwt {
         }
 
         try {
-            final String sessionId = (String) claims.get(RESUME_SESSION_ID_CLAIM);
+            final String conversationId = (String) claims.get(RESUME_CONVERSATION_ID_CLAIM);
             final String channelId = (String) claims.get(RESUME_CHANNEL_ID_CLAIM);
-            return new Principal(channelId, sessionId);
+            return new Principal(channelId, conversationId);
         } catch (Exception e) {
             log.error(e.getMessage());
             throw new HttpClientErrorException(UNAUTHORIZED, "Unauthorized", null, null, Charset.defaultCharset());

backend/sources/chat-plugin/src/main/java/co/airy/core/chat_plugin/payload/ResumeTokenRequestPayload.java
@@ -1,0 +1,13 @@
+package co.airy.core.chat_plugin.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResumeTokenRequestPayload {
+    private String channelId;
+    private String conversationId;
+}

commit 958e0e608cfdfcb6d30cee5b276a54556ae86102

[#1097] Fix CORS issue introduced by PR #1350

backend/sources/chat-plugin/src/main/java/co/airy/core/chat_plugin/config/AuthConfig.java
@@ -1,11 +1,11 @@
 package co.airy.core.chat_plugin.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,9 +24,11 @@ import java.util.List;
 )
 public class AuthConfig extends WebSecurityConfigurerAdapter {
     private final Jwt jwt;
+    private final String systemToken;
 
-    public AuthConfig(Jwt jwt) {
+    public AuthConfig(Jwt jwt, @Value("${system_token:#{null}}") String systemToken) {
         this.jwt = jwt;
+        this.systemToken = systemToken;
     }
 
     @Override
@@ -34,17 +36,13 @@ public class AuthConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 // Don't let Spring create its own session
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .addFilter(new JwtAuthenticationFilter(authenticationManager(), jwt))
+                .addFilter(new JwtAuthenticationFilter(authenticationManager(), jwt, systemToken))
                 .authorizeRequests()
+                .antMatchers("/actuator/**", "/ws.chatplugin").permitAll()
+                .mvcMatchers("/chatplugin.authenticate", "/chatplugin.resumeToken").permitAll()
                 .anyRequest().authenticated();
     }
 
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring()
-                .antMatchers("/actuator/**", "/ws.chatplugin")
-                .mvcMatchers("/chatplugin.authenticate", "/chatplugin.resumeToken");
-    }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource(final Environment environment) {

backend/sources/chat-plugin/src/main/java/co/airy/core/chat_plugin/config/JwtAuthenticationFilter.java
@@ -1,7 +1,6 @@
 package co.airy.core.chat_plugin.config;
 
 import co.airy.core.chat_plugin.Principal;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,10 +17,14 @@ import static co.airy.spring.web.Headers.getAuthToken;
 
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     private final Jwt jwt;
+    private final String systemToken;
+    private final String systemTokenPrincipal;
 
-    public JwtAuthenticationFilter(AuthenticationManager authManager, Jwt jwt) {
+    public JwtAuthenticationFilter(AuthenticationManager authManager, Jwt jwt, String systemToken) {
         super(authManager);
         this.jwt = jwt;
+        this.systemToken = systemToken;
+        this.systemTokenPrincipal = systemToken == null ? null : String.format("system-token-%s", systemToken.substring(0, Math.min(systemToken.length(), 4)));
     }
 
     @Override
@@ -42,6 +45,10 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     }
 
     private UsernamePasswordAuthenticationToken getAuthentication(String token) {
+        if (systemToken != null && systemToken.equals(token)) {
+            return new UsernamePasswordAuthenticationToken(systemTokenPrincipal, null, List.of());
+        }
+
         final Principal principal = jwt.authenticate(token);
 
         if (principal != null) {

lib/java/spring/auth/src/main/java/co/airy/spring/auth/AuthConfig.java
@@ -26,11 +26,11 @@ import java.util.List;
 public class AuthConfig extends WebSecurityConfigurerAdapter {
     private final Jwt jwt;
     private final String[] ignoreAuthPatterns;
-    private final String apiToken;
+    private final String systemToken;
 
-    public AuthConfig(Jwt jwt, @Value("${system_token:#{null}}") String apiToken, List<IgnoreAuthPattern> ignorePatternBeans) {
+    public AuthConfig(Jwt jwt, @Value("${system_token:#{null}}") String systemToken, List<IgnoreAuthPattern> ignorePatternBeans) {
         this.jwt = jwt;
-        this.apiToken = apiToken;
+        this.systemToken = systemToken;
         this.ignoreAuthPatterns = ignorePatternBeans.stream()
                 .flatMap((ignoreAuthPatternBean -> ignoreAuthPatternBean.getIgnorePattern().stream()))
                 .toArray(String[]::new);
@@ -42,7 +42,7 @@ public class AuthConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 // Don't let Spring create its own session
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .addFilter(new JwtAuthenticationFilter(authenticationManager(), jwt, this.apiToken))
+                .addFilter(new JwtAuthenticationFilter(authenticationManager(), jwt, this.systemToken))
                 .authorizeRequests(authorize -> authorize
                         .antMatchers("/actuator/**", "/ws*").permitAll()
                         .antMatchers(ignoreAuthPatterns).permitAll()

lib/java/spring/auth/src/main/java/co/airy/spring/auth/JwtAuthenticationFilter.java
@@ -1,7 +1,6 @@
 package co.airy.spring.auth;
 
 import co.airy.spring.jwt.Jwt;
-import lombok.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,24 +17,21 @@ import java.util.List;
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 
     private final Jwt jwt;
-    private final String apiToken;
+    private final String systemToken;
     private final String apiTokenPrincipal;
 
-    public JwtAuthenticationFilter(AuthenticationManager authManager, Jwt jwt, String apiToken) {
+    public JwtAuthenticationFilter(AuthenticationManager authManager, Jwt jwt, String systemToken) {
         super(authManager);
         this.jwt = jwt;
-        this.apiToken = apiToken;
-        this.apiTokenPrincipal = apiToken == null ? null : String.format("api-token-%s", apiToken.substring(0, Math.min(apiToken.length(), 4)));
+        this.systemToken = systemToken;
+        this.apiTokenPrincipal = systemToken == null ? null : String.format("system-token-%s", systemToken.substring(0, Math.min(systemToken.length(), 4)));
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest req,
                                     HttpServletResponse res,
                                     FilterChain chain) throws IOException, ServletException {
-        String authToken = req.getHeader(HttpHeaders.AUTHORIZATION);
-        if (authToken != null && authToken.startsWith("Bearer")) {
-            authToken = authToken.substring(7);
-        }
+        String authToken = getAuthToken(req);
 
         if (authToken == null) {
             chain.doFilter(req, res);
@@ -53,7 +49,7 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     }
 
     private UsernamePasswordAuthenticationToken getAuthentication(String token) {
-        if (apiToken != null && apiToken.equals(token)) {
+        if (systemToken != null && systemToken.equals(token)) {
             return new UsernamePasswordAuthenticationToken(apiTokenPrincipal, null, List.of());
         }
 
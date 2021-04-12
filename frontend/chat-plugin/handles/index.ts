4542eae28cbd7a6bbb28d36de549963a207c6f49
Kazeem Adetunji <ogbeche77@yahoo.com>
2021-04-12 18:31:00 +0200

    Work in progress

--- frontend/chat-plugin/handles/index.ts
+++ frontend/chat-plugin/handles/index.ts
@@ -2,3 +2,4 @@ export const cyBubble = 'bubble';
 export const cyInputbarTextarea = 'inputbarTextarea';
 export const cyInputbarButton = 'inputbarButton';
 export const cyChatPluginMessageList = 'chatPluginMessageList';
+export const cyChatPluginHeaderBarCloseButton = 'chatPluginHeaderBarCloseButton';
--- integration/chat-plugin/end_conversation.spec.ts
+++ integration/chat-plugin/end_conversation.spec.ts
@@ -1,4 +1,4 @@
-import {cyBubble, cyInputbarTextarea, cyInputbarButton, cyChatPluginMessageList} from 'chat-plugin-handles';
+import {cyBubble, cyInputbarTextarea, cyInputbarButton, cyChatPluginMessageList, cyChatPluginHeaderBarCloseButton} from 'chat-plugin-handles';
 
 import {
   cyChannelsChatPluginAddButton,
@@ -9,23 +9,23 @@ import {
 
 describe('Websocket test', () => {
   it('Send message from Inbox to Chatplugin, end the current conversation and begin a new one', () => {
-    cy.visit('/ui/login');
-    cy.get('form')
-      .within(() => {
-        cy.get('input[type=email]').type(Cypress.env('username'));
-        cy.get('input[type=password]').type(Cypress.env('password'));
-      })
-      .submit();
+    // cy.visit('/ui/login');
+    // cy.get('form')
+    //   .within(() => {
+    //     cy.get('input[type=email]').type(Cypress.env('username'));
+    //     cy.get('input[type=password]').type(Cypress.env('password'));
+    //   })
+    //   .submit();
 
-    cy.wait(500);
+    // cy.wait(500);
 
-    cy.visit('/ui/channels');
-    cy.wait(500);
-    cy.url().should('include', '/channels');
-    cy.get(`[data-cy=${cyChannelsChatPluginAddButton}]`).click();
-    cy.get(`[data-cy=${cyChannelsChatPluginConnectButton}]`).click();
-    cy.get(`[data-cy=${cyChannelsChatPluginFormNameInput}]`).type(Cypress.env('chatPluginName'));
-    cy.get(`[data-cy=${cyChannelsChatPluginFormSubmitButton}]`).click();
+    // cy.visit('/ui/channels');
+    // cy.wait(500);
+    // cy.url().should('include', '/channels');
+    // cy.get(`[data-cy=${cyChannelsChatPluginAddButton}]`).click();
+    // cy.get(`[data-cy=${cyChannelsChatPluginConnectButton}]`).click();
+    // cy.get(`[data-cy=${cyChannelsChatPluginFormNameInput}]`).type(Cypress.env('chatPluginName'));
+    // cy.get(`[data-cy=${cyChannelsChatPluginFormSubmitButton}]`).click();
 
     cy.visit('/chatplugin/ui/example?channel_id=' + Cypress.env('channelId'));
     cy.get(`[data-cy=${cyBubble}]`).click();
@@ -38,46 +38,47 @@ describe('Websocket test', () => {
 
     cy.wait(2500);
 
-    cy.request('POST', '/users.login', {
-      email: Cypress.env('username'),
-      password: Cypress.env('password'),
-    }).then(response => {
-      const loginToken = response.body['token'];
+    // cy.request('POST', '/users.login', {
+    //   email: Cypress.env('username'),
+    //   password: Cypress.env('password'),
+    // }).then(response => {
+    //   const loginToken = response.body['token'];
 
-      cy.request({
-        method: 'POST',
-        url: '/conversations.list',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${loginToken}`,
-        },
-        body: {
-          cursor: null,
-          page_size: 2,
-        },
-      }).then(response => {
-        const conversationId = response.body.data[0].id;
+    //   cy.request({
+    //     method: 'POST',
+    //     url: '/conversations.list',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       Authorization: `Bearer ${loginToken}`,
+    //     },
+    //     body: {
+    //       cursor: null,
+    //       page_size: 2,
+    //     },
+    //   }).then(response => {
+    //     const conversationId = response.body.data[0].id;
 
-        cy.request({
-          method: 'POST',
-          url: '/messages.send',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${loginToken}`,
-          },
-          body: {
-            conversation_id: `${conversationId}`,
-            message: {
-              text: Cypress.env('websocketMessage'),
-            },
-          },
-        }).then(response => {
-          expect(response).property('status').to.equal(200);
-        });
-        cy.wait(3000);
+    //     cy.request({
+    //       method: 'POST',
+    //       url: '/messages.send',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //         Authorization: `Bearer ${loginToken}`,
+    //       },
+    //       body: {
+    //         conversation_id: `${conversationId}`,
+    //         message: {
+    //           text: Cypress.env('websocketMessage'),
+    //         },
+    //       },
+    //     }).then(response => {
+    //       expect(response).property('status').to.equal(200);
+    //     });
+    //     cy.wait(3000);
 
-        cy.get(`[data-cy=${cyChatPluginMessageList}]`).children().its('length').should('eq', 3);
+        // cy.get(`[data-cy=${cyChatPluginMessageList}]`).children().its('length').should('eq', 3);
+        cy.get(`[data-cy=${cyChatPluginHeaderBarCloseButton}]`).click();
       });
     });
-  });
-});
+//   });
+// });

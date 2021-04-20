67395f00858e165eb96b86016ee7fc5a94902df4
Audrey Kadjar <audreykadjar@gmail.com>
2021-04-20 14:29:30 +0200

    added old header back

--- frontend/ui/src/pages/Inbox/Messenger/ConversationStatus/index.tsx
+++ frontend/ui/src/pages/Inbox/Messenger/ConversationStatus/index.tsx
@@ -1,9 +1,9 @@
-import React, {useEffect, useState, useRef} from 'react';
+import React from 'react';
 import {connect, ConnectedProps} from 'react-redux';
 import {withRouter, RouteComponentProps} from 'react-router-dom';
 import styles from './index.module.scss';
 import {getCurrentConversation} from '../../../../selectors/conversations';
-//import {conversationState} from '../../../../actions/conversations';
+import {conversationState} from '../../../../actions/conversations';
 
 const mapStateToProps = (state, ownProps) => {
   return {
@@ -11,43 +11,28 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-// const mapDispatchToProps = {
-//   conversationState,
-// };
+const mapDispatchToProps = {
+  conversationState,
+};
 
-const connector = connect(mapStateToProps, null);
+const connector = connect(mapStateToProps, mapDispatchToProps);
 
 type Props = ConnectedProps<typeof connector> & RouteComponentProps<{id: string}>;
 
-function usePrevious(value: string) {
-  const ref = useRef(null);
-  useEffect(() => {
-    ref.current = value;
-  });
-  return ref.current;
-}
-
 function ConversationStatus(props: Props) {
-  const {conversation} = props;
+  const {conversation, conversationState} = props;
   const currentConversationState = conversation.metadata.state || 'OPEN';
-  const [conversationState, setConversationState] = useState(currentConversationState);
-  const prevConversationId = usePrevious(conversation && conversation.id);
-
-  useEffect(() => {
-    if (conversation.id !== prevConversationId) setConversationState(currentConversationState);
-  }, [conversation]);
-
-  //onClick={() => conversationState(conversation.id, 'CLOSED')
 
   return (
-    <div className={`${styles.conversationStatus} ${conversationState === 'CLOSED' ? styles.closed : styles.open}`}>
+    <div
+      className={`${styles.conversationStatus} ${currentConversationState === 'CLOSED' ? styles.closed : styles.open}`}>
       <div className={styles.closedButtonWrapper}>
-        <div className={styles.closedButton} onClick={() => setConversationState('CLOSED')}>
+        <div className={styles.closedButton} onClick={() => conversationState(conversation.id, 'CLOSED')}>
           Closed
         </div>
       </div>
       <div className={styles.openButtonWrapper}>
-        <div className={styles.openButton} onClick={() => setConversationState('OPEN')}>
+        <div className={styles.openButton} onClick={() => conversationState(conversation.id, 'OPEN')}>
           Open
         </div>
       </div>

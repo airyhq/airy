5136ea646dcce1cfec468c61dc03f9adaac33bcc
thorstenairy <stayprod.io@gmail.com>
2021-04-28 13:49:51 +0200

    fixed reference issue

--- frontend/ui/src/pages/Inbox/ConversationListHeader/index.tsx
+++ frontend/ui/src/pages/Inbox/ConversationListHeader/index.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {connect, ConnectedProps} from 'react-redux';
+import _, {connect, ConnectedProps} from 'react-redux';
 
 import {SearchField} from 'components';
 import {StateModel} from '../../../reducers';

786ddc7722faec729a54f5a5c24a730e05ae7eba
Christoph Pröschel <chris@airy.co>
2021-05-04 15:05:09 +0200

    [#1707] Attach user profile to application logs

--- lib/java/spring/web/src/main/java/co/airy/spring/web/config/AsynchronousSpringEventsConfig.java
+++ lib/java/spring/web/src/main/java/co/airy/spring/web/config/AsynchronousSpringEventsConfig.java
@@ -1,12 +1,14 @@
-package co.airy.spring.events.config;
+package co.airy.spring.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
+@EnableAsync
 public class AsynchronousSpringEventsConfig {
     @Bean(name = "applicationEventMulticaster")
     public ApplicationEventMulticaster simpleApplicationEventMulticaster() {

11914b6d3d37ae0ee77d8667fb7e01f4f507abdb
Christoph Pröschel <chris@airy.co>
2021-05-07 12:07:37 +0200

    cleanup

--- cli/pkg/providers/aws/aws.go
+++ cli/pkg/providers/aws/aws.go
@@ -44,7 +44,6 @@ func New(w io.Writer) *provider {
 }
 
 func (p *provider) GetOverrides() tmpl.Variables {
-	//return []string{"--set", "global.ngrokEnabled=false", "--set", "global.loadbalancerAnnotations={service.beta.kubernetes.io/aws-load-balancer-type: nlb}"}
 	return tmpl.Variables{
 		LoadBalancerAnnotations: "{service.beta.kubernetes.io/aws-load-balancer-type: nlb}",
 	}

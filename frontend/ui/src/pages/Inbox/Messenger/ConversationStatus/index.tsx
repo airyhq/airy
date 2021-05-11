1ab923de18a02e0508b1a01c6617a68456ef754b
Aitor Algorta <aitor@airy.co>
2021-05-11 15:58:08 +0200

    [#1764] Filter fixed + state header bar toggle fix (#1774)
    * filter fixed + state header bar toggle fix
    
    * linting

--- frontend/ui/src/pages/Inbox/Messenger/ConversationStatus/index.tsx
+++ frontend/ui/src/pages/Inbox/Messenger/ConversationStatus/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import {connect, ConnectedProps} from 'react-redux';
 import {withRouter, RouteComponentProps} from 'react-router-dom';
 import styles from './index.module.scss';
-import {getConversation} from '../../../../selectors/conversations';
 import {conversationState} from '../../../../actions/conversations';
+import {StateModel} from '../../../../reducers';
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state: StateModel, ownProps) => {
   return {
-    conversation: getConversation(state, ownProps),
+    currentConversationState:
+      state.data.conversations.all.items[ownProps.match.params.conversationId]?.metadata?.state || 'OPEN',
   };
 };
 
@@ -17,22 +18,23 @@ const mapDispatchToProps = {
 
 const connector = connect(mapStateToProps, mapDispatchToProps);
 
-type Props = ConnectedProps<typeof connector> & RouteComponentProps<{id: string}>;
+type Props = ConnectedProps<typeof connector> & RouteComponentProps<{conversationId: string}>;
 
 function ConversationStatus(props: Props) {
-  const {conversation, conversationState} = props;
-  const currentConversationState = conversation.metadata.state || 'OPEN';
+  const {currentConversationState, conversationState} = props;
 
   return (
     <div
       className={`${styles.conversationStatus} ${currentConversationState === 'CLOSED' ? styles.closed : styles.open}`}>
       <div className={styles.closedButtonWrapper}>
-        <div className={styles.closedButton} onClick={() => conversationState(conversation.id, 'CLOSED')}>
+        <div
+          className={styles.closedButton}
+          onClick={() => conversationState(props.match.params.conversationId, 'CLOSED')}>
           Closed
         </div>
       </div>
       <div className={styles.openButtonWrapper}>
-        <div className={styles.openButton} onClick={() => conversationState(conversation.id, 'OPEN')}>
+        <div className={styles.openButton} onClick={() => conversationState(props.match.params.conversationId, 'OPEN')}>
           Open
         </div>
       </div>

2100afa15981d0b63d16c297d47a16226cdf96f1
Christoph Pröschel <chris@airy.co>
2021-05-18 18:00:35 +0200

    integrate frontend

--- frontend/ui/src/reducers/data/config/index.ts
+++ frontend/ui/src/reducers/data/config/index.ts
@@ -4,7 +4,7 @@ import * as actions from '../../../actions/config';
 type Action = ActionType<typeof actions>;
 
 export type Config = {
-  components: {[key: string]: {enabled: boolean}};
+  components: {[key: string]: {enabled: boolean, healthy: boolean}};
 };
 
 const defaultState = {
@@ -14,8 +14,21 @@ const defaultState = {
 export default function configReducer(state = defaultState, action: Action): Config {
   switch (action.type) {
     case getType(actions.saveClientConfig):
+      let components = action.payload.components;
       return {
-        ...action.payload,
+        ...state,
+        // Aggregate services on their component name
+        components: Object.keys(components).reduce((agg, key) => {
+          const {healthy, enabled, component} = components[key];
+          return {
+            ...agg,
+            [component]: {
+              enabled,
+              // A component is only healthy if all its services are healthy
+              healthy: agg[component] ? agg[component].healthy && healthy : healthy
+            }
+          }
+        }, {})
       };
     default:
       return state;

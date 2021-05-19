5b6a8d510ee8d32e9387bdc9ef4142e4dac4cf91
Christoph Pröschel <chris@airy.co>
2021-05-19 15:52:06 +0200

    fix airy.yaml output

--- cli/pkg/workspace/airy_yaml.go
+++ cli/pkg/workspace/airy_yaml.go
@@ -1,25 +1,25 @@
 package workspace
 
 type KubernetesConf struct {
-	AppImageTag             string      `yaml:"appImageTag"`
-	ContainerRegistry       string      `yaml:"containerRegistry"`
-	Namespace               string      `yaml:"namespace"`
-	NgrokEnabled            string      `yaml:"ngrokEnabled"`
-	Host                    string      `yaml:"host"`
-	LoadBalancerAnnotations interface{} `yaml:"loadBalancerAnnotations"`
+	AppImageTag             string            `yaml:"appImageTag"`
+	ContainerRegistry       string            `yaml:"containerRegistry"`
+	Namespace               string            `yaml:"namespace"`
+	NgrokEnabled            string            `yaml:"ngrokEnabled"`
+	Host                    string            `yaml:"host"`
+	LoadBalancerAnnotations map[string]string `yaml:"loadbalancerAnnotations,omitempty"`
 }
 
 type componentsConf map[string]map[string]string
 
 type AiryConf struct {
-	Kubernetes KubernetesConf
-	Security   SecurityConf
-	Components map[string]componentsConf
+	Kubernetes KubernetesConf			 `yaml:"kubernetes"`
+	Security   SecurityConf              `yaml:"security"`
+	Components map[string]componentsConf `yaml:"components,omitempty"`
 }
 
 type SecurityConf struct {
-	SystemToken    string            `yaml:"systemToken"`
+	SystemToken    string            `yaml:"systemToken,omitempty"`
 	AllowedOrigins string            `yaml:"allowedOrigins"`
 	JwtSecret      string            `yaml:"jwtSecret"`
-	Oidc           map[string]string `yaml:"oidc"`
+	Oidc           map[string]string `yaml:"oidc,omitempty"`
 }

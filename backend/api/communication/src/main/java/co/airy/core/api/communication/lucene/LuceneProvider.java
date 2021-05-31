db4d547187f152fb0e30e76ca53992883cc7e3b1
Christoph Proeschel <proeschel.christoph@gmail.com>
2021-05-31 16:21:50 +0200

    [#1732] Fix display name search typo (#1873)

--- backend/api/communication/src/main/java/co/airy/core/api/communication/lucene/LuceneProvider.java
+++ backend/api/communication/src/main/java/co/airy/core/api/communication/lucene/LuceneProvider.java
@@ -75,11 +75,11 @@ public class LuceneProvider implements LuceneStore {
             return LuceneQueryResult.builder()
                     .conversations(conversations)
                     .filteredTotal(hits.totalHits.value)
-                    .total(reader.maxDoc()).build();
+                    .total(reader.numDocs()).build();
         } catch (Exception e) {
             log.error("Failed to query Lucene store with query {}", query, e);
             return LuceneQueryResult.builder().conversations(List.of())
-                    .total(reader.maxDoc()).filteredTotal(0).build();
+                    .total(reader.numDocs()).filteredTotal(0).build();
         }
     }
 

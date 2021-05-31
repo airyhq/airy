8b39cd25fa114b7416d2425aaed71778ef907d56
Audrey Kadjar <audreykadjar@gmail.com>
2021-05-31 18:10:35 +0200

    edited test

--- integration/chat-plugin/create_conversations.spec.ts
+++ integration/chat-plugin/create_conversations.spec.ts
@@ -1,8 +1,8 @@
 import {cyInputbarButton, cyInputbarTextarea} from 'chat-plugin-handles';
 
 for (let i = 0; i < 300; i++) {
-  describe('Connect chatplugin channel', () => {
-    it('Connect chatplugin channel', () => {
+  describe('Creates a chat plugin conversation', () => {
+    it('Connects to the chat plugin channel and sends a message', () => {
       cy.request({
         method: 'POST',
         url: '/channels.chatplugin.connect',
@@ -15,10 +15,6 @@ for (let i = 0; i < 300; i++) {
       }).then(response => {
         expect(response).property('status').to.equal(200);
         let channelId = response.body.id;
-
-        cy.log('response', response);
-        cy.log('channelId', channelId);
-
         cy.visit('/chatplugin/ui/example?channel_id=' + channelId);
         cy.get(`[data-cy=${cyInputbarTextarea}]`).type(Cypress.env('messageChatplugin'));
         cy.get(`[data-cy=${cyInputbarButton}]`).click();

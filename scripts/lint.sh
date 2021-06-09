8922d62457dbc420e8b108e38fbd0d4bd5a4ed13
ljupcovangelski <ljupco@airy.co>
2021-06-09 10:50:54 +0200

    fix lint

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,7 +7,7 @@ bazel test --test_tag_filters=lint //...
 echo
 echo "Check create-topics.sh is in sync"
 bazel run //infrastructure/tools/topics:app > /tmp/topics-output
-cmp /tmp/topics-output infrastructure/helm-chart/charts/provisioning/templates/kafka-create-topics.yaml
+cmp /tmp/topics-output infrastructure/helm-chart/charts/core/charts/provisioning/templates/kafka-create-topics.yaml
 echo
 echo "Check cli doc is in sync"
 cp docs/docs/cli/usage.md /tmp/

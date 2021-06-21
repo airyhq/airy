3bc473754d65f9e86a831aeba0144412a9fd127c
ljupcovangelski <ljupco@airy.co>
2021-06-21 12:08:38 +0200

    Add umbrella chart

--- scripts/upload-helm-images.sh
+++ scripts/upload-helm-images.sh
@@ -5,34 +5,32 @@ IFS=$'\n\t'
 bucket_name="airy-core-helm-charts"
 version=$(cat ./VERSION)
 
+case ${GITHUB_BRANCH} in
+refs/heads/develop)
+    s3_basepath=s3://$bucket_name/develop
+    ;;
+refs/heads/release*)
+    s3_basepath=s3://$bucket_name/$version-rc
+    ;;
+refs/heads/main)
+    s3_basepath=s3://$bucket_name/$version
+    aws s3 cp ../VERSION s3://$bucket_name/stable.txt
+    ;;
+*)
+    exit 0
+    ;;
+esac
+
 sudo snap install helm --classic
 
 mkdir helm-repo
 cd helm-repo
+helm package ../infrastructure/helm-chart/
 helm package ../infrastructure/helm-chart/charts/prerequisites/charts/kafka
 helm package ../infrastructure/helm-chart/charts/prerequisites/charts/beanstalkd/
 helm package ../infrastructure/helm-chart/charts/core/
 helm package ../infrastructure/helm-chart/charts/tools/charts/akhq/
 helm repo index .
 
-# case ${GITHUB_BRANCH} in
-# refs/heads/develop)
-#     s3_basepath=s3://$bucket_name/develop
-#     ;;
-# refs/heads/release*)
-#     s3_basepath=s3://$bucket_name/$version-rc
-#     ;;
-# refs/heads/main)
-#     s3_basepath=s3://$bucket_name/$version
-#     ;;
-# esac
-
-s3_basepath=s3://$bucket_name/${version}
-
 find . -type f -name "*.tgz" -exec aws s3 cp "{}" "${s3_basepath}/" \;
 aws s3 cp index.yaml  "${s3_basepath}/"
-
-if [[ "${GITHUB_BRANCH}" == "refs/heads/main" ]]
-then
-    aws s3 cp ../VERSION s3://$bucket_name/stable.txt
-fi

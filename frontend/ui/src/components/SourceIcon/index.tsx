eb717c8e5bb3c7b7946bc692ad81475ff5d606dc
AudreyKj <38159391+AudreyKj@users.noreply.github.com>
2021-07-13 15:24:06 +0200

    [#2149] UI crashes when filtering with unknown source involved  (#2152)
    * fixed ui crash for unknown source
    
    * added colorful ig icon

--- frontend/ui/src/components/SourceIcon/index.tsx
+++ frontend/ui/src/components/SourceIcon/index.tsx
@@ -5,6 +5,8 @@ import {ReactComponent as WhatsappIcon} from 'assets/images/icons/whatsapp_avata
 import {ReactComponent as SmsIcon} from 'assets/images/icons/sms_avatar.svg';
 import {ReactComponent as FacebookIcon} from 'assets/images/icons/messenger_avatar.svg';
 import {ReactComponent as AiryAvatar} from 'assets/images/icons/airy_avatar.svg';
+import {ReactComponent as InstagramIcon} from 'assets/images/icons/instagramSource.svg';
+import {ReactComponent as BubbleIcon} from 'assets/images/icons/bubble.svg';
 
 const sourceIconsMap = {
   google: GoogleIcon,
@@ -12,9 +14,11 @@ const sourceIconsMap = {
   'twilio.sms': SmsIcon,
   'twilio.whatsapp': WhatsappIcon,
   chatplugin: AiryAvatar,
+  instagram: InstagramIcon,
+  unknown: BubbleIcon,
 };
 
 export const SourceIcon = ({source, ...props}) => {
-  const SourceIcon = sourceIconsMap[source];
+  const SourceIcon = sourceIconsMap[source] ?? sourceIconsMap['unknown'];
   return <SourceIcon {...props} />;
 };

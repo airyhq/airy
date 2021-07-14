d2a65ac702480583c6135c4f3434b94624d9cd48
AudreyKj <38159391+AudreyKj@users.noreply.github.com>
2021-07-14 14:03:34 +0200

    [#2127] improve cypress state conversation test (#2128)
    * improve cypress state conversation test
    
    * state conversation test fix
    
    * added assertion to test
    
    * removed filed
    
    * files
    
    * added assertion and improved test
    
    * fix url error
    
    * assertion fix

--- integration/ui/state_conversations.spec.ts
+++ integration/ui/state_conversations.spec.ts
@@ -1,15 +1,31 @@
-import {cyOpenStateButton, cyClosedStateButton, cyConversationListItemInfo, cyConversationStatus} from 'handles';
+import {
+  cyOpenStateButton,
+  cyClosedStateButton,
+  cyConversationListItemInfo,
+  cyConversationStatus,
+  cyClickableListItem,
+} from 'handles';
 
 function closeConversation() {
   cy.get(`[data-cy=${cyOpenStateButton}]`).first().click();
   cy.get(`[data-cy=${cyClosedStateButton}]`);
-  cy.get(`[data-cy=${cyConversationStatus}]`).invoke('attr', 'class').should('contain', 'closed');
+
+  const conversationStatusClosed = cy
+    .get(`[data-cy=${cyConversationStatus}]`)
+    .invoke('attr', 'class')
+    .should('contain', 'closed');
+  expect(conversationStatusClosed).to.exist;
 }
 
 function openConversation() {
   cy.get(`[data-cy=${cyClosedStateButton}]`).first().click();
   cy.get(`[data-cy=${cyOpenStateButton}]`);
-  cy.get(`[data-cy=${cyConversationStatus}]`).invoke('attr', 'class').should('contain', 'open');
+
+  const conversationStatusOpen = cy
+    .get(`[data-cy=${cyConversationStatus}]`)
+    .invoke('attr', 'class')
+    .should('contain', 'open');
+  expect(conversationStatusOpen).to.exist;
 }
 
 describe('toggles the state of a conversation, accurately changing the Open and Closed state buttons in the ConversationList and Messenger', () => {
@@ -18,6 +34,8 @@ describe('toggles the state of a conversation, accurately changing the Open and 
     cy.url().should('include', '/inbox');
 
     cy.get(`[data-cy=${cyConversationListItemInfo}]`).then(conversationListItemInfo => {
+      cy.get(`[data-cy=${cyClickableListItem}]`).first().click();
+
       if (conversationListItemInfo.find(`[data-cy=${cyOpenStateButton}]`).length > 0) {
         closeConversation();
         openConversation();

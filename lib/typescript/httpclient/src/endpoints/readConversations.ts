41804e1da4229590e25ba96701077f3eb8fcbb3d
Christoph Proeschel <proeschel.christoph@gmail.com>
2021-07-16 11:29:54 +0200

    [#1211] Rename  endpoint to (#2167)

--- lib/typescript/httpclient/src/endpoints/readConversations.ts
+++ lib/typescript/httpclient/src/endpoints/readConversations.ts
@@ -1,4 +1,4 @@
 export const readConversationsDef = {
-  endpoint: 'conversations.read',
+  endpoint: 'conversations.markRead',
   mapRequest: conversationId => ({conversation_id: conversationId}),
 };

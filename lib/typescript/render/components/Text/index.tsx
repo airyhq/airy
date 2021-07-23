8c2120ed72081f3221e78bec14e129f936c1a3bd
Christoph Pröschel <chris@airy.co>
2021-07-23 16:53:05 +0200

    [#2117] Ingest and render Google Business Messages requests for live agents

--- lib/typescript/render/components/Text/index.tsx
+++ lib/typescript/render/components/Text/index.tsx
@@ -4,13 +4,14 @@ import styles from './index.module.scss';
 
 type TextRenderProps = {
   text: string;
+  className?: string;
   fromContact?: boolean;
 };
 
-export const Text = ({text, fromContact}: TextRenderProps) => (
+export const Text = ({text, fromContact, className}: TextRenderProps) => (
   <Linkify
     tagName="div"
-    className={`${fromContact ? styles.contactContent : styles.memberContent}`}
+    className={`${fromContact ? styles.contactContent : styles.memberContent} ${className || ''}`}
     options={{
       defaultProtocol: 'https',
       className: `${styles.messageLink} ${fromContact ? styles.contactContent : styles.memberContent}`,
---
+++ lib/typescript/render/providers/google/components/RequestedLiveAgent/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const RequestedLiveAgent = () => <span>User has requested to speak to a human agent.</span>

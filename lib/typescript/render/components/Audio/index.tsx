739f8b24926df44096eab111b86a306d47f2117b
Audrey Kadjar <audreykadjar@gmail.com>
2021-09-10 18:38:41 +0200

    attachment wip

--- lib/typescript/render/components/Audio/index.tsx
+++ lib/typescript/render/components/Audio/index.tsx
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, {useRef, useEffect, useState} from 'react';
+import styles from './index.module.scss';
+import {ReactComponent as PlayIcon} from 'assets/images/icons/play.svg';
+import {ReactComponent as PauseIcon} from 'assets/images/icons/pause.svg';
+import {ReactComponent as StopIcon} from 'assets/images/icons/stop.svg';
+import {ReactComponent as DownloadIcon} from 'assets/images/icons/download.svg';
 
 type AudioRenderProps = {
   audioUrl: string;
 };
 
 export const Audio = ({audioUrl}: AudioRenderProps) => {
-  return <audio src={audioUrl} controls />;
+  const player = useRef(null);
+  const [duration, setDuration] = useState<number>(0);
+
+  useEffect(() => {
+    player.current?.load();
+
+    setTimeout(() => {
+      const totalTime = Number((player.current?.duration / 100).toFixed(2));
+      if (typeof totalTime === 'number') {
+        setDuration(Number(totalTime));
+      }
+    }, 200);
+  }, [player]);
+
+  const playAudio = () => {
+    player.current?.play();
+  };
+
+  const pauseAudio = () => {
+    player.current?.pause();
+  };
+
+  const stopAudio = () => {
+    if (player && player.current) {
+      player.current?.pause();
+      player.current.currentTime = 0;
+    }
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.player}>
+        <audio ref={player} src={audioUrl} />
+        <div className={styles.buttonsContainer}>
+          <button>
+            <PlayIcon onClick={playAudio} />
+          </button>
+          <button>
+            <PauseIcon onClick={pauseAudio} />
+          </button>
+          <button>
+            <StopIcon onClick={stopAudio} />
+          </button>
+          <a href={audioUrl} download>
+            <DownloadIcon />
+          </a>
+        </div>
+        {duration && !isNaN(duration) && <span>{duration}</span>}
+      </div>
+    </div>
+  );
 };

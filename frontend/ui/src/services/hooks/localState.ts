10b35cfd698136e4779d6c6546898ce86855baea
Christoph Proeschel <proeschel.christoph@gmail.com>
2021-10-13 13:20:21 +0200

    [#2453] Store chatplugin customization state in the browser (#2475)

---
+++ frontend/ui/src/services/hooks/localState.ts
@@ -0,0 +1,18 @@
+import {SetStateAction, Dispatch, useEffect, useState} from 'react';
+
+export const useLocalState = <S>(key: string, defaultValue: S): [S, Dispatch<SetStateAction<S>>] => {
+  const [value, setValue] = useState(() => {
+    const localValue = window.localStorage.getItem(key);
+    return localValue !== null ? (JSON.parse(localValue) as S) : defaultValue;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue];
+};
+
+export const getUseLocalState =
+  (prefix: string) =>
+  <S>(key: string, defaultValue: S) =>
+    useLocalState<S>(`${prefix}.${key}`, defaultValue);

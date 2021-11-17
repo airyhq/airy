e0a6c89942a15d53fdf598260ce9b1d8cf04d266
Thorsten <thorsten@airy.co>
2021-11-17 17:03:05 +0100

    working drag and drop

---
+++ frontend/chat-plugin/lib/src/components/dragAndDrop/index.tsx
@@ -0,0 +1,55 @@
+import React, {ReactChild, ReactChildren, useEffect, useState} from 'react';
+import style from './index.module.scss';
+
+type DragAndDropWrapperProps = {
+  children: ReactChild | ReactChildren;
+  setDragDropFile: (file: File) => void;
+};
+
+export const DragAndDropWrapper = (props: DragAndDropWrapperProps) => {
+  const {children, setDragDropFile} = props;
+  const [isFileDragged, setIsFileDragged] = useState(false);
+
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+    event.preventDefault();
+    setIsFileDragged(true);
+  };
+
+  const handleDragEnter = (event: React.DragEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+    event.preventDefault();
+    setIsFileDragged(true);
+  };
+
+  const handleFileDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    const file = event.dataTransfer.files[0];
+    setDragDropFile(file);
+    setIsFileDragged(false);
+  };
+
+  const handleDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+    event.preventDefault();
+    setIsFileDragged(false);
+  };
+
+  return (
+    <div
+      className={style.container}
+      onDragEnter={e => handleDragEnter(e)}
+      onDragOver={e => handleDragOver(e)}
+      onDrop={e => handleFileDrop(e)}
+      onDragLeave={e => handleDragLeave(e)}>
+      {isFileDragged && (
+        <div className={style.dragOverlay}>
+          <h1>Drop Files Here</h1>
+        </div>
+      )}
+      {children}
+    </div>
+  );
+};

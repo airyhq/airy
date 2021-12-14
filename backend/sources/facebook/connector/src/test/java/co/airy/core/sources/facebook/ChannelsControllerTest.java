d0d40d9744ac0151b0292b1cf51f9c11ff7c475e
Juan Sebastian Pena Rodriguez <juan-sebastian@pena-rodriguez.com>
2021-12-14 18:42:02 +0100

    Integration test for FB source - part 2

--- backend/sources/facebook/connector/src/test/java/co/airy/core/sources/facebook/ChannelsControllerTest.java
+++ backend/sources/facebook/connector/src/test/java/co/airy/core/sources/facebook/ChannelsControllerTest.java
@@ -1,13 +1,18 @@
 package co.airy.core.sources.facebook;
 
+import co.airy.avro.communication.Channel;
+import co.airy.avro.communication.ChannelConnectionState;
+import co.airy.avro.communication.Metadata;
 import co.airy.core.sources.facebook.api.Api;
 import co.airy.core.sources.facebook.payload.ConnectPageRequestPayload;
+import co.airy.core.sources.facebook.api.model.PageWithConnectInfo;
 import co.airy.kafka.schema.Topic;
 import co.airy.kafka.schema.application.ApplicationCommunicationChannels;
 import co.airy.kafka.schema.application.ApplicationCommunicationMessages;
 import co.airy.kafka.schema.application.ApplicationCommunicationMetadata;
 import co.airy.kafka.test.KafkaTestHelper;
 import co.airy.kafka.test.junit.SharedKafkaTestResource;
+import co.airy.model.metadata.MetadataKeys;
 import co.airy.spring.core.AirySpringBootApplication;
 import co.airy.spring.test.WebTestHelper;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,6 +21,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,7 +30,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doNothing;
 
 @SpringBootTest(classes = AirySpringBootApplication.class)
 @TestPropertySource(value = "classpath:test.properties")
@@ -72,18 +84,44 @@ class ChannelsControllerTest {
     @Test
     void canConnect() throws Exception {
         final ConnectPageRequestPayload connectPayload = this.mockConnectPageRequestPayload();
-        final String endPoint = "/channels.facebook.connect";
-        final String requestBody = objectMapper.writeValueAsString(connectPayload);
+        final PageWithConnectInfo pageWithConnectInfo = this.mockPageWithConnectInfo();
 
-        final String content = webTestHelper.post(endPoint, requestBody)
+        doReturn("new-long-live-token-string").when(api).exchangeToLongLivingUserAccessToken(connectPayload.getPageToken());
+        doReturn(pageWithConnectInfo).when(api).getPageForUser(connectPayload.getPageId(), "new-long-live-token-string");
+        doNothing().when(api).connectPageToApp(pageWithConnectInfo.getAccessToken());
+
+
+        final String content = webTestHelper.post(
+                "/channels.facebook.connect",
+                objectMapper.writeValueAsString(connectPayload))
             .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+        final JsonNode jsonNode = new ObjectMapper().readTree(content);
+
+        List<Channel> channels = kafkaTestHelper.consumeValues(1, applicationCommunicationChannels.name());
+        final Channel channel = channels.get(0);
+
+        assertThat(ChannelConnectionState.CONNECTED, equalTo(channel.getConnectionState()));
+
+        final List<Metadata> metadataList = kafkaTestHelper.consumeValues(1, applicationCommunicationMetadata.name());
+        final Metadata metadata = metadataList.get(0);
+
+        assertThat(MetadataKeys.ChannelKeys.IMAGE_URL, equalTo(metadata.getKey()));
     }
 
     private ConnectPageRequestPayload mockConnectPageRequestPayload() {
         return new ConnectPageRequestPayload(
                 "7562107744668308",
-                "36JbOHjxhUQeiPeUbIAXOk4RgbM9yO5MJXgs1Ya5UKmRPxKTgUYokxDAi3YtcrDQtTklDbcpqTWzOleVw5nUHgohVijZSRdBEzfXY9EgRzQ4VNPr6t575WUmjGedjIjsu4G7auMPcIVuzqt1W1jDol25eNYik2VCr3SNrhnfoJztqRi6ylxErIdDThVzHVK3nqdYtERv",
+                "token-string",
                 "facebook app",
                 "facebook app image");
+    }
+
+    private PageWithConnectInfo mockPageWithConnectInfo() {
+        return new PageWithConnectInfo(
+                "7562107744668308",
+                "facebook app",
+                "some-access-token",
+                new PageWithConnectInfo.PagePic(new PageWithConnectInfo.PagePic.PicData("facebook app image")),
+                true);
     }
 }

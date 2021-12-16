55f4fbe4cd3f4d60aa5273bd643493a2e0b12fcc
Juan Sebastian Pena Rodriguez <juan-sebastian@pena-rodriguez.com>
2021-12-16 17:40:45 +0100

    no need for subclassing

--- backend/sources/facebook/connector/src/main/java/co/airy/core/sources/facebook/api/model/MetadataKeys.java
+++ backend/sources/facebook/connector/src/main/java/co/airy/core/sources/facebook/api/model/MetadataKeys.java
@@ -1,10 +1,12 @@
 package co.airy.core.sources.facebook.api.model;
 
 
-public class MetadataKeys extends co.airy.model.metadata.MetadataKeys {
-    public static class ChannelKeys extends co.airy.model.metadata.MetadataKeys.ChannelKeys {
+public class MetadataKeys {
+    public static class ChannelKeys {
         public static final String PAGE_ID = "page_id";
         public static final String PAGE_TOKEN = "page_token";
         public static final String ACCOUNT_ID = "account_id";
+        public static final String NAME = "name";
+        public static final String IMAGE_URL = "image_url";
     }
 }

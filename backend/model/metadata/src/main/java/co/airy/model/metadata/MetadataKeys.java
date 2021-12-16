48e980da28ed0af50bfa6cabbc5d36dc966e09ff
Juan Sebastian Pena Rodriguez <juan-sebastian@pena-rodriguez.com>
2021-12-16 17:26:19 +0100

    Move specific metadata keys to child class

--- backend/model/metadata/src/main/java/co/airy/model/metadata/MetadataKeys.java
+++ backend/model/metadata/src/main/java/co/airy/model/metadata/MetadataKeys.java
@@ -41,9 +41,6 @@ public class MetadataKeys {
     public static class ChannelKeys {
         public static final String NAME = "name";
         public static final String IMAGE_URL = "image_url";
-        public static final String PAGE_ID = "page_id";
-        public static final String PAGE_TOKEN = "page_token";
-        public static final String ACCOUNT_ID = "account_id";
     }
 
     public static class MessageKeys {
---
+++ backend/sources/facebook/connector/src/main/java/co/airy/core/sources/facebook/api/model/MetadataKeys.java
@@ -0,0 +1,10 @@
+package co.airy.core.sources.facebook.api.model;
+
+
+public class MetadataKeys extends co.airy.model.metadata.MetadataKeys {
+    public static class ChannelKeys extends co.airy.model.metadata.MetadataKeys.ChannelKeys {
+        public static final String PAGE_ID = "page_id";
+        public static final String PAGE_TOKEN = "page_token";
+        public static final String ACCOUNT_ID = "account_id";
+    }
+}

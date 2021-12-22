12d2ff5d2a37424ae8a3265fe8792e15f8e544fd
Juan Sebastian Pena Rodriguez <juan-sebastian@pena-rodriguez.com>
2021-12-22 09:56:27 +0100

    [#1497] media resolver crashes on bad configuration (#2672)

--- backend/media/src/main/java/co/airy/core/media/services/MediaUpload.java
+++ backend/media/src/main/java/co/airy/core/media/services/MediaUpload.java
@@ -1,10 +1,17 @@
 package co.airy.core.media.services;
 
+import co.airy.log.AiryLoggerFactory;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.Getter;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
@@ -14,14 +21,19 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
 
 @Service
 @EnableRetry
-public class MediaUpload {
+public class MediaUpload implements HealthIndicator {
+    private static final Logger log = AiryLoggerFactory.getLogger(MediaUpload.class);
     private final AmazonS3 amazonS3Client;
     private final String bucket;
     private final String path;
     private final URL host;
+
+    @Getter
+    private boolean connectionStatus;
 
     public MediaUpload(AmazonS3 amazonS3Client,
                        @Value("${s3.bucket}") String bucket,
@@ -30,6 +42,9 @@ public class MediaUpload {
         this.bucket = bucket;
         this.path = appendSlash(path);
         this.host = new URL(String.format("https://%s.s3.amazonaws.com/", bucket));
+
+        // Check if the connection is healthy. This is only done during the construction face
+        checkConnectionStatus();
     }
 
     @Retryable
@@ -58,5 +73,30 @@ public class MediaUpload {
 
     private String appendSlash(String path) {
         return !path.endsWith("/") ? path + "/" : path;
+    }
+
+    private void checkConnectionStatus() {
+        connectionStatus = false;
+        try {
+            List<Bucket> buckets = amazonS3Client.listBuckets();
+            if (buckets != null) {
+                log.info("check connection to S3 successful");
+                connectionStatus = true;
+            }
+        } catch (SdkClientException e) {
+            log.error("AWS SDK exception", e);
+
+        } catch (Exception e) {
+            log.error("unexpected exception", e);
+        }
+    }
+
+    @Override
+    public Health health() {
+        if (connectionStatus) {
+            return Health.up().build();
+        }
+        log.error("s3 connection status is not healthy check credentials");
+        return Health.down().build();
     }
 }

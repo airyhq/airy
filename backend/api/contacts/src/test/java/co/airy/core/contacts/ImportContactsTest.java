e49e2b11cb6cc3d6510a489603ec97d2b8c98325
Juan Sebastian Pena Rodriguez <juan-sebastian@pena-rodriguez.com>
2022-01-13 15:31:21 +0100

    clean import contact test

--- backend/api/contacts/src/test/java/co/airy/core/contacts/ImportContactsTest.java
+++ backend/api/contacts/src/test/java/co/airy/core/contacts/ImportContactsTest.java
@@ -3,11 +3,16 @@ package co.airy.core.contacts;
 import co.airy.core.contacts.dto.Contact;
 import co.airy.core.contacts.payload.ContactResponsePayload;
 import co.airy.core.contacts.payload.CreateContactPayload;
+import co.airy.core.contacts.payload.ListContactsResponsePayload;
 import co.airy.core.contacts.util.Topics;
 import co.airy.kafka.test.KafkaTestHelper;
 import co.airy.kafka.test.junit.SharedKafkaTestResource;
 import co.airy.spring.core.AirySpringBootApplication;
 import co.airy.spring.test.WebTestHelper;
+import co.airy.test.RunnableTest;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterAll;
@@ -24,11 +29,16 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static co.airy.test.Timing.retryOnException;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = AirySpringBootApplication.class)
@@ -41,9 +51,6 @@ public class ImportContactsTest {
 
     @Autowired
     private WebTestHelper webTestHelper;
-
-    @Autowired
-    private Stores stores;
 
     @RegisterExtension
     public static final SharedKafkaTestResource sharedKafkaTestResource = new SharedKafkaTestResource();
@@ -66,32 +73,49 @@ public class ImportContactsTest {
         webTestHelper.waitUntilHealthy();
     }
 
+    @NoArgsConstructor
+    private class ContactsList implements RunnableTest {
+
+        @Getter
+        private String listContent;
+
+        public void test() throws Exception {
+            listContent = webTestHelper.post("/contacts.list")
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data", hasSize(4)))
+                .andReturn().getResponse().getContentAsString();
+        }
+
+    }
+
     @Test
     void canImportContacts() throws Exception {
         final List<CreateContactPayload> payload = mockContactsListPayload();
 
-        final String content = webTestHelper.post(
+        final String importContent = webTestHelper.post(
                 "/contacts.import",
                 objectMapper.writeValueAsString(payload))
             .andExpect(status().isCreated()).andReturn().getResponse().getContentAsString();
 
         List<ContactResponsePayload> contactsResp = objectMapper.readValue(
-                content,
+                importContent,
                 new TypeReference<List<ContactResponsePayload>>() {});
-        contactsResp.stream().forEach((contactResponse) -> {
-            try {
-                retryOnException(() -> {
-                    final Contact c = stores.getContact(contactResponse.getId());
-                    assertNotNull(c, String.format("Contact with id %s not found", contactResponse.getId()));
 
-                    assertThat(contactResponse.getDisplayName(), equalTo(c.getDisplayName()));
-                    assertThat(contactResponse.getAvatarUrl(), equalTo(c.getAvatarUrl()));
-                    assertThat(contactResponse.getTitle(), equalTo(c.getTitle()));
-                    assertThat(contactResponse.getGender(), equalTo(c.getGender()));
-                }, "The cache is not sync yet");
-            } catch (Exception e) {
-                assertThat(String.format("exception after retryOnException %s", e.getMessage()), false);
-            }
+        ContactsList cl = new ContactsList();
+        retryOnException(cl, "Not able to get contacts list");
+
+        final ListContactsResponsePayload contactsList = objectMapper.readValue(cl.getListContent(), ListContactsResponsePayload.class);
+        final Map<String, ContactResponsePayload> contactsMap = contactsList.getData().stream()
+            .collect(Collectors.toMap(ContactResponsePayload::getId, Function.identity()));
+
+        contactsResp.stream().forEach((contactResponse) -> {
+            final ContactResponsePayload c = contactsMap.get(contactResponse.getId());
+            assertNotNull(c, String.format("Contact with id %s not found", contactResponse.getId()));
+
+            assertThat(contactResponse.getDisplayName(), equalTo(c.getDisplayName()));
+            assertThat(contactResponse.getAvatarUrl(), equalTo(c.getAvatarUrl()));
+            assertThat(contactResponse.getTitle(), equalTo(c.getTitle()));
+            assertThat(contactResponse.getGender(), equalTo(c.getGender()));
         });
     }
 

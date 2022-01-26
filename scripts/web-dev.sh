2b5068fb6232d80cbe9d1ebe96d935fafce099ae
Aitor Algorta <aitor@airy.co>
2022-01-26 15:26:18 +0100

    change script

--- scripts/web-dev.sh
+++ scripts/web-dev.sh
@@ -5,9 +5,9 @@ IFS=$'\n\t'
 echo "web target to run: $1"
 echo "starting devserver"
 if [ -n "$2" ]; then
-  echo "Using custom host: https://${@:2}.airy.co"
+  echo "Using custom host: https://$2.airy.co"
   echo "${@:3}"  
-  bazel run "$1" -- --defines="{\"process.env.API_HOST\":\"'https://${@:2}.airy.co'\"}" "${@:3}"
+  bazel run "$1" -- --defines="{\"process.env.API_HOST\":\"'https://$2.airy.co'\"}" "${@:3}"
 else
   echo "${@:2}"
   bazel run "$1" -- --defines="{\"process.env.API_HOST\":\"'http://airy.core'\"}" "${@:2}"  

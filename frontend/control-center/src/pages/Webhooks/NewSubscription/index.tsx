616da40a1b55b2ae0ed6548fc02459145d37a9e4
Thorsten <thorsten@airy.co>
2022-04-25 15:01:20 +0200

    WIP

---
+++ frontend/control-center/src/pages/Webhooks/NewSubscription/index.tsx
@@ -0,0 +1,137 @@
+import {Button} from 'components/cta/Button';
+import React, {useEffect, useRef, useState} from 'react';
+import styles from './index.module.scss';
+
+type NewSubscriptionProps = {
+  name?: string;
+  url?: string;
+  messageCreated?: boolean;
+  messageUpdated?: boolean;
+  conversationCreated?: boolean;
+  conversationUpdated?: boolean;
+};
+
+const NewSubscription = (props: NewSubscriptionProps) => {
+  const {name, url, messageCreated, messageUpdated, conversationCreated, conversationUpdated} = props;
+  const [newUrl, setNewUrl] = useState(url);
+  const [newName, setNewName] = useState(name);
+  const [messageCreatedChecked, setMessageCreatedChecked] = useState(messageCreated);
+  const [messageUpdatedChecked, setMessageUpdatedChecked] = useState(messageUpdated);
+  const [conversationCreatedChecked, setConversationCreatedChecked] = useState(conversationCreated);
+  const [conversationUpdatedChecked, setConversationUpdatedChecked] = useState(conversationUpdated);
+  let events = useRef([]).current;
+
+  const handleChecked = (event: string) => {
+    switch (event) {
+      case 'message.created': {
+        setMessageCreatedChecked(!messageCreatedChecked);
+        messageCreatedChecked && events.push('message.created');
+        break;
+      }
+      case 'message.updated': {
+        setMessageUpdatedChecked(!messageUpdatedChecked);
+        messageUpdatedChecked && events.push('message.updated');
+        break;
+      }
+      case 'conversation.created': {
+        setConversationCreatedChecked(!conversationCreatedChecked);
+        conversationCreatedChecked && events.push('conversation.created');
+        break;
+      }
+      case 'conversation.updated': {
+        setConversationUpdatedChecked(!conversationUpdatedChecked);
+        conversationUpdatedChecked && events.push('conversation.updated');
+        break;
+      }
+    }
+    console.log(events);
+
+    return events;
+  };
+
+  const updateWebsocket = () => {};
+
+  return (
+    <form className={styles.formContainer}>
+      <div className={styles.container}>
+        <h1>WEBHOOK</h1>
+        <div className={styles.inputContainer}>
+          <input placeholder={name || 'Name'} value={newName} onChange={event => setNewName(event.target.value)} />
+          <input
+            placeholder={url || 'URL'}
+            value={newUrl}
+            onChange={event => setNewUrl(event.target.value)}
+            required={true}
+          />
+        </div>
+        <h1>ALL EVENTS</h1>
+        <div className={styles.checkboxContainer}>
+          <input
+            type="checkbox"
+            id="message.created"
+            name="message.created"
+            checked={messageCreatedChecked}
+            onChange={() => handleChecked('message.created')}
+          />
+          <label htmlFor="message.created">
+            <span>Message.Created</span>
+          </label>
+          <input
+            type="checkbox"
+            id="message.updated"
+            name="message.updated"
+            checked={messageUpdatedChecked}
+            onChange={() => handleChecked('message.updated')}
+          />
+          <label htmlFor="message.updated">
+            <span>Message.Updated</span>
+          </label>
+          <input
+            type="checkbox"
+            id="conversation.created"
+            name="conversation.created"
+            checked={conversationCreatedChecked}
+            onChange={() => handleChecked('conversation.created')}
+          />
+          <label htmlFor="conversation.created">
+            <span>Conversation.Created</span>
+          </label>
+          <input
+            type="checkbox"
+            id="conversation.updated"
+            name="conversation.updated"
+            checked={conversationUpdatedChecked}
+            onChange={() => handleChecked('conversation.updated')}
+          />
+          <label htmlFor="message.created">
+            <span>Conversation.Updated</span>
+          </label>
+        </div>
+        <div className={styles.headerKeyContainer}>
+          <div className={styles.headerKeyItem}>
+            <span>(Customer Header)*</span>
+            <input
+              placeholder="Lorem Ipsum is simply dummy textgggg of the printing and typesetting industry. Lorem Ipsum has been the
+              industrys standard dummy text ever since the 1500s"></input>
+          </div>
+          <div className={styles.headerKeyItem}>
+            <span>*Sign key</span>
+            <p>
+              Lorem Ipsum is simply duggggmmy text of the printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s
+            </p>
+          </div>
+        </div>
+      </div>
+      <Button
+        onClick={() => {}}
+        style={{alignSelf: 'center', width: '213px', height: '48px', borderRadius: '10px'}}
+        disabled={newUrl === ''}
+        type="submit">
+        Confirm
+      </Button>
+    </form>
+  );
+};
+
+export default NewSubscription;

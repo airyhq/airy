8987fade121510f86a87a480e4c8811c76a15d78
Thorsten <thorsten@airy.co>
2022-04-26 16:30:49 +0200

    WIP

--- frontend/control-center/src/pages/Webhooks/NewSubscriptionModal/index.tsx
+++ frontend/control-center/src/pages/Webhooks/NewSubscriptionModal/index.tsx
@@ -10,7 +10,8 @@ type NewSubscriptionProps = {
   conversationCreated?: boolean;
   conversationUpdated?: boolean;
   newWebhook: boolean;
-  setUpdateWebhook: (update: boolean) => void;
+  setUpdateWebhook: (update: boolean, url: string, name?: string, events?: string[]) => void;
+  setSubscribeWebhook: (url: string, name?: string, events?: string[]) => void;
 };
 
 const NewSubscription = (props: NewSubscriptionProps) => {
@@ -23,45 +24,67 @@ const NewSubscription = (props: NewSubscriptionProps) => {
     conversationUpdated,
     newWebhook,
     setUpdateWebhook,
+    setSubscribeWebhook,
   } = props;
   const [newUrl, setNewUrl] = useState(url);
   const [newName, setNewName] = useState(name);
+  let [newEvents, setNewEvents] = useState([]);
   const [messageCreatedChecked, setMessageCreatedChecked] = useState(messageCreated);
   const [messageUpdatedChecked, setMessageUpdatedChecked] = useState(messageUpdated);
   const [conversationCreatedChecked, setConversationCreatedChecked] = useState(conversationCreated);
   const [conversationUpdatedChecked, setConversationUpdatedChecked] = useState(conversationUpdated);
-  let events = useRef([]).current;
 
   const handleChecked = (event: string) => {
     switch (event) {
       case 'message.created': {
-        setMessageCreatedChecked(!messageCreatedChecked);
-        messageCreatedChecked && events.push('message.created');
+        // setMessageCreatedChecked(!messageCreatedChecked);
+        !messageCreatedChecked
+          ? !newEvents.includes(event) && newEvents.push(...'message.created')
+          : (newEvents = newEvents.filter(item => item !== event));
         break;
       }
       case 'message.updated': {
         setMessageUpdatedChecked(!messageUpdatedChecked);
-        messageUpdatedChecked && events.push('message.updated');
+        !messageUpdatedChecked
+          ? !newEvents.includes(event) && newEvents.push(...'message.updated')
+          : (newEvents = newEvents.filter(item => item !== event));
         break;
       }
       case 'conversation.created': {
         setConversationCreatedChecked(!conversationCreatedChecked);
-        conversationCreatedChecked && events.push('conversation.created');
+        !conversationCreatedChecked
+          ? !newEvents.includes(event) && newEvents.push(...'conversation.created')
+          : (newEvents = newEvents.filter(item => item !== event));
         break;
       }
       case 'conversation.updated': {
         setConversationUpdatedChecked(!conversationUpdatedChecked);
-        conversationUpdatedChecked && events.push('conversation.updated');
+        !conversationUpdatedChecked
+          ? !newEvents.includes(event) && newEvents.push(...'conversation.updated')
+          : (newEvents = newEvents.filter(item => item !== event));
         break;
       }
     }
 
-    return events;
+    console.log('EVENTS: ', newEvents);
+
+    return newEvents;
   };
 
-  const updateHook = () => {
-    setUpdateWebhook(true);
+  const updateWebhook = () => {
+    setUpdateWebhook(true, newUrl, newName, newEvents);
   };
+
+  const subscribeToNewWebhook = () => {
+    setSubscribeWebhook(newUrl, newName, newEvents);
+  };
+  // const updateWebhook = (url: string, name?: string, events?: string[]) => {
+  //   setUpdateWebhook(true, url, name, events);
+  // };
+
+  // const subscribeToNewWebhook = (url: string, name?: string, events?: string[]) => {
+  //   setSubscribeWebhook(url, name, events);
+  // };
 
   return (
     <form className={styles.formContainer}>
@@ -70,8 +93,8 @@ const NewSubscription = (props: NewSubscriptionProps) => {
         <div className={styles.inputContainer}>
           <input placeholder={name || 'Name'} value={newName} onChange={event => setNewName(event.target.value)} />
           <input
-            placeholder={url || 'URL'}
-            value={newUrl}
+            placeholder={newWebhook ? 'URL' : url}
+            value={newWebhook ? '' : newUrl}
             onChange={event => setNewUrl(event.target.value)}
             required={true}
           />
@@ -136,10 +159,10 @@ const NewSubscription = (props: NewSubscriptionProps) => {
         </div>
       </div>
       <Button
-        onClick={newWebhook ? subscribeToNewWebhook() : updateWebhook()}
+        onClick={newWebhook ? subscribeToNewWebhook : updateWebhook}
         style={{alignSelf: 'center', width: '213px', height: '48px', borderRadius: '10px'}}
         disabled={newUrl === ''}
-        type="submit">
+        type="button">
         {newWebhook ? 'Confirm' : 'Update'}
       </Button>
     </form>

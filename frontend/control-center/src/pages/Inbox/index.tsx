f1a2661142f5db0ee43314e9a088f9dd68620956
Thorsten <thorsten@airy.co>
2022-05-04 17:20:35 +0200

    added channels

--- frontend/control-center/src/pages/Inbox/index.tsx
+++ frontend/control-center/src/pages/Inbox/index.tsx
@@ -1,10 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import { SourceInfo } from '../../components/SourceInfo';
+import {getSourcesInfo, SourceInfo} from '../../components/SourceInfo';
 import {setPageTitle} from '../../services/pageTitle';
+import ChannelCard from '../Connectors/ChannelCard';
 import styles from './index.module.scss';
 
 const Inbox = () => {
   const [sourcesInfo, setSourcesInfo] = useState([]);
+
+  useEffect(() => {
+    setSourcesInfo(getSourcesInfo('Inbox'));
+  }, []);
 
   useEffect(() => {
     setPageTitle('Inbox');
@@ -19,31 +24,18 @@ const Inbox = () => {
       </div>
 
       <div className={styles.channelsContainer}>
-        <div style={{display: 'flex', flexDirection: 'column'}}>
+        <div style={{display: 'flex', flexDirection: 'column', marginBottom: '36px'}}>
           <span style={{marginBottom: '16px', marginTop: '16px', marginLeft: '36px'}}>Channels</span>
           <div style={{display: 'flex', alignItems: 'center'}}>
             <div style={{width: '150px', height: '4px', background: '#1578D4'}}></div>
-            <div style={{width: '100%', height: '1px', background: '#1578D4'}}></div>
+            <div style={{width: '100%', height: '1px', background: '#CAD5DB'}}></div>
           </div>
         </div>
-      </div>
-      <div className={styles.wrapper}>
-        {sourcesInfo.map((infoItem: SourceInfo) => {
-          return (
-              <div />
-            // channelsBySource(infoItem.type).length > 0 && (
-            //   <div style={{display: 'flex'}} key={infoItem.type}>
-            //     <ChannelCard
-            //       installed
-            //       sourceInfo={infoItem}
-            //       addChannelAction={() => {
-            //         navigate(infoItem.channelsListRoute);
-            //       }}
-            //     />
-            //   </div>
-            // )
-          );
-        })}
+        <div className={styles.wrapper}>
+          {sourcesInfo.map((infoItem: SourceInfo) => {
+            return <ChannelCard sourceInfo={infoItem} addChannelAction={() => {}} installed={true} />;
+          })}
+        </div>
       </div>
     </div>
   );

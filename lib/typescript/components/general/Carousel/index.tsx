cc631da7d81712f4daef962f119e25ddcd0ddb2c
Audrey Kadjar <audreykadjar@gmail.com>
2022-05-12 11:56:38 +0200

    removed logs

--- lib/typescript/components/general/Carousel/index.tsx
+++ lib/typescript/components/general/Carousel/index.tsx
@@ -39,7 +39,6 @@ export const Carousel = ({children}) => {
   };
 
   const maximumScrollRight = (element: HTMLDivElement) => {
-    console.log('element', element);
     const rightCutOf = carouselChildren.current.scrollLeft + element.clientWidth;
     let currentChild = -1;
     let currentPosX = 0;
@@ -56,7 +55,6 @@ export const Carousel = ({children}) => {
     }
 
     if (currentElementIndex < carouselChildren.current.children.length - 1) currentElementIndex++;
-    console.log('maxScroll', maxScroll);
     return maxScroll;
   };
 
@@ -90,7 +88,6 @@ export const Carousel = ({children}) => {
   const resetScrollButtons = useCallback(() => {
     const element = carouselChildren.current;
     if (buttonLeft.current) {
-      console.log('LEFT', element.scrollLeft);
       if (element.scrollLeft > 0) {
         buttonLeft.current.style.display = 'block';
       } else {

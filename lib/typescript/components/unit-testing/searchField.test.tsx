d4ed4e35edd219a4b689eeb0a8f0df019a5a93c4
Audrey Kadjar <audreykadjar@gmail.com>
2022-05-18 14:21:49 +0200

    testing wip

--- lib/typescript/components/unit-testing/searchField.test.tsx
+++ lib/typescript/components/unit-testing/searchField.test.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react'
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent} from "@testing-library/react";
 import {UrlInputField} from "../inputs/UrlInputField";
 import userEvent from '@testing-library/user-event';
+import * as ReactDOM from 'react-dom/client';
 
 describe("UrlInputField", () => {
   test("Adds http prefix to non-valid URL", async () => {
-    render(<UrlInputField />);
+   render(<UrlInputField />);
  
     const inputEl = screen.getByTestId("url-input");
     userEvent.type(inputEl, "http://airy.core");
  
-    expect(screen.getByTestId("email-input")).toEqual("http://airy.core");
+    expect(screen.getByTestId("url-input")).toEqual("http://airy.core");
 
   });
 });

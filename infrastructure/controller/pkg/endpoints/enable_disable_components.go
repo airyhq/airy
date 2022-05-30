08d52905b3e921faa5a389fc5c7ba1dfaf36f418
Juan Sebastian Pena Rodriguez <juan-sebastian@pena-rodriguez.com>
2022-05-30 19:17:43 +0200

    add Annotations for enable/disable state

--- infrastructure/controller/pkg/endpoints/enable_disable_components.go
+++ infrastructure/controller/pkg/endpoints/enable_disable_components.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -33,7 +34,7 @@ func (s *EnableDisableComponents) ServeHTTP(w http.ResponseWriter, r *http.Reque
 	var c component
 	//TODO: Write error messages and logs
 	if err := json.Unmarshal(blob, &c); err != nil {
-		klog.Fatalf("invalid payload %s", err)
+		klog.Errorf("invalid payload %s", err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -79,12 +80,35 @@ func (s *EnableDisableComponents) ServeHTTP(w http.ResponseWriter, r *http.Reque
 		v1.ListOptions{LabelSelector: fmt.Sprintf("core.airy.co/component=%s", c.Name)},
 	)
 	if err != nil {
-		klog.Fatalf("not able to get config-map for component %s, %s", c.Name, err)
+		klog.Errorf("not able to get config-map for component %s, %s", c.Name, err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	if len(configmaps.Items) != 1 {
+	if len(configmaps.Items) == 0 {
+		klog.Errorf("no config-map with this component name %s", c.Name)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if len(configmaps.Items) > 1 {
 		klog.Warningf("invalid number of config-maps %d", len(configmaps.Items))
 	}
+
+	cnf := configmaps.Items[0]
+	if cnf.Annotations == nil {
+		cnf.Annotations = make(map[string]string)
+	}
+
+	cnf.Annotations["enabled"] = strconv.FormatBool(c.Enable)
+	_, err = s.clientSet.CoreV1().ConfigMaps(s.namespace).Update(
+		r.Context(),
+		&cnf,
+		v1.UpdateOptions{},
+	)
+	if err != nil {
+		klog.Errorf("not able to update config-map for component %s, %s", c.Name, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	w.WriteHeader(http.StatusAccepted)
 }

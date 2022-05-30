447c0b51b0c72f17bee5a7eb63db437ba78432de
Juan Sebastian Pena Rodriguez <juan-sebastian@pena-rodriguez.com>
2022-05-30 14:10:34 +0200

    add some logs

--- infrastructure/controller/pkg/endpoints/enable_disable_components.go
+++ infrastructure/controller/pkg/endpoints/enable_disable_components.go
@@ -8,6 +8,7 @@ import (
 
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/klog"
 )
 
 type EnableDisableComponents struct {
@@ -24,6 +25,7 @@ func (s *EnableDisableComponents) ServeHTTP(w http.ResponseWriter, r *http.Reque
 	blob, err := ioutil.ReadAll(r.Body)
 	//TODO: Write error messages and logs
 	if err != nil {
+		klog.Infof("not able to read request body %s", err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -31,6 +33,7 @@ func (s *EnableDisableComponents) ServeHTTP(w http.ResponseWriter, r *http.Reque
 	var c component
 	//TODO: Write error messages and logs
 	if err := json.Unmarshal(blob, &c); err != nil {
+		klog.Fatalf("invalid payload %s", err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -71,6 +74,17 @@ func (s *EnableDisableComponents) ServeHTTP(w http.ResponseWriter, r *http.Reque
 		}
 	}
 
-	//TODO: write the change to a config map
+	configmaps, err := s.clientSet.CoreV1().ConfigMaps(s.namespace).List(
+		r.Context(),
+		v1.ListOptions{LabelSelector: fmt.Sprintf("core.airy.co/component=%s", c.Name)},
+	)
+	if err != nil {
+		klog.Fatalf("not able to get config-map for component %s, %s", c.Name, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if len(configmaps.Items) != 1 {
+		klog.Warningf("invalid number of config-maps %d", len(configmaps.Items))
+	}
 	w.WriteHeader(http.StatusAccepted)
 }

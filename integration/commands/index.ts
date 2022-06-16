c3a07c66d06056485930ea20edbe00dff31fde0c
Audrey Kadjar <audreykadjar@gmail.com>
2022-06-16 13:52:17 +0200

    typing fix

--- integration/commands/index.ts
+++ integration/commands/index.ts
@@ -3,18 +3,6 @@ import {editContactDetails} from './editContactDetails';
 import {editDisplayName} from './editDisplayName';
 import {listConversationsForContact} from './listConversationsForContact';
 
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      createChatPluginConversation(): Chainable<void>;
-      editContactDetails(): Chainable<void>;
-      connectChatPluginConnector(): Chainable<void>;
-      editDisplayName(): Chainable<void>;
-      listConversationsForContact(): Chainable<void>;
-    }
-  }
-}
-
 Cypress.Commands.add('createChatPluginConversation', createChatPluginConversation);
 Cypress.Commands.add('editContactDetails', editContactDetails);
 Cypress.Commands.add('connectChatPluginConnector', connectChatPluginConnector);

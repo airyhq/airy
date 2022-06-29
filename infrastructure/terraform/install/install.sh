8af96eaa58be8548c073467944c780b0b10eebdf
ljupcovangelski <ljupco@airy.co>
2022-06-29 17:21:42 +0200

    Flags and variables in install.sh script

--- infrastructure/terraform/install/install.sh
+++ infrastructure/terraform/install/install.sh
@@ -1,12 +1,38 @@
 #! /bin/bash
+set -eo pipefail
+IFS=$'\n\t'
 
-# Script to provision your kubernetes cluster and install airy
-provider='aws-eks'
+source ./install.flags
 
-cd $provider
+if [ -z ${PROVIDER+x} ]; then
+  echo "PROVIDER is not set. Exiting."
+  exit 1
+fi
+
+if [ -z ${WORKSPACE+x} ]; then
+  echo "WORKSPACE is not set. Exiting."
+  exit 1
+fi
+
+if [ -f ${PROVIDER}/variables.sh ]; then
+    source ${PROVIDER}/variables.sh
+fi
+
+if [ ! -f ${WORKSPACE}/airy.yaml ]; then
+    touch ${WORKSPACE}/airy.yaml
+fi
+
+read -p "Install airy-core on provider ${PROVIDER} [y/n]?" -n 1 -r
+echo
+if [[ ! ${REPLY} =~ ^[yY]$ ]]; then
+  echo "Installation aborted"
+  exit 1
+fi
+
+cd ${PROVIDER} 2>/dev/null || ( echo "Terraform directory ${PROVIDER} doesn't exist"; exit 1)
 terraform init
 terraform apply -auto-approve
 
-cd ../airy-core
+cd "../airy-core" 2>/dev/null ||  ( echo "Terraform directory \"airy-core\" doesn't exist"; exit 1)
 terraform init
 terraform apply -auto-approve

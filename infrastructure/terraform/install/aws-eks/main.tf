210549f9886ff34124314e58988a77fb82fe8a4e
ljupcovangelski <ljupco@airy.co>
2022-06-30 16:19:26 +0200

    Fix module source path

--- infrastructure/terraform/install/aws-eks/main.tf
+++ infrastructure/terraform/install/aws-eks/main.tf
@@ -1,6 +1,5 @@
 module "eks" {
-  #source = "github.com/airyhq/airy.git/infrastructure/terraform/modules/aws_eks"
-  source = "../../modules/aws_eks"
+  source = "github.com/airyhq/airy.git/infrastructure/terraform/modules/aws_eks"
 
   aws_profile            = var.aws_profile
   aws_region             = var.aws_region

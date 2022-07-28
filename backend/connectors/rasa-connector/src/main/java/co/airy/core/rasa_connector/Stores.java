c41b920214a5e742cf6f35c68e63dcbcfc0a714e
Arman Jindal <asj2152@columbia.edu>
2022-07-28 18:07:55 +0200

    Reading from kafka stream and logging

--- backend/connectors/rasa-connector/src/main/java/co/airy/core/rasa_connector/Stores.java
+++ backend/connectors/rasa-connector/src/main/java/co/airy/core/rasa_connector/Stores.java
@@ -1,11 +1,18 @@
 // Store - Class that gives you access to Kafka
 package co.airy.core.rasa_connector;
 
+import co.airy.avro.communication.Message;
+import co.airy.kafka.schema.application.ApplicationCommunicationMessages;
 import co.airy.kafka.streams.KafkaStreamsWrapper;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+
+import static co.airy.model.message.MessageRepository.isNewMessage;
 
 @Component
 public class Stores implements ApplicationListener<ApplicationStartedEvent>, DisposableBean {
@@ -18,11 +25,21 @@ public class Stores implements ApplicationListener<ApplicationStartedEvent>, Dis
 
     @Override
     public void onApplicationEvent(ApplicationStartedEvent event){
+        //Called after the instructor
 
+        final StreamsBuilder builder = new StreamsBuilder();
+        builder.<String, Message>stream(
+                new ApplicationCommunicationMessages().name(),
+                Consumed.with(Topology.AutoOffsetReset.LATEST)
+        ).filter((messageId, message) -> message != null && isNewMessage(message) && message.getIsFromContact() == true).peek((messageId, message) -> System.out.println(messageId + message.toString()));
+
+        streams.start(builder.build(), appId);
     }
 
     @Override
     public void destroy(){
-
+        if (streams != null) {
+            streams.close();
+        }
     }
 }

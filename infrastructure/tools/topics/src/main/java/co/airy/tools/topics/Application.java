e91c5322d79a72606dd14dbee9f08ce4b795d537
Christoph Proeschel <chris@airy.co>
2022-07-28 15:42:32 +0200

    [#3096] Use jaas for creating topics (#3455)
    * [#3096] Use jaas for creating topics
    
    * Test and confirm locally

--- infrastructure/tools/topics/src/main/java/co/airy/tools/topics/Application.java
+++ infrastructure/tools/topics/src/main/java/co/airy/tools/topics/Application.java
@@ -12,7 +12,7 @@ import static java.util.stream.Collectors.joining;
 public class Application {
 
     public static void main(String[] args) {
-        String createTopicTemplate = "    kafka-topics.sh --create --if-not-exists --zookeeper \"${ZOOKEEPER}\" --replication-factor \"${REPLICAS}\" --partitions \"${PARTITIONS}\" --topic \"${AIRY_CORE_NAMESPACE}%s\" %s";
+        String createTopicTemplate = "    kafka-topics.sh --create --if-not-exists \"${CONNECTION_OPTS[@]}\" --replication-factor \"${REPLICAS}\" --partitions \"${PARTITIONS}\" --topic \"${AIRY_CORE_NAMESPACE}%s\" %s";
         String headerTemplate = "apiVersion: v1" +
                 "\n" +
                 "kind: ConfigMap" +
@@ -42,18 +42,40 @@ public class Application {
                 "    set -euo pipefail\n" +
                 "    IFS=$'\\n\\t'\n" +
                 "\n" +
-                "    ZOOKEEPER=${ZOOKEEPER:-zookeeper:2181}\n" +
+                "\n" +
+                "    if [ -z \"${ZOOKEEPER+x}\" ]; then\n" +
+                "        if [ -z \"${KAFKA_BROKERS+x}\" ]; then\n" +
+                "            echo \"Neither ZOOKEEPER nor KAFKA_BROKERS is set. Exiting.\"\n" +
+                "            exit 1\n" +
+                "        fi\n" +
+                "        CONNECTION_OPTS=(--bootstrap-server $KAFKA_BROKERS)\n" +
+                "        echo \"ZOOKEEPER is not set, using --bootstrap-server option instead\"\n" +
+                "      else\n" +
+                "        CONNECTION_OPTS=(--zookeeper $ZOOKEEPER)\n" +
+                "    fi\n" +
+                "\n" +
+                "\n" +
                 "    PARTITIONS=${PARTITIONS:-10}\n" +
                 "    REPLICAS=${KAFKA_MINIMUM_REPLICAS:-1}\n" +
                 "    AIRY_CORE_NAMESPACE=${AIRY_CORE_NAMESPACE:-}\n" +
                 "\n" +
-                "    echo \"Creating Kafka topics\"\n" +
-                "\n" +
-                "    if [ -n \"${AIRY_CORE_NAMESPACE}\" ]\n" +
-                "    then\n" +
+                "    if [ -n \"${AIRY_CORE_NAMESPACE}\" ]; then\n" +
                 "      AIRY_CORE_NAMESPACE=\"${AIRY_CORE_NAMESPACE}.\"\n" +
                 "      echo \"Using ${AIRY_CORE_NAMESPACE} to namespace topics\"\n" +
-                "    fi";
+                "    fi\n" +
+                "\n" +
+                "    if [ -n \"${AUTH_JAAS}\" ]; then\n" +
+                "      cat <<EOF > /opt/kafka/jaas.config\n" +
+                "        security.protocol=SASL_SSL\n" +
+                "        sasl.jaas.config=$AUTH_JAAS\n" +
+                "        sasl.mechanism=PLAIN\n" +
+                "    EOF\n" +
+                "      CONNECTION_OPTS+=(--command-config /opt/kafka/jaas.config)\n" +
+                "      echo \"Using jaas authentication for connecting to Kafka\"\n" +
+                "    fi\n" +
+                "\n" +
+                "    echo \"Creating Kafka topics\"\n"
+                ;
 
         TopicsFinder finder = new TopicsFinder();
 

a42d461ebe79d16802d02d6e5b2bd9aa8cd83b8e
Audrey Kadjar <audreykadjar@gmail.com>
2022-08-02 11:19:26 +0200

    global type fix

--- integration/cypress.config.ts
+++ integration/cypress.config.ts
@@ -1,5 +1,17 @@
 import {defineConfig} from 'cypress';
 
+declare global {
+  namespace Cypress {
+    interface Chainable<Subject> {
+      createChatPluginConversation(): Chainable<Subject>;
+      editContactDetails(): Chainable<Subject>;
+      connectChatPluginConnector(): Chainable<Subject>;
+      editDisplayName(): Chainable<Subject>;
+      listConversationsForContact(): Chainable<Subject>;
+    }
+  }
+}
+
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost',

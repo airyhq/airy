131ec694dac43b87f97490f0ddb02d27ffccfed9
Audrey Kadjar <audreykadjar@gmail.com>
2022-08-02 16:26:50 +0200

    typing fix

--- integration/commands/index.ts
+++ integration/commands/index.ts
@@ -5,12 +5,12 @@ import {listConversationsForContact} from './listConversationsForContact';
 
 declare global {
   namespace Cypress {
-    interface Chainable<Subject> {
-      createChatPluginConversation(): Chainable<Subject>;
-      editContactDetails(): Chainable<Subject>;
-      connectChatPluginConnector(): Chainable<Subject>;
-      editDisplayName(): Chainable<Subject>;
-      listConversationsForContact(): Chainable<Subject>;
+    interface Chainable {
+      createChatPluginConversation(): Chainable<Element>;
+      editContactDetails(): Chainable<Element>;
+      connectChatPluginConnector(): Chainable<Element>;
+      editDisplayName(): Chainable<Element>;
+      listConversationsForContact(): Chainable<Element>;
     }
   }
 }

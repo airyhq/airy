36a395015381a2d055648155bed9986bd3588195
Audrey Kadjar <audreykadjar@gmail.com>
2022-08-03 10:56:34 +0200

    fix build file

--- integration/commands/index.ts
+++ integration/commands/index.ts
@@ -1,7 +1,20 @@
+/// <reference types="cypress" />
 import {connectChatPluginConnector, createChatPluginConversation} from './createChatPluginConversation';
 import {editContactDetails} from './editContactDetails';
 import {editDisplayName} from './editDisplayName';
 import {listConversationsForContact} from './listConversationsForContact';
+
+declare global {
+  namespace Cypress {
+    interface Chainable<Subject> {
+      createChatPluginConversation(): Chainable<Subject>;
+      editContactDetails(): Chainable<Subject>;
+      connectChatPluginConnector(): Chainable<Subject>;
+      editDisplayName(): Chainable<Subject>;
+      listConversationsForContact(): Chainable<Subject>;
+    }
+  }
+}
 
 Cypress.Commands.add('createChatPluginConversation', createChatPluginConversation);
 Cypress.Commands.add('editContactDetails', editContactDetails);

fe46d5d5505d5a2238b8189f4f37d700f762f61f
Audrey Kadjar <audreykadjar@gmail.com>
2022-08-03 10:08:55 +0200

    added index.d.ts type

--- integration/commands/index.ts
+++ integration/commands/index.ts
@@ -3,18 +3,6 @@ import {editContactDetails} from './editContactDetails';
 import {editDisplayName} from './editDisplayName';
 import {listConversationsForContact} from './listConversationsForContact';
 
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      createChatPluginConversation(): Chainable<Element>;
-      editContactDetails(): Chainable<Element>;
-      connectChatPluginConnector(): Chainable<Element>;
-      editDisplayName(): Chainable<Element>;
-      listConversationsForContact(): Chainable<Element>;
-    }
-  }
-}
-
 Cypress.Commands.add('createChatPluginConversation', createChatPluginConversation);
 Cypress.Commands.add('editContactDetails', editContactDetails);
 Cypress.Commands.add('connectChatPluginConnector', connectChatPluginConnector);
---
+++ integration/index.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable<Subject> {
+    createChatPluginConversation(): Chainable<Subject>;
+    editContactDetails(): Chainable<Subject>;
+    connectChatPluginConnector(): Chainable<Subject>;
+    editDisplayName(): Chainable<Subject>;
+    listConversationsForContact(): Chainable<Subject>;
+  }
+}

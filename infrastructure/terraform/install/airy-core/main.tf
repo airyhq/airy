b7c696ac1c94e8ebdd14522d994e4fc293aef5fe
Arman Jindal <58370547+armanjindal@users.noreply.github.com>
2022-08-08 11:31:19 +0200

    [#3541] Rename module for airy-core (#3547)
    Fixes #3541

--- infrastructure/terraform/install/airy-core/main.tf
+++ infrastructure/terraform/install/airy-core/main.tf
@@ -16,7 +16,7 @@ provider "kubernetes" {
   config_path = data.terraform_remote_state.kubernetes.outputs.kubeconfig_path
 }
 
-module "my_airy_core" {
+module "airy_core" {
   source         = "github.com/airyhq/airy.git/infrastructure/terraform/modules/core"
   values_yaml    = file("${var.airy_workspace}/airy.yaml")
   resources_yaml = file("${path.module}/files/defaultResourceLimits.yaml")

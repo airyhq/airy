aea6322ab839d2dc7236ed2869a30aa81639706b
Audrey Kadjar <audreykadjar@gmail.com>
2022-08-23 10:15:17 +0200

    imports fix

--- frontend/control-center/src/components/index.ts
+++ frontend/control-center/src/components/index.ts
@@ -1,7 +1,5 @@
 export * from './ChannelAvatar';
 export * from './Description';
 export * from '../pages/Connectors/InfoCard';
-export * from './TopBar';
-export * from './Sidebar';
 export * from './Switch';
 export * from './Wrapper';

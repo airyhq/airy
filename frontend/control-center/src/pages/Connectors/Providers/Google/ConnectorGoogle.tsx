51e154928522dc8792c999c1e7d94aa93e5403d4
Audrey Kadjar <audreykadjar@gmail.com>
2022-09-01 11:05:53 +0200

    remove unnecessary components

--- frontend/control-center/src/pages/Connectors/Providers/Google/ConnectorGoogle.tsx
+++
@@ -1,9 +0,0 @@
-import {Source} from 'model';
-import React from 'react';
-import ConnectorConfig from '../../ConnectorConfig';
-
-const ConnectorGoogle = () => {
-  return <ConnectorConfig connector={Source.google} />;
-};
-
-export default ConnectorGoogle;

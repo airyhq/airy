0f13fdd0a6e58610d0b8ee46e99876f5cd531594
Thorsten <thorsten@airy.co>
2022-09-01 17:25:07 +0200

    aligned with status page

--- frontend/control-center/src/services/getComponentStatus.ts
+++ frontend/control-center/src/services/getComponentStatus.ts
@@ -6,8 +6,8 @@ export const getComponentStatus = (
   isConfigured: boolean,
   isEnabled: boolean
 ) => {
+  if (isInstalled && !isEnabled) return ComponentStatus.disabled;
   if (!isHealthy) return ComponentStatus.notHealthy;
   if (isInstalled && !isConfigured) return ComponentStatus.notConfigured;
   if (isInstalled && isConfigured && isEnabled) return ComponentStatus.enabled;
-  if (isInstalled && !isEnabled) return ComponentStatus.disabled;
 };

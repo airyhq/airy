0809ef70732ae242a93e75c99d46d410fe6b78ef
Audrey Kadjar <audreykadjar@gmail.com>
2022-09-06 15:27:28 +0200

    remove comment

--- frontend/control-center/src/routes/routes.ts
+++ frontend/control-center/src/routes/routes.ts
@@ -3,23 +3,6 @@ export const ROOT_ROUTE = '/';
 export const CONNECTORS_ROUTE = '/connectors';
 
 export const CATALOG_ROUTE = '/catalog';
-// export const CATALOG_FACEBOOK_ROUTE = '/catalog/facebook';
-// export const CATALOG_CHAT_PLUGIN_ROUTE = '/catalog/chatplugin';
-// export const CATALOG_TWILIO_SMS_ROUTE = '/catalog/twilio.sms';
-// export const CATALOG_TWILIO_WHATSAPP_ROUTE = '/catalog/twilio.whatsapp';
-// export const CATALOG_WHATSAPP_BUSINESS_CLOUD_ROUTE = '/catalog/whatsapp-cloud';
-// export const CATALOG_GOOGLE_ROUTE = '/catalog/google';
-// export const CATALOG_INSTAGRAM_ROUTE = '/catalog/instagram';
-// export const CATALOG_DIALOGFLOW_ROUTE = '/catalog/dialogflow';
-// export const CATALOG_ZENDESK_ROUTE = '/catalog/zendesk';
-// export const CATALOG_SALESFORCE_ROUTE = '/catalog/salesforce';
-// export const CATALOG_CONGNIFY_ROUTE = '/catalog/congnify';
-// export const CATALOG_AMELIA_ROUTE = '/catalog/amelia';
-// export const CATALOG_FRONTEND_INBOX_ROUTE = '/catalog/frontend-inbox';
-// export const CATALOG_RASA_ROUTE = '/catalog/rasa';
-// export const CATALOG_WEBHOOKS_ROUTE = '/catalog/webhooks';
-// export const CATALOG_MOBILE_ROUTE = '/catalog/mobile';
-// export const CATALOG_VIBER_ROUTE = '/catalog/viber';
 
 export const INBOX_ROUTE = '/inbox';
 export const INBOX_CONNECTED_ROUTE = '/inbox/connected';

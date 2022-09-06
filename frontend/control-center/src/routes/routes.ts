cb3ecbff95efadb189682971a954fb54089b95e0
Audrey Kadjar <audreykadjar@gmail.com>
2022-09-06 16:01:01 +0200

    remove unecessary routes

--- frontend/control-center/src/routes/routes.ts
+++ frontend/control-center/src/routes/routes.ts
@@ -22,13 +22,6 @@ export const CATALOG_MOBILE_ROUTE = '/catalog/mobile';
 export const CATALOG_VIBER_ROUTE = '/catalog/viber';
 
 export const INBOX_ROUTE = '/inbox';
-export const INBOX_CONNECTED_ROUTE = '/inbox/connected';
-export const INBOX_FACEBOOK_ROUTE = '/inbox/facebook';
-export const INBOX_CHAT_PLUGIN_ROUTE = '/inbox/chatplugin';
-export const INBOX_TWILIO_SMS_ROUTE = '/inbox/twilio.sms';
-export const INBOX_TWILIO_WHATSAPP_ROUTE = '/inbox/twilio.whatsapp';
-export const INBOX_GOOGLE_ROUTE = '/inbox/google';
-export const INBOX_INSTAGRAM_ROUTE = '/inbox/instagram';
 
 export const STATUS_ROUTE = '/status';
 export const WEBHOOKS_ROUTE = '/webhooks';

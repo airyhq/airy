1566a689d23806768901a8a6b3bcab879de841a0
Bilge <bilge@airy.co>
2022-09-14 12:23:07 +0200

    [#2556] GCP-GKE terrafrom module (#3717)

--- infrastructure/terraform/modules/gcp-gke/variables.tf
+++ infrastructure/terraform/modules/gcp-gke/variables.tf
@@ -1,51 +1,28 @@
 variable "project_id" {
-  description = "The project is airy-core-gke"
+  description = "The project defined in gcloud config is airy-core"
 }
-
 variable "region" {
-  description = "The region is us-central1"
+  description = "The region defined in gcloud config is us-central1"
 }
-
-variable "gke_username" {
-  default     = ""
-  description = "gke username"
+variable "zone" {
+  default     = "us-central1-a"
+  description = "The zone defined in gcloud config is us-central1-a"
 }
-
-variable "gke_password" {
-  default     = ""
-  description = "gke password"
-}
-
 variable "gke_num_nodes" {
   default     = 2
   description = "number of gke nodes"
 }
-
-variable "zone" {
-  default     = "us-central1-a"
-  description = "The zone is us-central1-a"
-}
-
-variable "ssh_key" {
-  description = "The name of the ssh key to use"
-  default     = "~/.ssh/id_rsa"
-}
-
-
 variable "vpc_name" {
   default = "airy-core-vpc"
 }
-
 variable "private_subnets" {
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
-
 variable "public_subnets" {
   type        = list(string)
   default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
-
 variable "network" {
   description = "The VPC network created to host the cluster in"
   default     = "gke-network"
@@ -61,4 +38,16 @@ variable "ip_range_pods_name" {
 variable "ip_range_services_name" {
   description = "The secondary ip range to use for services"
   default     = "ip-range-services"
+}
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
+variable "ssh_key" {
+  description = "The name of the ssh key to use"
+  default     = "~/.ssh/id_rsa"
 }

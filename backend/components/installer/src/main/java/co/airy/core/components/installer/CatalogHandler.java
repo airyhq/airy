e7c5e15035b99d5fa1906241ab47801284d0692f
Juan Sebastian Pena Rodriguez <juan-sebastian@pena-rodriguez.com>
2022-09-15 00:07:52 +0200

    Catalog first steps

---
+++ backend/components/installer/src/main/java/co/airy/core/components/installer/CatalogHandler.java
@@ -0,0 +1,14 @@
+package co.airy.core.api.components.installer;
+
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+
+import co.airy.log.AiryLoggerFactory;
+
+@Service
+public class CatalogHandler {
+    private static final Logger log = AiryLoggerFactory.getLogger(CatalogHandler.class);
+
+    CatalogHandler() {
+    }
+}

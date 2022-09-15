f423772fa57e8b35901cc73d0bc44a7fdb81a9c2
Audrey Kadjar <audreykadjar@gmail.com>
2022-09-15 12:36:52 +0200

    fixed status button

--- frontend/control-center/src/services/getComponentStatus.ts
+++ frontend/control-center/src/services/getComponentStatus.ts
@@ -6,8 +6,8 @@ export const getComponentStatus = (
   isConfigured: boolean,
   isEnabled: boolean
 ) => {
-  if (isInstalled && !isEnabled) return ComponentStatus.disabled;
   if (isInstalled && !isConfigured) return ComponentStatus.notConfigured;
+  if (isInstalled && !isEnabled) return ComponentStatus.disabled;
   if (!isHealthy) return ComponentStatus.notHealthy;
   if (isInstalled && isConfigured && isEnabled) return ComponentStatus.enabled;
 };

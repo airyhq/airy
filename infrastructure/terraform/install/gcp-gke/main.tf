dfdad93a49a56db638482f5fa5081fae08dbf31a
Bilge <bilge@airy.co>
2022-09-16 15:24:57 +0200

    [#2556] Cleaning up the GCP-GKE after the review (#3717)

--- infrastructure/terraform/install/gcp-gke/main.tf
+++ infrastructure/terraform/install/gcp-gke/main.tf
@@ -1,6 +1,5 @@
 module "gcp-gke" {
-  #source = "github.com/airyhq/airy.git/infrastructure/terraform/modules/gcp-gke"
-  source =  "../../modules/gcp-gke"
+  source = "github.com/airyhq/airy.git/infrastructure/terraform/modules/gcp-gke"
 
   project_id                 = var.project_id
   region                     = var.region

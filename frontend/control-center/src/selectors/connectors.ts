09c9cb7b3244e5e72412648df3f26b931a600410
Thorsten <thorsten@airy.co>
2022-09-28 15:06:28 +0200

    added mergedConnectors selector

--- frontend/control-center/src/selectors/connectors.ts
+++ frontend/control-center/src/selectors/connectors.ts
@@ -1,6 +1,26 @@
 import {StateModel} from '../reducers';
 import {useSelector} from 'react-redux';
-import {Source} from 'model';
+import {Channel, Source} from 'model';
+import {createSelector} from 'reselect';
+import {merge} from 'lodash-es';
+import {removePrefix} from '../services';
+import {allChannelsConnected} from './channels';
+
+type Connector = {
+  name?: string;
+  isInstalled?: boolean;
+  isEnabled?: boolean;
+  isHealthy?: boolean;
+  isConfigured?: boolean;
+  configureValues?: {};
+  isChannel?: boolean;
+  connectedChannels?: number;
+  status?: string;
+};
+
+export type ConnectorsModel = {
+  [connectorName: string]: Connector;
+};
 
 export const useCurrentConnectorForSource = (source: Source) => {
   const connectors = useSelector((state: StateModel) => state.data.connector);
@@ -10,3 +30,65 @@ export const useCurrentConnectorForSource = (source: Source) => {
 
   return {...connectorInfoArrFlat[1]};
 };
+
+export const getMergedConnectors = createSelector(
+  (state: StateModel) => state.data.catalog,
+  (state: StateModel) => state.data.config.components,
+  (state: StateModel) => state.data.connector,
+  (state: StateModel) => Object.values(allChannelsConnected(state)),
+  (catalog, components, connectors, channels) => {
+    let structuredConnector: Connector = {};
+    let structuredCatalog: Connector = {};
+    let structuredComponent: Connector = {};
+    const catalogList: ConnectorsModel = {};
+    const connectorsList: ConnectorsModel = {};
+    const componentsList: ConnectorsModel = {};
+
+    Object.entries(catalog).map(catalog => {
+      const channelsBySource = (Source: Source) => channels.filter((channel: Channel) => channel.source === Source);
+      const connectedChannels = channelsBySource(catalog[1].source).length;
+      const isChannel = catalog[1].isChannel?.includes('true') ?? false;
+      const isInstalled = catalog[1].installed;
+      const name = removePrefix(catalog[0]);
+
+      structuredCatalog = {
+        name: name,
+        isInstalled: isInstalled,
+        isConfigured: !isInstalled && false,
+        isHealthy: !isInstalled && false,
+        isEnabled: !isInstalled && false,
+        isChannel: isChannel,
+        connectedChannels: connectedChannels,
+      };
+      catalogList[structuredCatalog.name] = structuredCatalog;
+    });
+
+    Object.entries(connectors).map(connector => {
+      let isConfigured = false;
+      if (Object.values(connector[1]).length > 0) {
+        isConfigured = true;
+      }
+
+      structuredConnector = {
+        name: connector[0],
+        configureValues: connector[1],
+        isConfigured: isConfigured,
+      };
+
+      connectorsList[structuredConnector.name] = structuredConnector;
+    });
+
+    Object.entries(components).map(component => {
+      structuredComponent = {
+        name: component[0],
+        isEnabled: component[1].enabled,
+        isHealthy: component[1].healthy,
+      };
+      componentsList[structuredComponent.name] = structuredComponent;
+    });
+
+    const mergedConnectors = merge(catalogList, connectorsList, componentsList);
+
+    return mergedConnectors;
+  }
+);

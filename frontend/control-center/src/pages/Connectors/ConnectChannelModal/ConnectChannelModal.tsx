3bdedb126719380b3d51504ed3f4433ef285ef47
Thorsten <thorsten@airy.co>
2022-09-29 15:54:55 +0200

    WIP

---
+++ frontend/control-center/src/pages/Connectors/ConnectChannelModal/ConnectChannelModal.tsx
@@ -0,0 +1,18 @@
+import { Source } from 'model';
+import React from 'react';
+import styles from './ConnectChannelModal.module.scss';
+
+type ConnectChannelModaProps = {};
+
+export const ConnectChannelModal = (props: ConnectChannelModaProps) => {
+
+    const getInputValues = (source: Source) => {
+
+    }
+
+
+
+  return <div className={styles.container}>
+
+  </div>;
+};

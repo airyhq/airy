190e92096192e13bc3cd59c726a619cb728281b5
Juan Sebastian Pena Rodriguez <juan-sebastian@pena-rodriguez.com>
2022-10-10 08:53:52 -0500

    Add missing deps

--- backend/components/installer/src/test/java/co/airy/core/api/components/installer/CatalogHandlerTest.java
+++ backend/components/installer/src/test/java/co/airy/core/api/components/installer/CatalogHandlerTest.java
@@ -2,12 +2,22 @@ package co.airy.core.api.components.installer;
 
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
+
+import co.airy.kafka.schema.application.ApplicationCommunicationMetadata;
+import co.airy.kafka.test.KafkaTestHelper;
+import co.airy.kafka.test.junit.SharedKafkaTestResource;
 import co.airy.spring.core.AirySpringBootApplication;
 
+import io.kubernetes.client.openapi.ApiClient;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class)
@@ -15,6 +25,31 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @TestPropertySource(value = "classpath:test.properties")
 @ExtendWith(SpringExtension.class)
 public class CatalogHandlerTest {
+
+    @RegisterExtension
+    public static final SharedKafkaTestResource sharedKafkaTestResource = new SharedKafkaTestResource();
+    private static KafkaTestHelper kafkaTestHelper;
+
+    private static final ApplicationCommunicationMetadata applicationCommunicationMetadata = new ApplicationCommunicationMetadata();
+
+    @MockBean
+    private ApiClient apiClient;
+
+    @Autowired
+    private CatalogHandler catalogHandler;
+
+    @BeforeAll
+    static void beforeAll() throws Exception {
+        kafkaTestHelper = new KafkaTestHelper(
+                sharedKafkaTestResource,
+                applicationCommunicationMetadata);
+        kafkaTestHelper.beforeAll();
+    }
+
+    @AfterAll
+    static void afterAll() throws Exception {
+        kafkaTestHelper.afterAll();
+    }
 
     @Test
     void canListComponents() throws Exception {

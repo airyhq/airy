e4d937a3bcd4756461678f31a9248b1572e99ac5
Juan Sebastian Pena Rodriguez <juan-sebastian@pena-rodriguez.com>
2022-10-10 11:00:44 -0500

    CatalogHandler tests first steps

--- backend/components/installer/src/test/java/co/airy/core/api/components/installer/CatalogHandlerTest.java
+++ backend/components/installer/src/test/java/co/airy/core/api/components/installer/CatalogHandlerTest.java
@@ -2,7 +2,9 @@ package co.airy.core.api.components.installer;
 
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
@@ -13,9 +15,20 @@ import co.airy.kafka.test.junit.SharedKafkaTestResource;
 import co.airy.spring.core.AirySpringBootApplication;
 
 import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.models.V1Job;
+import io.kubernetes.client.openapi.models.V1ObjectMeta;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.doReturn;
+
+import java.io.File;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -32,8 +45,14 @@ public class CatalogHandlerTest {
 
     private static final ApplicationCommunicationMetadata applicationCommunicationMetadata = new ApplicationCommunicationMetadata();
 
+    @Mock
+    private ApplicationReadyEvent event;
+
     @MockBean
     private ApiClient apiClient;
+
+    @MockBean
+    private HelmJobHandler helmJobHandler;
 
     @Autowired
     private CatalogHandler catalogHandler;
@@ -51,7 +70,23 @@ public class CatalogHandlerTest {
         kafkaTestHelper.afterAll();
     }
 
+
     @Test
-    void canListComponents() throws Exception {
+    public void canOnApplicationEvent(@TempDir File tempDir) throws Exception {
+        callOnApplicationEvent(tempDir);
     }
+
+    @Test
+    void canGetComponents(@TempDir File tempDir) throws Exception {
+        callOnApplicationEvent(tempDir);
+
+        final V1Job job = new V1Job()
+            .metadata(new V1ObjectMeta().name("helm-installed").namespace("test-namespace"));
+    }
+
+    private void callOnApplicationEvent(File tempDir) throws Exception {
+        ReflectionTestUtils.setField(catalogHandler, "repoFolder", tempDir);
+        catalogHandler.onApplicationEvent(event);
+    }
+
 }

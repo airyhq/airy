b01b49eb6e5b9d9bcfb488e0eb05520517048f5a
Juan Sebastian Pena Rodriguez <juan-sebastian@pena-rodriguez.com>
2022-10-25 14:06:36 -0500

    mask secrets

--- backend/components/configuration/src/main/java/co/airy/core/api/components/configuration/model/ComponentConfig.java
+++ backend/components/configuration/src/main/java/co/airy/core/api/components/configuration/model/ComponentConfig.java
@@ -2,6 +2,7 @@ package co.airy.core.api.components.configuration.model;
 
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import lombok.AllArgsConstructor;
@@ -15,13 +16,28 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ComponentConfig {
 
+    private static final Pattern secretMatcher = Pattern.compile("(?i)secret|key|token|credentials|saFile");
+
     private String name;
     private Map<String, String> data;
+
+    private static Map<String, String> maskSecrets(Map<String, String> data) {
+        return data
+            .entrySet()
+            .stream()
+            .map(e -> {
+                if (secretMatcher.matcher(e.getKey()).find()) {
+                    e.setValue("*********");
+                }
+                return e;
+            })
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
 
     public static Map<String, Object> componentsConfigsListToMap(List<ComponentConfig> componentsConfigs) {
         final Map<String, Map<String, String>> cc = componentsConfigs
             .stream()
-            .collect(Collectors.toMap(ComponentConfig::getName, ComponentConfig::getData));
+            .collect(Collectors.toMap(ComponentConfig::getName, v -> maskSecrets(v.getData())));
         return Map.of("components", cc);
     }
 }

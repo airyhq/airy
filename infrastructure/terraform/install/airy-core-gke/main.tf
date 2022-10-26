08ada034d509efa73ed9c66bd910579cffb82af4
Bilge <bilge@airy.co>
2022-10-26 16:27:24 +0200

    [3645]Terraform deployment for airy-core on gcp-gke

--- infrastructure/terraform/install/airy-core-gke/main.tf
+++ infrastructure/terraform/install/airy-core-gke/main.tf
@@ -5,14 +5,15 @@ data "terraform_remote_state" "gcp-gke" {
   }
 }
 
+provider "helm" {
+  kubernetes {
+    config_path = var.kubeconfig_output_path
+  }
+}
 
-# data "google_client_config" "default" {}
-
-# provider "kubernetes" {
-#   host                   = "https://${module.gcp-gke.kubernetes_endpoint}"
-#   token                  = data.google_client_config.default.access_token
-#   cluster_ca_certificate = base64decode(module.gcp-gke.ca_certificate)
-# }
+provider "kubernetes" {
+  config_path = var.kubeconfig_output_path
+}
 
 
 # data "google_client_config" "default" {}
@@ -21,27 +22,14 @@ data "terraform_remote_state" "gcp-gke" {
 # data.terraform_remote_state.gcp-gke.resources.google_container_cluster.gke_core.modules.gke_auth.host
 # data.terraform_remote_state.gcp-gke.outputs.ca_certificate
 
-provider "helm" {
-  kubernetes {
-    config_path = var.kubeconfig_output_path
-    host  = "https://${data.terraform_remote_state.gcp-gke.outputs.kubernetes_endpoint}"
-    token = data.terraform_remote_state.gcp-gke.outputs.client_token
-    cluster_ca_certificate = base64decode(
-      data.terraform_remote_state.gcp-gke.outputs.ca_certificate
-      )
-    # cluster_ca_certificate = base64decode(
-    #   data.terraform_remote_state.gcp-gke.google_container_cluster.gke_core.master_auth[0].cluster_ca_certificate
-    # )
-}
+
+module "airy_core" {
+  source         = "github.com/airyhq/airy.git/infrastructure/terraform/modules/core"
+  values_yaml    = file("${var.airy_workspace}/airy.yaml")
+  resources_yaml = file("${path.module}/files/defaultResourceLimits.yaml")
 }
 
-# module "airy_core" {
-#   source         = "github.com/airyhq/airy.git/infrastructure/terraform/modules/core"
-#   values_yaml    = file("${var.airy_workspace}/airy.yaml")
-#   resources_yaml = file("${path.module}/files/defaultResourceLimits.yaml")
-# }
-
-# resource "local_file" "cli_yaml" {
-#   filename = "${var.airy_workspace}/cli.yaml"
-#   content  = "apihost: http://${module.airy_core.loadbalancer}\n"
-# }
+resource "local_file" "cli_yaml" {
+  filename = "${var.airy_workspace}/cli.yaml"
+  content  = "apihost: http://${module.airy_core.loadbalancer}\n"
+}
--- infrastructure/terraform/install/airy-core-gke/state.tf
+++ infrastructure/terraform/install/airy-core-gke/state.tf
@@ -1,5 +1,5 @@
 terraform {
   backend "local" {
-    path = "../gcp-gke/terraform.tfstate" #"../airy-core-gke.tfstate"
+    path = "../airy-core-gke.tfstate"
   }
 }

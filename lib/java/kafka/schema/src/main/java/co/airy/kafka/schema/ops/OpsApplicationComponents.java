31c60784621e5561c3d5bfd652895b533be2f1c0
Juan Sebastian Pena Rodriguez <juan-sebastian@pena-rodriguez.com>
2022-11-01 08:13:07 -0500

    Add topic name

---
+++ lib/java/kafka/schema/src/main/java/co/airy/kafka/schema/ops/OpsApplicationComponents.java
@@ -0,0 +1,10 @@
+package co.airy.kafka.schema.ops;
+
+import co.airy.kafka.schema.OpsApplication;
+
+public class OpsApplicationComponents extends OpsApplication {
+    @Override
+    public String dataset() {
+        return "components";
+    }
+}

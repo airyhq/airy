991be4d8c104c55bc0eb19924eefa5569216667c
Juan Sebastian Pena Rodriguez <juan-sebastian@pena-rodriguez.com>
2022-11-04 14:11:50 -0500

    test for installationStatus

---
+++ backend/components/installer/src/test/java/co/airy/core/api/components/installer/InstallationStatusComponentsHandlerTest.java
@@ -0,0 +1,114 @@
+package co.airy.core.api.components.installer;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+
+import co.airy.core.api.components.installer.model.ComponentDetails;
+import co.airy.kafka.schema.application.ApplicationCommunicationMetadata;
+import co.airy.kafka.test.KafkaTestHelper;
+import co.airy.kafka.test.junit.SharedKafkaTestResource;
+import co.airy.spring.core.AirySpringBootApplication;
+
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiResponse;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Job;
+import io.kubernetes.client.openapi.models.V1ObjectMeta;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.doReturn;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import co.airy.core.api.components.installer.model.InstallationStatus;
+
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
+@SpringBootTest(classes = AirySpringBootApplication.class)
+@TestPropertySource(value = "classpath:test.properties")
+@ExtendWith(SpringExtension.class)
+public class InstallationStatusComponentsHandlerTest {
+
+    @RegisterExtension
+    public static final SharedKafkaTestResource sharedKafkaTestResource = new SharedKafkaTestResource();
+    private static KafkaTestHelper kafkaTestHelper;
+
+    private static final ApplicationCommunicationMetadata applicationCommunicationMetadata = new ApplicationCommunicationMetadata();
+
+    @Mock
+    private ApplicationReadyEvent event;
+
+    @MockBean
+    private ApiClient apiClient;
+
+    @MockBean
+    private HelmJobHandler helmJobHandler;
+
+    @Autowired
+    private CatalogHandler catalogHandler;
+
+    @Autowired
+    private GitHandler gitHandler;
+
+    @Captor
+    private ArgumentCaptor<ArrayList<String>> cmd;
+
+    @Captor
+    private ArgumentCaptor<Map<String, String>> labels;
+
+    @BeforeAll
+    static void beforeAll() throws Exception {
+        kafkaTestHelper = new KafkaTestHelper(
+                sharedKafkaTestResource,
+                applicationCommunicationMetadata);
+        kafkaTestHelper.beforeAll();
+    }
+
+    @AfterAll
+    static void afterAll() throws Exception {
+        kafkaTestHelper.afterAll();
+    }
+
+    private String getInstalledComponents() {
+        final List<String> installedComponents = List.of(
+            "api-contacts",
+            "enterprise-salesforce-contacts-ingestion",
+            "enterprise-zendesk-connector",
+            "integration-webhook",
+            "rasa-connector",
+            "sources-chatplugin",
+            "sources-facebook",
+            "sources-google",
+            "sources-twilio",
+            "sources-whatsapp");
+
+        return String.join("\n", installedComponents);
+    }
+}

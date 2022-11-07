01bfb05360bda1f4b26c04621ee2987f5312e814
Audrey Kadjar <audreykadjar@gmail.com>
2022-11-07 15:24:09 +0100

    disable fix

--- lib/typescript/translations/translations.ts
+++ lib/typescript/translations/translations.ts
@@ -991,8 +991,7 @@ const resources = {
       ibmDescription: 'IBM Watson Assistant verwendet künstliche Intelligenz, die den Kunden versteht.',
 
       //Amazon Lex V2
-      amazonLexV2Description:
-        'Ein Webdienst zur Erstellung von Konversationsschnittstellen für Anwendungen mit Sprache und Text.',
+      amazonLexV2Description: 'Ein Webdienst zur Erstellung von Konversationsschnittstellen',
 
       //Amazon S3
       amazons3Description: 'Amazon Simple Storage Service (Amazon S3) ist ein Objektspeicherdienst.',
@@ -1510,8 +1509,7 @@ const resources = {
       ibmDescription: `L'assistant IBM Watson utilise une intelligence artificielle qui comprend les clients.`,
 
       //Amazon Lex V2
-      amazonLexV2Description:
-        'Un service web pour construire une interface conversationnelle pour les applications utilisant la voix et le texte.',
+      amazonLexV2Description: 'Un service web pour construire une interface conversationnelle.',
 
       //Amazon S3
       amazons3Description: `Amazon Simple Storage Service (Amazon S3) est un service de stockage d'objets.`,
@@ -2072,8 +2070,7 @@ const resources = {
       ibmDescription: 'El Asistente Watson de IBM utiliza inteligencia artificial que entiende a los clientes.',
 
       //Amazon Lex V2
-      amazonLexV2Description:
-        'Un servicio web para construir una interfaz conversacional para aplicaciones utilizando voz y texto.',
+      amazonLexV2Description: 'Un servicio web para construir una interfaz conversacional.',
 
       //Amazon S3
       amazons3Description: 'Amazon Simple Storage Service (Amazon S3) es un servicio de almacenamiento de objetos.',

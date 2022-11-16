5e208e6f6578e6b4cae183ae5f74cce68ceb47b7
Audrey Kadjar <audreykadjar@gmail.com>
2022-11-16 16:02:44 +0100

    frontend for websovcket update

--- frontend/control-center/src/components/AiryWebsocket/index.tsx
+++ frontend/control-center/src/components/AiryWebsocket/index.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {connect, ConnectedProps} from 'react-redux';
 import {WebSocketClient} from 'websocketclient';
+import {updateComponentInstallationStatusAction} from '../../actions';
 
 import {env} from '../../env';
 
@@ -9,17 +10,14 @@ type AiryWebSocketProps = {children: React.ReactNode} & ConnectedProps<typeof co
 export const AiryWebSocketContext = React.createContext({refreshSocket: null});
 
 const mapDispatchToProps = () => dispatch => ({
-  onComponentUpdate: update => console.log(update);
+  onComponentUpdate: update => dispatch(updateComponentInstallationStatusAction(update)),
 });
 
 const connector = connect(null, mapDispatchToProps);
 
-
 const AiryWebSocket: React.FC<AiryWebSocketProps> = props => {
-  const {children} = props;
+  const {children, onComponentUpdate} = props;
   const [webSocketClient, setWebSocketClient] = useState(null);
-
-  const onComponentUpdate = update => console.log('update', update);
 
   const refreshSocket = () => {
     if (webSocketClient) {

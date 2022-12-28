830b8040f5d5025fa8a8fd777b30fb0833985769
Ljupco Vangelski <ljupcovangelski@users.noreply.github.com>
2022-12-28 11:29:08 +0100

    [#4006] Update oauth scopes for GCP (#4007)

--- infrastructure/terraform/modules/gcp-gke/variables.tf
+++ infrastructure/terraform/modules/gcp-gke/variables.tf
@@ -1,11 +1,11 @@
 variable "project_id" {
-  default     = "airy-core"
   description = "The project defined in gcloud config is airy-core"
+  default     = "airy-core"
 }
 
 variable "region" {
-  default     = "us-central1"
   description = "The region defined in gcloud config is us-central1"
+  default     = "us-central1"
 }
 
 variable "gke_name" {
@@ -14,26 +14,35 @@ variable "gke_name" {
 }
 
 variable "gke_num_nodes" {
-  default     = 1
   description = "Number of gke nodes"
+  default     = 2
 }
 
 variable "gke_node_locations" {
-  default     = []
   description = "List of zones for the nodes in the node pool"
+  default     = []
 }
 
 variable "vpc_name" {
-  default     = "airy-core-vpc"
   description = "The name of the created VPC"
+  default     = "airy-core-vpc"
 }
 
 variable "kubeconfig_output_path" {
-  default     = "../kube.conf"
   description = "The location of the kubeconfig file"
+  default     = "../kube.conf"
 }
 
 variable "gke_instance_type" {
-  default     = "n1-standard-2"
   description = "The type of the instances in the node pool"
+  default     = "n1-standard-2"
+}
+
+variable "gke_oauth_scopes" {
+  description = "The OAuth scopes used for the nodegroups in Kubernetes"
+  default = [
+    "https://www.googleapis.com/auth/logging.write",
+    "https://www.googleapis.com/auth/monitoring",
+    "https://www.googleapis.com/auth/cloud-platform"
+  ]
 }

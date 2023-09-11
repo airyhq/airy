e4baba2e55c69a38913fe0d59a930a2b6ddee58a
Ljupco Vangelski <ljupcovangelski@users.noreply.github.com>
2023-09-11 18:28:21 +0200

    Fix selecting fields in streams (#4123)

--- frontend/control-center/src/pages/Streams/Creation/index.tsx
+++ frontend/control-center/src/pages/Streams/Creation/index.tsx
@@ -203,6 +203,7 @@ const Creation = (props: ListModeProps) => {
                             backgroundColor: 'transparent',
                             border: '1px solid gray',
                             borderRadius: '10px',
+                            pointerEvents: 'none',
                           }}
                         />
                       </div>
@@ -232,6 +233,7 @@ const Creation = (props: ListModeProps) => {
                             backgroundColor: 'transparent',
                             border: '1px solid gray',
                             borderRadius: '10px',
+                            pointerEvents: 'none',
                           }}
                         />
                       </div>
@@ -281,6 +283,7 @@ const Creation = (props: ListModeProps) => {
                               backgroundColor: 'transparent',
                               border: '1px solid gray',
                               borderRadius: '10px',
+                              pointerEvents: 'none',
                             }}
                           />
                         </div>
@@ -312,6 +315,7 @@ const Creation = (props: ListModeProps) => {
                               backgroundColor: 'transparent',
                               border: '1px solid gray',
                               borderRadius: '10px',
+                              pointerEvents: 'none',
                             }}
                           />
                         </div>
@@ -394,6 +398,7 @@ const Creation = (props: ListModeProps) => {
                                   backgroundColor: 'transparent',
                                   border: '1px solid gray',
                                   borderRadius: '10px',
+                                  pointerEvents: 'none',
                                 }}
                               />
                             </div>
@@ -437,6 +442,7 @@ const Creation = (props: ListModeProps) => {
                                   backgroundColor: 'transparent',
                                   border: '1px solid gray',
                                   borderRadius: '10px',
+                                  pointerEvents: 'none',
                                 }}
                               />
                             </div>

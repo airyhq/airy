1ba800798216058453b0e7368d3b846137392fe9
Aitor Algorta <aitor@airy.co>
2024-02-12 15:15:43 +0100

    general improvements

---
+++ backend/components/schema-registry-manager/src/types.js
@@ -0,0 +1,8 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', {value: true});
+exports.SchemaProvider = void 0;
+var SchemaProvider;
+(function (SchemaProvider) {
+  SchemaProvider['karapace'] = 'karapace';
+  SchemaProvider['confluentCloud'] = 'confluent-cloud';
+})(SchemaProvider || (exports.SchemaProvider = SchemaProvider = {}));

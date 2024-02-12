1df0d50f58d4c8d1da8f13cc7964f4a95b60861b
Aitor Algorta <aitor@airy.co>
2024-02-12 15:33:38 +0100

    remove js

--- backend/components/schema-registry-manager/src/types.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', {value: true});
-exports.SchemaProvider = void 0;
-var SchemaProvider;
-(function (SchemaProvider) {
-  SchemaProvider['karapace'] = 'karapace';
-  SchemaProvider['confluentCloud'] = 'confluent-cloud';
-})(SchemaProvider || (exports.SchemaProvider = SchemaProvider = {}));
